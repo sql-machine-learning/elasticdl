a916899a2f213be3b9f1d0923acf3b89f679d2c1
Li Zou <zou000@gmail.com>
2018-12-03 15:38:07 -0800

    Add master

--- python/elasticdl/system/master.py
+++ python/elasticdl/system/master.py
@@ -1,7 +1,9 @@
 from binascii import crc32
+from elasticdl.recordio import File
 import queue
 import threading
 import time
+
 
 class Work(object):
     '''
@@ -9,33 +11,36 @@ class Work(object):
     '''
     @staticmethod
     def _id(*parts):
-        # convert each integer part into a 4-byte array, join them and return crc32
+        # convert each integer part into a 4-byte array, join them and return
+        # crc32
         return crc32(b''.join(for p in parts p.to_bytes(4, 'little')))
 
-    def __init__(self, file_index, offset, epoch, trail=0):
+    def __init__(self, file_index, offset, epoch, trial=0):
         self.file_index = file_index
         self.offset = offset
         self.epoch = epoch
-        self.trail = trail
-        # Finish earlier epoch first, but randomize the order of chunks within an epoch.
+        self.trial = trial
+        # Finish earlier epoch first, but randomize the order of chunks within
+        # an epoch.
         self.id = _id(epoch, file_index, offset)
         self.priority = (epoch, self.id)
 
-    def next_trail(self):
-        return Work(self.file_index, self.offset, self.epoch, self.trail + 1)
+    def next_trial(self):
+        return Work(self.file_index, self.offset, self.epoch, self.trial + 1)
 
     def next_epoch(self):
         return Work(self.file_index, self.offset, self.epoch + 1)
 
+
 class WorkQueue(object):
-    def __init__(self, num_epoch, max_trail, files):
+    def __init__(self, num_epoch, max_trial, files):
         self._files = files
         self._num_epoch = num_epoch
-        self._max_trail = max_trail
+        self._max_trial = max_trial
         self._q = queue.PriorityQueue()
         self._in_flight = {}
         self._lock = threading.Lock()
-    
+
     def put(self, file_index, offset):
         with self._lock:
             work = Work(file_index, offset, 0)
@@ -47,14 +52,14 @@ class WorkQueue(object):
             # TODO: support worker timeout
             self._in_flight[work.id] = work
             return (self.id, self._files[work.file_index], work.offset)
-        
+
     def work_done(self, id, succeed):
         with self._lock:
             work = self._in_flight.pop(id)
             next_work = None
             if not succeed:
-                if work.trail + 1 < self._max_trail:
-                    next_work = work.next_trail() 
+                if work.trial + 1 < self._max_trial:
+                    next_work = work.next_trial()
                 else:
                     print('work failed', work)
             elif work.epoch + 1 < self._num_epoch:
@@ -64,35 +69,36 @@ class WorkQueue(object):
             if next_work:
                 self._q.put(next_work)
             self._q.task_done()
-    
+
     def join(self):
         self._q.join()
 
+
 class Master(object):
-    def __init__(self, data_files, num_epoch, max_trail):
+    def __init__(self, data_files, num_epoch, max_trial):
         assert num_epoch > 0
-        assert max_trail > 0
+        assert max_trial > 0
 
         self._data_files = data_files
-        self._work_queue = WorkQueue(num_epoch, max_trail)
+        self._work_queue = WorkQueue(num_epoch, max_trial)
         self._lock = threading.Lock()
         self._num_workers = 0
-    
+
     def register_worker(self):
         with self._lock:
             self._num_workers += 1
             return self._work_queue
 
     def run(self):
-        # TODO: use a thread pool to build index.
-        start = time.time()
-        for (f in self._data_files):
-
-
-
-    
-
-    
-
-        
-        
+        # TODO: use a thread pool to build index in parallel.
+        with self._lock:
+            # Technically, we don't need blocking for building indices, but
+            # doing so makes the system cleaner as if there is a corrupted
+            # file, etc., the master will crash and no worker will be started.
+            start = time.time()
+            for (i, f in enumerate(self._data_files)):
+                with File(f) as fd:
+                    for (c in range(fd.get_index().total_chunks())):
+                        self._work_queue.put(i, fd.get_index().chunk_offset(c))
+            print('Time spent on building index: %s seconds:' % time.time() - start)
+        self._work_queue.join()
