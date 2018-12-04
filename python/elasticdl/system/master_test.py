c77336fee17dfd7c6c793d7ac31416160663c9e9
Li Zou <zou000@gmail.com>
2018-12-04 14:31:19 -0800

    wip

---
+++ python/elasticdl/system/master_test.py
@@ -0,0 +1,11 @@
+from master import Master
+from unittest import TestCase
+
+class Worker(object):
+    def __init__(q):
+        self._q = q
+    
+    def run()
+
+class MasterTest(TestCase):
+    def setUp(self)
