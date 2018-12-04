7cf23ce8a0ed557e3ba758af2e586c65c416fba2
Li Zou <zou000@gmail.com>
2018-12-04 14:59:48 -0800

    * Refactor recordio index to make it more efficient and easier to mock

--- python/elasticdl/recordio/file_index.py
+++ python/elasticdl/recordio/file_index.py
@@ -2,24 +2,31 @@ from elasticdl.recordio.global_variables import header_size
 from elasticdl.recordio import Header
 import os
 
+# TODO: use @dataclass
+class _ChunkData(object):
+  def __init__(self, offset, len, num_record):
+    # File offset of each chunk
+    self.offset = offset
+    # Total size of each chunk (include chunk header)
+    self.len=len
+    # Total record count of each chunk
+    self.num_record = num_record
+
+  def __str__(self):
+    return 'offset: %s len: %s num_record: %s' % (self.offset, self.len, self.num_record)
+
+    
 
 class FileIndex(object):
     """ Parse and index the chunk meta info in a recordio file
     """
 
     def __init__(self, in_file):
-        # File offset of each chunk
-        self._chunk_offsets = []
-        # Total size of each chunk (include chunk header)
-        self._chunk_lens = []
-        # Total record count of each chunk
-        self._chunk_records = []
         # Total record count of a recordio file
         self._total_records = 0
-        # Total chunk count of a recordio file
-        self._total_chunks = 0
         # Input recordio file
         self._in_file = in_file
+        self._chunk_data = []
 
         # Do the parse process
         self._parse(in_file)
@@ -33,19 +40,12 @@ class FileIndex(object):
         file_size = os.path.getsize(in_file.name)
         offset = 0
         header = Header()
-        chunk_index = 0
 
         while offset < file_size:
             header.parse(in_file, offset)
-
-            self._chunk_offsets.append(offset)
-            self._chunk_lens.append(header.compress_size())
-            self._chunk_records.append(header.total_count())
+            self._chunk_data.append(_ChunkData(offset, header.compress_size(), header.total_count()))
             self._total_records += header.total_count()
-
             offset += header.compress_size() + header_size
-            chunk_index += 1
-        self._total_chunks = chunk_index
 
     def locate_record(self, index):
         """ Locate the index of chunk and inner chunk for a record in recordio file.
@@ -58,11 +58,10 @@ class FileIndex(object):
           (chunk_index, record_index): the index of chunk in recordio file and index of record in the chunk.
         """
         global_count = 0
-        for chk_index in range(self._total_chunks):
-            global_count += self._chunk_records[chk_index]
+        for chk_index, chunk in enumerate(self._chunk_data):
+            global_count += chunk.num_record
             if index < global_count:
-                return chk_index, (
-                    self._chunk_records[chk_index] - (global_count - index))
+                return chk_index, (chunk.num_record - (global_count - index))
         return -1, -1
 
     def chunk_offset(self, chunk_index):
@@ -74,7 +73,7 @@ class FileIndex(object):
         Returns:
           Chunk offset
         """
-        return self._chunk_offsets[chunk_index]
+        return self._chunk_data[chunk_index].offset
 
     def chunk_len(self, chunk_index):
         """ Returns the total chunk data size
@@ -85,7 +84,7 @@ class FileIndex(object):
         Returns:
           Total chunk size
         """
-        return self._chunk_lens[chunk_index]
+        return self._chunk_data[chunk_index].len
 
     def chunk_records(self, chunk_index):
         """ Returns the record numbers in the chunk
@@ -96,7 +95,7 @@ class FileIndex(object):
         Returns:
           Total record number of the chunk
         """
-        return self._chunk_records[chunk_index]
+        return self._chunk_data[chunk_index].num_record
 
     def total_records(self):
         """ Returns the total record number of the file
@@ -112,4 +111,9 @@ class FileIndex(object):
         Returns:
           Total number of chunks
         """
-        return self._total_chunks
+        return len(self._chunk_data)
+    
+    def __getitem__(self, chunk_index):
+      return self._chunk_data[chunk_index]
+    
+
