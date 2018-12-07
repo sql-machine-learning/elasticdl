fc3fae0bba97e0db5c3e96e926d2716e371680e9
Li Zou <zou000@gmail.com>
2018-12-06 18:49:26 -0800

    Add script to encode/decode mnist data Add sript to generate mnist data in recordio format Add script to show mnist data in recordio file

---
+++ python/elasticdl/datasets/mnist/record_test.py
@@ -0,0 +1,17 @@
+import unittest
+import record
+import numpy as np
+
+
+class RecordTest(unittest.TestCase):
+    def test_round_trip(self):
+        # a random array
+        d = np.ndarray(shape=(28, 28), dtype="uint8")
+        encoded = record.encode(d, 5)
+        dd, label = record.decode(encoded)
+        np.testing.assert_array_equal(d, dd)
+        self.assertEqual(label, 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
