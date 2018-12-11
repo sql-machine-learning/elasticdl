c44cafa52d894ea42d912daa529a222fdcae1577
Li Zou <zou000@gmail.com>
2018-12-10 21:23:11 -0800

    * change image build and run process

--- dockerfile/dev.sh
+++ dockerfile/dev.sh
@@ -1,3 +1,19 @@
 #! /bin/bash
-docker run --rm -it -v $HOME:/home/$USER devel:$USER /bin/bash
+set -x
+USER_ID=$(id -u)
+GRP_ID=$(id -g)
+USER_NAME=${USER}
+if [[ $USER_ID == 0 ]]; then
+    USER_ID=${SUDO_UID}
+    GRP_ID=${SUDO_GID}
+    USER_NAME=${SUDO_USER}
+fi
+HOME_DIR=$(eval echo ~${USER_NAME})
 
+docker run --rm -it \
+    -u ${USER_ID}:${GRP_ID} \
+    -v ${HOME_DIR}:/home/${USER_NAME}  \
+    -w /home/${USER_NAME} \
+    -e USER=${USER_NAME} \
+    -e HOME=/home/${USER_NAME} \
+    reg.docker.alibaba-inc.com/elasticdl/dev /bin/bash
