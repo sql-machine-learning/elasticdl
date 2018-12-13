e2c6239c9dad551336e5b90d47af5d141f975aa5
Li Zou <zou000@gmail.com>
2018-12-13 13:17:22 -0800

    * add tmux to docker image and change root passwd to root

--- dockerfile/devel/entry.sh
+++ dockerfile/devel/entry.sh
@@ -2,5 +2,8 @@
 
 useradd -r -g ${REAL_GID} -u ${REAL_UID} ${REAL_USER} -d /home/${REAL_USER} -s /bin/bash || exit -1
 
+# Change root password to 'root'
+echo -e "root\nroot" | (passwd root) || exit -2
+
 su - ${REAL_USER}
 
