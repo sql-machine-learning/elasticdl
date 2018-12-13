7b65ba462fa93a7072d33007f5bbcad4e94a0200
乙划 <zht108229@alipay.com>
2018-12-13 11:28:32 +0000

    Support user defined data processing in worker

--- python/elasticdl/tflib/worker/worker_test.py
+++ python/elasticdl/tflib/worker/worker_test.py
@@ -4,8 +4,8 @@ import queue
 from unittest.mock import patch
 from recordio.file_index import _ChunkData as C
 from recordio.file import File
-from python.elasticdl.tflib import ParameterServerClient, no_partition, ParameterServer, Worker
-from python.elasticdl.system.master import Master
+from elasticdl.tflib import ParameterServerClient, no_partition, ParameterServer, Worker
+from elasticdl.system.master import Master
 import tensorflow as tf
 import numpy as np
 
@@ -49,11 +49,11 @@ class MockMasterThread(threading.Thread):
 class Dummy(object):
     # This should be called first such that optimizer() and vars() would return valid values.
     def __init__(self):
-        self._opt = tf.train.GradientDescentOptimizer(0.1)
         self._vars = None
 
-    def optimizer(self):
-        return self._opt
+    @staticmethod
+    def optimizer():
+        return tf.train.GradientDescentOptimizer(0.1)
 
     @staticmethod
     def _create_model_var():
@@ -89,29 +89,41 @@ class Dummy(object):
     def loss(y_predict, y_true):
         return tf.reduce_mean(tf.square(y_true - y_predict))
 
+    @staticmethod
+    def data_process_py_func(data):
+        parsed = np.frombuffer(data, dtype="float32")
+        x = parsed[0]
+        y = parsed[1]
+        return x, y
 
-def dummy_create_dataset(
-    self, data_file, file_offset, shuffle_buffer_size=0, batch_size=1
+    # data_process_py_func output data type
+    data_py_output_type = [tf.float32, tf.float32]
+
+    # data_process_tf_func is optional. Add it to verify the functionality.
+    @staticmethod
+    def data_process_tf_func(x, y):
+        _x = tf.identity(x)
+        _y = tf.identity(y)
+        return x, y
+
+
+def dummy_create_recordio_dataset(
+    data_file, file_offset
 ):
     def gen():
         for i in range(200):
-            x = np.random.rand()
-            yield (x, 2 * x + 1)
+            x = np.float32(np.random.rand())
+            y = np.float32(2 * x + 1)
+            yield np.concatenate((x, y), axis=None).tobytes()
 
     dataset = tf.data.Dataset.from_generator(
-        gen, (tf.float32, tf.float32), (tf.TensorShape([]), tf.TensorShape([]))
+        gen, (tf.string), (tf.TensorShape([]))
     )
-
-    # shuffle and batch if needed
-    if shuffle_buffer_size:
-        dataset = dataset.shuffle(shuffle_buffer_size)
-    if batch_size > 1:
-        dataset = dataset.batch(batch_size)
 
     return dataset
 
 
-@patch.object(Worker, "_create_dataset", dummy_create_dataset)
+@patch.object(Worker, "_create_recordio_dataset", dummy_create_recordio_dataset)
 class WorkerTestCase(unittest.TestCase):
     def test(self):
         prog = Dummy()
@@ -132,9 +144,7 @@ class WorkerTestCase(unittest.TestCase):
             Worker(
                 ps_client=ps_client,
                 work_queue=m.register_worker(),
-                forward_func=prog.forward,
-                loss_func=prog.loss,
-                optimizer=prog.optimizer(),
+                umd=Dummy
             )
             for _ in range(worker_num)
         ]
