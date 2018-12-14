2d9ce17349159eb2c08f10121db61fecec177815
Li Zou <zou000@gmail.com>
2018-12-14 15:27:26 -0800

    * Fix a bug in docker entry script where group id is not present

--- dockerfile/dev.sh
+++ dockerfile/dev.sh
@@ -1,18 +1,20 @@
 #! /bin/bash
 set -x
-USER_ID=$(id -u)
 GRP_ID=$(id -g)
-USER_NAME=${USER}
+USER_ID=$(id -u)
 if [[ $USER_ID == 0 ]]; then
     USER_ID=${SUDO_UID}
     GRP_ID=${SUDO_GID}
-    USER_NAME=${SUDO_USER}
 fi
+
+GRP_NAME=$(getent group ${GRP_ID} | cut -d: -f1)
+USER_NAME=$(getent passwd ${USER_ID} | cut -d: -f1)
 HOME_DIR=$(eval echo ~${USER_NAME})
 
 docker run --rm -it --net=host \
     -v ${HOME_DIR}:/home/${USER_NAME}  \
     -e REAL_GID=${GRP_ID} \
+    -e REAL_GRP=${GRP_NAME} \
     -e REAL_UID=${USER_ID} \
     -e REAL_USER=${USER_NAME} \
     reg.docker.alibaba-inc.com/elasticdl/dev /bin/bash
