2daa3c1ea9e9958335f4c9960ddca2916ee096a3
Li Zou <zou000@gmail.com>
2018-12-19 22:32:32 -0800

    * Add script to build base image with pre-processed MNIST data

--- dockerfile/base/build.sh
+++ dockerfile/base/build.sh
@@ -2,6 +2,9 @@
 set -e
 set -x
 
-tmp_dir=$(mktemp -d /tmp)
-cp Dockerfile $tmp_dir
-
+tmp_dir=$(mktemp -d)
+cp Dockerfile ${tmp_dir}
+../../python/elasticdl/datasets/mnist/gen_data.py ${tmp_dir}/data
+docker build -t elasticdl/base -t reg.docker.alibaba-inc.com/elasticdl/base ${tmp_dir}
+echo "You may push the image to registry: docker push reg.docker.alibaba-inc.com/elasticdl/dev"
+  
