003d1893508ab83aff48432164a1d8590f6d8a57
Li Zou <zou000@gmail.com>
2018-12-19 22:36:03 -0800

    fix

--- dockerfile/base/build.sh
+++ dockerfile/base/build.sh
@@ -6,5 +6,4 @@ tmp_dir=$(mktemp -d)
 cp Dockerfile ${tmp_dir}
 ../../python/elasticdl/datasets/mnist/gen_data.py ${tmp_dir}/data
 docker build -t elasticdl/base -t reg.docker.alibaba-inc.com/elasticdl/base ${tmp_dir}
-echo "You may push the image to registry: docker push reg.docker.alibaba-inc.com/elasticdl/dev"
-  
+echo "You may push the image to registry: docker push reg.docker.alibaba-inc.com/elasticdl/base"
