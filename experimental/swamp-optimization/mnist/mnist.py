8c8e0e3088ca180c2fa52b3227d5dc4786fc5de8
yuyicg <chris.cheng.hz@gmail.com>
2018-12-20 12:21:58 +0000

    add pull prob

--- experimental/swamp-optimization/mnist/mnist.py
+++ experimental/swamp-optimization/mnist/mnist.py
@@ -13,6 +13,7 @@ import queue
 import time
 import gc
 from matplotlib import pyplot as plot
+import random
 
 
 class Net(nn.Module):
@@ -84,7 +85,7 @@ class Trainer(object):
                             self._up.put(pickle.dumps(
                                 {"model": model.state_dict(), "opt": optimizer.state_dict(), "loss": loss.data}))
                     else:
-                        if self._down != None:
+                        if self._down != None and random.random() > self._args.pull_probability:
                             m = pickle.loads(self._down.get())
                             model.load_state_dict(m["model"])
                             optimizer.load_state_dict(m["opt"])
@@ -219,6 +220,8 @@ def main():
     parser.add_argument('--loss-file', default='loss.png',
                         help='the name of loss figure file')
     parser.add_argument('--log-interval', type=int, default=50, metavar='N',
+                        help='how many batches to wait before logging training status')
+    parser.add_argument('--pull-probability', type=float, default=0, metavar='N',
                         help='how many batches to wait before logging training status')
     args = parser.parse_args()
 
