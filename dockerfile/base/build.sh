303098a049e51103de1568c0b5cad93a1b4be6e3
Li Zou <zou000@gmail.com>
2018-12-20 13:38:36 -0800

    * move dockerfile in to buils

--- dockerfile/base/build.sh
+++ dockerfile/base/build.sh
@@ -3,7 +3,25 @@ set -e
 set -x
 
 tmp_dir=$(mktemp -d)
-cp Dockerfile ${tmp_dir}
 ../../python/elasticdl/datasets/mnist/gen_data.py ${tmp_dir}/data
-docker build -t elasticdl/base -t reg.docker.alibaba-inc.com/elasticdl/base ${tmp_dir}
+
+docker build -t elasticdl/base -t reg.docker.alibaba-inc.com/elasticdl/base ${tmp_dir} -f- << EOF
+FROM tensorflow/tensorflow:1.12.0-py3
+
+# ElasticDL Code dependencies
+RUN apt-get update && apt-get -y --no-install-recommends install \
+    libsnappy-dev 
+
+RUN pip3 install \
+    python-snappy \
+    crc32c \
+    torch torchvision
+
+# Install pre-build RecordIO package
+RUN pip3 install -I https://github.com/ElasticDL/pyrecordio/raw/develop/artifacts/recordio-0.0.1-py3-none-any.whl
+
+# ElasticDL prebuilt datasets
+COPY data /data
+EOF
+
 echo "You may push the image to registry: docker push reg.docker.alibaba-inc.com/elasticdl/base"
