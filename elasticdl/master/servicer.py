5ec3118f9b1aae2a915a30b5621aa8ba557e865e
Li Zou <zou000@gmail.com>
2019-04-02 16:23:05 -0700

    * add get model and update gradient support

--- elasticdl/master/servicer.py
+++ elasticdl/master/servicer.py
@@ -1,17 +1,22 @@
+import threading
 
 from proto import master_pb2
 from proto import master_pb2_grpc
+from util.converter import NdarrayToTensor, TensorToNdarray
 
 
 class MasterServicer(master_pb2_grpc.MasterServicer):
     """Master service implementation"""
 
-    def __init__(self, logger):
+    def __init__(self, logger, grads_to_wait):
         self.logger = logger
+        self._lock = threading.Lock()
         # TODO: random initialization
         self._model = {}
         self._version = 0
         self._gradient_sum = {}
+        self._grad_to_wait = grads_to_wait
+        self._grad_n = 0
 
     def GetTask(self, request, context):
         # TODO: implent task queues. Return an empty task for now.
@@ -30,8 +35,10 @@ class MasterServicer(master_pb2_grpc.MasterServicer):
             raise ValueError(err_msg)
 
         res = master_pb2.Model()
-        res.version = self._version
-        # TODO: convert self._model to tensor.
+        with self._lock:
+            res.version = self._version
+            for k, v in self._model.items():
+                res.param[k].CopyFrom(NdarrayToTensor(v))
         return res
 
     def ReportTaskResult(self, request, context):
@@ -60,8 +67,33 @@ class MasterServicer(master_pb2_grpc.MasterServicer):
             return res
 
         # TODO: Update task queue with task_id
-        # TODO: accumulate gradient
-        # TODO: update model
+        with self._lock:
+            tmp = {}
+            # Do sanity check before accumulating gradients.
+            for k, v in request.gradient.items():
+                if k not in self._model:
+                    raise ValueError(
+                        "Gradient key: %s is not part of model", k
+                    )
+                arr = TensorToNdarray(v)
+                if arr.shape != self._model[k].shape:
+                    raise ValueError(
+                        "Gradient key: %s has incompatible dimension", k
+                    )
+                tmp[k] = arr
+
+            for k, v in tmp.items():
+                if k in self._gradient_sum:
+                    self._gradient_sum[k] = self._gradient_sum[k] + v
+                else:
+                    self._gradient_sum[k] = v
+
+            self._grad_n += 1
+            if self._grad_n >= self._grad_to_wait:
+                # TODO: update model
+                self._version += 1
+                self._gradient_sum.clear()
+                self._grad_n = 0
         res.accepted = True
         res.model_version = self._version
         return res
--- elasticdl/master/servicer_test.py
+++ elasticdl/master/servicer_test.py
@@ -1,15 +1,17 @@
 import logging
 import unittest
+import numpy as np
 
 from google.protobuf import empty_pb2
 
 from proto import master_pb2
+from util.converter import TensorToNdarray, NdarrayToTensor
 from .servicer import MasterServicer
 
 
 class ServicerTest(unittest.TestCase):
     def testGetTask(self):
-        master = MasterServicer(logging.getLogger())
+        master = MasterServicer(logging.getLogger(), 2)
 
         # No task yet, make sure the returned versions are as expected.
         task = master.GetTask(empty_pb2.Empty(), None)
@@ -22,49 +24,114 @@ class ServicerTest(unittest.TestCase):
         self.assertEqual(1, task.model_version)
 
     def testGetModel(self):
-        master = MasterServicer(logging.getLogger())
+        master = MasterServicer(logging.getLogger(), 2)
         req = master_pb2.GetModelRequest()
         req.min_version = 0
 
         # Get version 0
+        master._model["x"] = np.array([1.0, 1.0], dtype=np.float32)
         model = master.GetModel(req, None)
         self.assertEqual(0, model.version)
+        self.assertEqual(["x"], list(model.param.keys()))
+        np.testing.assert_array_equal(
+            np.array([1.0, 1.0]), TensorToNdarray(model.param["x"])
+        )
 
         # increase master's model version, now should get version 1
         master._version = 1
+        master._model["x"] = np.array([2.0, 2.0], dtype=np.float32)
+        master._model["y"] = np.array([12.0, 13.0], dtype=np.float32)
         model = master.GetModel(req, None)
         self.assertEqual(1, model.version)
+        self.assertEqual(["x", "y"], list(model.param.keys()))
+        np.testing.assert_array_equal(
+            np.array([2.0, 2.0]), TensorToNdarray(model.param["x"])
+        )
+        np.testing.assert_array_equal(
+            np.array([12.0, 13.0]), TensorToNdarray(model.param["y"])
+        )
 
         # try to get version 2, it should raise exception.
         req.min_version = 2
         self.assertRaises(ValueError, master.GetModel, req, None)
 
-
     def testReportTaskResult(self):
-        master = MasterServicer(logging.getLogger())
+        def makeGrad():
+            """ Make a ReportTaskResultRequest compatible with model"""
+            req = master_pb2.ReportTaskResultRequest()
+            req.gradient["x"].CopyFrom(
+                NdarrayToTensor(np.array([0.1], dtype=np.float32))
+            )
+            req.gradient["y"].CopyFrom(
+                NdarrayToTensor(np.array([0.1, 0.2], dtype=np.float32))
+            )
+            req.model_version = 1
+            return req
+
+        master = MasterServicer(logging.getLogger(), 3)
         master._version = 1
-        req = master_pb2.ReportTaskResultRequest()
+        master._model["x"] = np.array([2.0], dtype=np.float32)
+        master._model["y"] = np.array([12.0, 13.0], dtype=np.float32)
 
         # Report a future version, should raise exception
+        req = makeGrad()
         req.model_version = 2
         self.assertRaises(ValueError, master.ReportTaskResult, req, None)
 
         # Report an old version, should not be accepted
+        req = makeGrad()
         req.model_version = 0
         res = master.ReportTaskResult(req, None)
         self.assertFalse(res.accepted)
         self.assertEqual(1, res.model_version)
 
         # Report a current version, but with error, should not be accepted.
-        req.model_version = 1
+        req = makeGrad()
         req.err_message = "worker error"
         res = master.ReportTaskResult(req, None)
         self.assertFalse(res.accepted)
         self.assertEqual(1, res.model_version)
 
+        # Report a unknown gradient, should raise.
+        req = makeGrad()
+        req.gradient["z"].CopyFrom(
+            NdarrayToTensor(np.array([0.1], dtype=np.float32))
+        )
+        self.assertRaises(ValueError, master.ReportTaskResult, req, None)
+
+        # Report an incompatible gradient, should raise.
+        req = makeGrad()
+        req.gradient["y"].CopyFrom(
+            NdarrayToTensor(np.array([0.1], dtype=np.float32))
+        )
+        self.assertRaises(ValueError, master.ReportTaskResult, req, None)
+
         # Report a current version without error, should be accepted.
-        req.model_version = 1
-        req.err_message = ""
+        req = makeGrad()
         res = master.ReportTaskResult(req, None)
         self.assertTrue(res.accepted)
         self.assertEqual(1, res.model_version)
+
+        # Report a current version with part of gradients, should be accepted.
+        req = makeGrad()
+        del req.gradient["y"]
+        res = master.ReportTaskResult(req, None)
+        self.assertTrue(res.accepted)
+        self.assertEqual(1, res.model_version)
+        # Gradient should be accumulated.
+        np.testing.assert_array_equal(
+            np.array([0.2], dtype=np.float32), master._gradient_sum["x"]
+        )
+        np.testing.assert_array_equal(
+            np.array([0.1, 0.2], dtype=np.float32), master._gradient_sum["y"]
+        )
+        self.assertEqual(2, master._grad_n)
+
+        # Report a current version without error, should be accepted, and a new
+        # version created
+        req = makeGrad()
+        res = master.ReportTaskResult(req, None)
+        self.assertTrue(res.accepted)
+        self.assertEqual(2, res.model_version)
+        self.assertFalse(master._gradient_sum)
+        self.assertEqual(0, master._grad_n)
