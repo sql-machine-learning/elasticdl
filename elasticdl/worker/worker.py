c2baddb303c4ba6a0d898d828a69dbf1e16c6d47
haitao <zht108229@alipay.com>
2019-04-10 20:53:02 +0800

    worker distributed training

--- elasticdl/worker/worker.py
+++ elasticdl/worker/worker.py
@@ -1,9 +1,9 @@
+from proto import master_pb2_grpc
+from proto import master_pb2
+import recordio
+from tensorflow.python.ops import math_ops
 import tensorflow as tf
 assert tf.executing_eagerly()
-
-from tensorflow.python.ops import math_ops
-from proto import master_pb2
-from proto import master_pb2_grpc
 
 
 class Worker(object):
@@ -37,21 +37,61 @@ class Worker(object):
         # TODO: get task from master
         pass
 
-    def get_model(self):
-        # TODO: get model from master
+    def get_model(self, min_version):
+        # TODO: get model from master, and update model_version
         pass
 
-    def report_task_result(self):
+    def report_task_result(self, task_id, err_msg):
         # TODO: report task result to master
         pass
 
-    def report_gradient(self):
+    def report_gradient(self, grads, variables):
         # TODO: report gradient to ps
         pass
 
     def distributed_train(self):
-        # TODO: distributed training
-        pass
+        """
+        Distributed training.
+        """
+        while True:
+            task = self.get_task()
+            if task.shard_file_name is '':
+                # No more task
+                break
+            batch_size = task.minibatch_size
+            task_data_size = task.end - task.start + 1
+            record_buf = []
+            err_msg = ''
+            record_count = 0
+            try:
+                with recordio.File(task.shard_file_name, 'r') as rdio_r:
+                    for record in rdio_r.get_reader(task.start, task.end + 1):
+                        record_count += 1
+                        record_buf.append(record)
+                        if len(record_buf) == batch_size or record_count == task_data_size:
+                            # TODO: optimize the logic to avoid unnecessary get_model call.
+                            self.get_model(
+                                max(self._model_version, task.model_version))
+
+                            batch_input_data = self._input_fn(record_buf)
+
+                            with tf.GradientTape() as tape:
+                                output = self._model_cls.output(
+                                    batch_input_data)
+                                loss = self._model_cls.loss(
+                                    output, batch_input_data)
+                                # TODO:  Add regularization loss if any,
+                                #        which should be divided by the number of contributing workers.
+                            grads = tape.gradient(
+                                loss, self._keras_model.variables)
+                            print('Loss is ', loss.numpy())
+
+                            self.report_gradient(
+                                grads, self._keras_model.variables)
+                            record_buf = []
+            except Exception as ex:
+                err_msg = str(ex)
+            self.report_task_result(task.task_id, err_msg)
 
     def local_train(self, batch_size, epoch=1, kwargs=None):
         """
