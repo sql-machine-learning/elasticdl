24077dc7c9f73429fa4d2deb551162a34fe6fb32
haitao <zht108229@alipay.com>
2019-04-17 20:38:52 +0800

    add mnist example

--- elasticdl/common/model_helper.py
+++ elasticdl/common/model_helper.py
@@ -1,22 +1,14 @@
 # common helper methods for model manipulation.
+import importlib.util
+import sys
 import os
-from contextlib import contextmanager
+
 
 def load_user_model(model_file, model_class):
-    with _add_to_path(os.path.dirname(absolute_path)):
-        spec = importlib.util.spec_from_file_location(absolute_path, absolute_path)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        return getattr(module, model_class)
-
-@contextmanager
-def _add_to_path(p):
-    import sys
-
-    old_path = sys.path
-    sys.path = sys.path[:]
-    sys.path.insert(0, p)
-    try:
-        yield
-    finally:
-        sys.path = old_path
+    print('-------')
+    print(model_file)
+    print(model_class)
+    spec = importlib.util.spec_from_file_location(model_file, model_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+    return getattr(module, model_class)
