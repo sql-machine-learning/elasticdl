4f38b40ebd092817bc881dc5b5eb0c493a397be3
虞翊 <steven.cheng@alibaba-inc.com>
2019-05-15 18:51:17 +0800

    always call build_model

--- elasticdl/master/main.py
+++ elasticdl/master/main.py
@@ -80,8 +80,7 @@ def main():
     )
     model_module = load_user_model(args.model_file)
     model_inst = model_module.model
-    if not (type(model_inst) == tf.keras.Model):
-        build_model(model_inst, model_module.feature_columns())
+    build_model(model_inst, model_module.feature_columns())
     optimizer = model_module.optimizer()
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=64))
