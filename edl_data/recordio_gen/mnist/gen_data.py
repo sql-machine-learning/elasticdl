41a41f52f2827fb3b46f4eda73b25975a676815d
Wei Yan <ywskycn@gmail.com>
2019-05-20 17:38:40 +0800

    Make sure args using the same string format

--- edl_data/recordio_gen/mnist/gen_data.py
+++ edl_data/recordio_gen/mnist/gen_data.py
@@ -46,19 +46,19 @@ def main(argv):
     )
     parser.add_argument("dir", help="Output directory")
     parser.add_argument(
-        "--num-record-per-chunk",
+        "--num_record_per_chunk",
         default=1024,
         type=int,
         help="Approximate number of records in a chunk.",
     )
     parser.add_argument(
-        "--num-chunk",
+        "--num_chunk",
         default=16,
         type=int,
         help="Number of chunks in a RecordIO file",
     )
     parser.add_argument(
-        "--codec-type",
+        "--codec_type",
         default=None,
         help="Type of codec(tf_example or None)",
     )
