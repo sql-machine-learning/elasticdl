44b2b567e9b588556a1d22068f5e99c16b553041
Li Zou <zou000@gmail.com>
2019-05-21 21:33:45 +0800

    Change encode to take a dict

--- edl_data/codec/bytes_codec_test.py
+++ edl_data/codec/bytes_codec_test.py
@@ -17,9 +17,9 @@ class TestBytesCodec(unittest.TestCase):
             tf.feature_column.numeric_column(key="label",
             dtype=tf.int64, shape=[1])]
 
-        example_1 = [("f0", np.array([100.1])), ("label", np.array([1]))]
-        example_2 = [("f0", np.array([200.1])), ("label", np.array([2]))]
-        example_3 = [("f0", np.array([300.1])), ("label", np.array([3]))]
+        example_1 = {"f0": np.array([100.1]), "label": np.array([1])}
+        example_2 = {"f0": np.array([200.1]), "label": np.array([2])}
+        example_3 = {"f0": np.array([300.1]), "label": np.array([3])}
         examples = [example_1, example_2, example_3]
 
         tmp_file = tempfile.NamedTemporaryFile(delete=False)
@@ -37,9 +37,8 @@ class TestBytesCodec(unittest.TestCase):
             for idx in range(coded_r.count()):
                 exp = coded_r.get(idx)
                 expected_exp = examples[idx]
-                f_0, label = exp["f0"], exp["label"]
-                self.assertEqual(f_0, expected_exp[0][1])
-                self.assertEqual(label, expected_exp[1][1])
+                self.assertEqual(exp["f0"], expected_exp["f0"])
+                self.assertEqual(exp["label"], expected_exp["label"])
 
         os.remove(tmp_file.name)
 
