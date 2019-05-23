f6a932b7e64f587af4c006412ca84b6aef49add5
Li Zou <zou000@gmail.com>
2019-05-23 13:07:21 +0800

    * add more tests to BytesCodec

--- data/codec/bytes_codec_test.py
+++ data/codec/bytes_codec_test.py
@@ -11,16 +11,16 @@ class TestBytesCodec(unittest.TestCase):
     def test_encode_and_decode(self):
         feature_columns = [
             tf.feature_column.numeric_column(
-                key="f0", dtype=tf.float64, shape=[1]
+                key="f0", dtype=tf.float64, shape=[3, 4, 5]
             ),
             tf.feature_column.numeric_column(
                 key="label", dtype=tf.int64, shape=[1]
             ),
         ]
 
-        example_1 = {"f0": np.array([100.1]), "label": np.array([1])}
-        example_2 = {"f0": np.array([200.1]), "label": np.array([2])}
-        example_3 = {"f0": np.array([300.1]), "label": np.array([3])}
+        example_1 = {"f0": np.random.rand(3, 4, 5), "label": np.array([1])}
+        example_2 = {"f0": np.random.rand(3, 4, 5), "label": np.array([2])}
+        example_3 = {"f0": np.random.rand(3, 4, 5), "label": np.array([3])}
         examples = [example_1, example_2, example_3]
 
         # Create the codec for tf.train.Exampel data.
@@ -33,8 +33,8 @@ class TestBytesCodec(unittest.TestCase):
         for idx, e in enumerate(encoded):
             exp = codec.decode(e)
             expected_exp = examples[idx]
-            self.assertEqual(exp["f0"], expected_exp["f0"])
-            self.assertEqual(exp["label"], expected_exp["label"])
+            np.testing.assert_array_equal(exp["f0"], expected_exp["f0"])
+            np.testing.assert_array_equal(exp["label"], expected_exp["label"])
 
 
 if __name__ == "__main__":
