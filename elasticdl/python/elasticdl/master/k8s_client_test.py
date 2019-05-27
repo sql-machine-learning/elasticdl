f4f5a9fc7a195fa099872cdc33ef306ed2d747a4
Li Zou <zou000@gmail.com>
2019-05-27 16:29:48 +0800

    wip

---
+++ elasticdl/python/elasticdl/master/k8s_client_test.py
@@ -0,0 +1,49 @@
+import unittest
+
+import os
+import time
+
+from elasticdl.master import k8s
+
+class WorkerTracker(object):
+    def __init__(self):
+        self._count = 0
+
+    def event_cb(self, event):
+        print("----- %s -----\n" % event["type"])
+        if event["type"] == "ADDED":
+            self._count += 1
+        elif event["type"] == "DELETED":
+            self._count -= 1
+
+
+class K8sClientTest(unittest.TestCase):
+    def test_client(self):
+        tracker = WorkerTracker()
+
+        c = k8s.Client(
+            worker_image="gcr.io/google-samples/hello-app:1.0",
+            namespace="default",
+            job_name="training-job",
+            event_callback=tracker.event_cb,
+        )
+
+        # Start 3 workers
+        for i in range(3):
+            c.create_worker("worker-%d" % i, "500m", "500m", "64Mi", "64Mi")
+            time.sleep(5)
+
+        # wait for workers to be added
+        while tracker._count < 3:
+            time.sleep(1)
+        
+        # delete all workers
+        for i in range(tracker._count): 
+            c.delete_worker("worker-%d" % i)
+
+        # wait for workers to be deleted
+        while tracker._count > 0:
+            time.sleep(1)
+
+if __name__ == "__main__":
+    unittest.main()
