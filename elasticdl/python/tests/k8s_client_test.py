50cde818015ea5818e70a6e98e2a2fcf7d83220a
Li Zou <zou000@gmail.com>
2019-05-31 14:44:55 +0800

    Use 'echo' command

--- elasticdl/python/tests/k8s_client_test.py
+++ elasticdl/python/tests/k8s_client_test.py
@@ -19,7 +19,11 @@ class WorkerTracker(object):
             self._count -= 1
 
 
-@unittest.skipIf(os.environ.get('K8S_TESTS', 'True') == 'False', 'No Kubernetes cluster available')
+@unittest.skipIf(
+    os.environ.get(
+        'K8S_TESTS',
+        'True') == 'False',
+    'No Kubernetes cluster available')
 class K8sClientTest(unittest.TestCase):
     def test_client(self):
         tracker = WorkerTracker()
@@ -27,22 +31,28 @@ class K8sClientTest(unittest.TestCase):
         c = k8s.Client(
             worker_image="gcr.io/google-samples/hello-app:1.0",
             namespace="default",
-            job_name="test-job-%d-%d" % (int(time.time()), random.randint(1, 101)),
+            job_name="test-job-%d-%d" %
+            (int(
+                time.time()),
+                random.randint(
+                1,
+                101)),
             event_callback=tracker.event_cb,
         )
 
         # Start 3 workers
         resource = {"cpu": "100m", "memory": "64M"}
         for i in range(3):
-            _ = c.create_worker("worker-%d" % i, resource, resource)
+            _ = c.create_worker("worker-%d" %
+                                i, resource, resource, command=["echo"])
             time.sleep(5)
 
         # wait for workers to be added
         while tracker._count < 3:
             time.sleep(1)
-        
+
         # delete all workers
-        for i in range(tracker._count): 
+        for i in range(tracker._count):
             c.delete_worker("worker-%d" % i)
 
         # wait for workers to be deleted
