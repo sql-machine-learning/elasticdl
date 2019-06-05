0b7d75fd5073802f33e192244283b86ccf2684e0
Yuan Tang <terrytangyuan@gmail.com>
2019-06-05 13:30:56 -0400

    Add missing eval_metrics_fn in examples (#564)
    * Add missing eval_metrics_fn in examples
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Update model_building.md

--- elasticdl/python/examples/mnist_functional_api.py
+++ elasticdl/python/examples/mnist_functional_api.py
@@ -51,3 +51,11 @@ def input_fn(records):
     images = np.reshape(images, (batch_size, 28, 28))
     labels = np.array(label_list)
     return ({'image': images}, labels)
+
+
+def eval_metrics_fn(predictions, labels):
+    return {
+        'metric': tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                logits=predictions, labels=labels.flatten())),
+    }
