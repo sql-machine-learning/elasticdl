42a6c65d12a2ba063cb0aa88ebb67c3996937594
yuyicg <chris.cheng.hz@gmail.com>
2019-06-13 20:29:28 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ElasticDL",
+    version='0.0.1',
+    description="A Kubernetes Naitve Elastic Deep Learning Training Framework",
+    author="Ant",
+    url="https://github.com/wangkuiyi/elasticdl",
+    packages=find_packages(exclude=['*test*']),
+    package_data={'': ['proto/elasticdl.proto', 'docker/*']},
+)
