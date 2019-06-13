afc80bc219fad4e2dc870bbdb6a35d22b1b866d7
Yuan Tang <terrytangyuan@gmail.com>
2019-06-13 14:38:34 -0400

    Add k8s resource string parsing utility (#662)
    * Add k8s utils for resource parsing
    
    * More k8s native parsing
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Add docstring
    
    * More test cases
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * More tests and exceptions
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Removed lower() call
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Address comments
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

---
+++ elasticdl/python/elasticdl/common/k8s_utils.py
@@ -0,0 +1,73 @@
+import re
+
+
+_ALLOWED_RESOURCE_TYPES = ["memory", "disk", "ephemeral-storage", "cpu", "gpu"]
+
+
+def _is_numeric(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
+
+def _valid_gpu_spec(gpu_str):
+    if not gpu_str.isnumeric():
+        raise ValueError(
+            "invalid gpu request spec: " + gpu_str
+        )
+    return gpu_str
+
+
+def _valid_cpu_spec(cpu_str):
+    regexp = re.compile("([1-9]{1})([0-9]*)m$")
+    if not regexp.match(cpu_str) and not _is_numeric(cpu_str):
+        raise ValueError(
+            "invalid cpu request spec: " + cpu_str
+        )
+    return cpu_str
+
+
+def _valid_mem_spec(mem_str):
+    regexp = re.compile("([1-9]{1})([0-9]*)(E|P|T|G|M|K|Ei|Pi|Ti|Gi|Mi|Ki)$")
+    if not regexp.match(mem_str):
+        raise ValueError("invalid memory request spec: " + mem_str)
+    return mem_str
+
+
+def parse_resource(resource_str):
+    """Parse combined k8s resource string into a dict.
+
+    Args:
+        resource_str: The string representation for k8s resource,
+            e.g. "cpu=250m,memory=32Mi,disk=64Mi,gpu=1,ephemeral-storage=32Mi".
+
+    Return:
+        A Python dictionary parsed from the given resource string.
+    """
+    kvs = resource_str.split(",")
+    resource_names = []
+    parsed_res_dict = {}
+    for kv in kvs:
+        k, v = kv.split("=")
+        if k not in resource_names:
+            resource_names.append(k)
+        else:
+            raise ValueError(
+                "The resource string contains duplicate resource names: %s" % k
+            )
+        if k in ["memory", "disk", "ephemeral-storage"]:
+            _valid_mem_spec(v)
+        elif k == "cpu":
+            _valid_cpu_spec(v)
+        elif k == "gpu":
+            _valid_gpu_spec(v)
+        else:
+            raise ValueError(
+                "%s is not in the allowed list of resource types: %s" % (
+                    k, _ALLOWED_RESOURCE_TYPES
+                )
+            )
+        parsed_res_dict[k] = v
+    return parsed_res_dict
