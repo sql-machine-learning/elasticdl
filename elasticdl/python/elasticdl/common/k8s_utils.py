cb68f609b614d5f5cf1cff28fcc3b4349b3f7ffb
terrytangyuan <terrytangyuan@gmail.com>
2019-06-13 09:58:46 -0400

    More k8s native parsing
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/elasticdl/common/k8s_utils.py
+++ elasticdl/python/elasticdl/common/k8s_utils.py
@@ -1,55 +1,18 @@
 import re
 
 
-memory_multipliers = {
-    "k": 1000,
-    "M": 1000 ** 2,
-    "G": 1000 ** 3,
-    "T": 1000 ** 4,
-    "P": 1000 ** 5,
-    "E": 1000 ** 6,
-    "Ki": 1024,
-    "Mi": 1024 ** 2,
-    "Gi": 1024 ** 3,
-    "Ti": 1024 ** 4,
-    "Pi": 1024 ** 5,
-    "Ei": 1024 ** 6,
-}
+def _valid_cpu_spec(cpu_str):
+    regexp = re.compile("([1-9]{1})([0-9]*)m$")
+    if not regexp.match(cpu_str):
+        raise ValueError("invalid cpu request spec: " + cpu_str)
+    return cpu_str
 
 
-def _parse_cpu(cpu_str):
-    """Parse k8s cpu resource.
-
-    Args:
-        cpu_str: The string representation of the CPU resource.
-
-    Return:
-        CPU count in float.
-    """
-    m = re.compile("([0-9]+)m").match(cpu_str)
-    if m:
-        return float(m.group(1)) / 1000
-    else:
-        return float(cpu_str)
-
-
-def _parse_memory(mem_str):
-    """Parse k8s memory resource into bytes.
-
-    Args:
-        mem_str: The string representation of the memory resource.
-
-    Return:
-        Memory in bytes.
-    """
-    m = re.compile("([0-9]+)([A-Za-z]{1,2})").match(mem_str)
-    if m:
-        return int(m.group(1)) * memory_multipliers[m.group(2)]
-    elif mem_str == "0":
-        # Special case
-        return 0
-    else:
-        print("Error: unable to parse the memory string '%s'" % mem_str)
+def _valid_mem_spec(mem_str):
+    regexp = re.compile("([1-9]{1})([0-9]*)(E|P|T|G|M|K|Ei|Pi|Ti|Gi|Mi|Ki)$")
+    if not regexp.match(mem_str):
+        raise ValueError("invalid memory request spec: " + mem_str)
+    return mem_str
 
 
 def parse_resource(resource_str):
@@ -58,8 +21,8 @@ def parse_resource(resource_str):
     for kv in kvs:
         k, v = kv.split('=')
         if k in ['gpu', 'memory', 'disk']:
-            v = int(v)
+            _valid_mem_spec(v)
         elif k == 'cpu':
-            v = float(v)
+            _valid_cpu_spec(v)
         parsed_res_dict[k.lower()] = v
     return parsed_res_dict
--- elasticdl/python/tests/k8s_utils_test.py
+++ elasticdl/python/tests/k8s_utils_test.py
@@ -1,61 +1,31 @@
 import unittest
 
-from elasticdl.python.elasticdl.common.k8s_utils import (
-    _parse_cpu,
-    _parse_memory,
-    parse_resource
-)
+from elasticdl.python.elasticdl.common.k8s_utils import parse_resource
 
 
 class K8SUtilsTest(unittest.TestCase):
-    def test_parse_cpu(self):
-        self.assertAlmostEqual(0.001, _parse_cpu('1m'))
-        self.assertAlmostEqual(0.01, _parse_cpu('10m'))
-        self.assertAlmostEqual(0.1, _parse_cpu('100m'))
-        self.assertAlmostEqual(1, _parse_cpu('1000m'))
-        self.assertAlmostEqual(10, _parse_cpu('10000m'))
-        self.assertAlmostEqual(0.001, _parse_cpu('0.001'))
-        self.assertAlmostEqual(0.01, _parse_cpu('0.01'))
-        self.assertAlmostEqual(0.1, _parse_cpu('0.1'))
-        self.assertAlmostEqual(1, _parse_cpu('1'))
-        self.assertAlmostEqual(10, _parse_cpu('10'))
-
-    def test_parse_memory(self):
-        self.assertEqual(0, _parse_memory('0'))
-        self.assertEqual(1000, _parse_memory('1k'))
-        self.assertEqual(1000 ** 2, _parse_memory('1M'))
-        self.assertEqual(1000 ** 3, _parse_memory('1G'))
-        self.assertEqual(1000 ** 4, _parse_memory('1T'))
-        self.assertEqual(1000 ** 5, _parse_memory('1P'))
-        self.assertEqual(1000 ** 6, _parse_memory('1E'))
-        self.assertEqual(1024, _parse_memory('1Ki'))
-        self.assertEqual(1024 ** 2, _parse_memory('1Mi'))
-        self.assertEqual(1024 ** 3, _parse_memory('1Gi'))
-        self.assertEqual(1024 ** 4, _parse_memory('1Ti'))
-        self.assertEqual(1024 ** 5, _parse_memory('1Pi'))
-        self.assertEqual(1024 ** 6, _parse_memory('1Ei'))
-        self.assertEqual(15 * 1000, _parse_memory('15k'))
-        self.assertEqual(20 * (1000 ** 2), _parse_memory('20M'))
-        self.assertEqual(30 * (1000 ** 3), _parse_memory('30G'))
-        self.assertEqual(40 * (1000 ** 4), _parse_memory('40T'))
-        self.assertEqual(50 * (1000 ** 5), _parse_memory('50P'))
-        self.assertEqual(60 * (1000 ** 6), _parse_memory('60E'))
-        self.assertEqual(23 * 1024, _parse_memory('23Ki'))
-        self.assertEqual(34 * (1024 ** 2), _parse_memory('34Mi'))
-        self.assertEqual(45 * (1024 ** 3), _parse_memory('45Gi'))
-        self.assertEqual(56 * (1024 ** 4), _parse_memory('56Ti'))
-        self.assertEqual(67 * (1024 ** 5), _parse_memory('67Pi'))
-        self.assertEqual(78 * (1024 ** 6), _parse_memory('78Ei'))
 
     def test_parse_resource(self):
         self.assertEqual(
             {
-                'cpu': 1.0,
-                'memory': 1024,
-                'disk': 20480,
-                'gpu': 1,
+                'cpu': '250m',
+                'memory': '32Mi',
+                'disk': '64Mi',
+                'gpu': '128Mi',
             },
             parse_resource(
-                'cpu=1,memory=1024,disk=20480,gpu=1'
+                'cpu=250m,memory=32Mi,disk=64Mi,gpu=128Mi'
             )
+        )
+        self.assertRaisesRegex(
+            ValueError,
+            'invalid cpu request spec:',
+            parse_resource,
+            'cpu=250blah,memory=32Mi,disk=64Mi,gpu=128Mi'
+        )
+        self.assertRaisesRegex(
+            ValueError,
+            'invalid memory request spec:',
+            parse_resource,
+            'cpu=250m,memory=32blah,disk=64Mi,gpu=128Mi'
         )
