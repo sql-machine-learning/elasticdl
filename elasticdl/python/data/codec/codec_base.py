3cccf3ab8549e446b063f5f311711718a5a88252
Jialu Zhu <jialu.zhu@antfin.com>
2019-06-17 15:24:53 -0700

    fix

--- elasticdl/python/data/codec/codec_base.py
+++ elasticdl/python/data/codec/codec_base.py
@@ -4,7 +4,7 @@ class Codec(object):
 
     def encode(self, data):
         """
-        Take a {feature_name: ndarray} object and return the encoded data 
+        Take a {feature_name: ndarray} object and return the encoded data
         TODO: This should be changed totake a {feature_name: example}
               once we implement https://github.com/wangkuiyi/elasticdl/pull/675
         """
@@ -12,7 +12,7 @@ class Codec(object):
 
     def decode(self, data):
         """
-        Take a sample of raw data and return a {feature_name: Tensor} 
+        Take a sample of raw data and return a {feature_name: Tensor}
         dictionary
         """
         raise("decode function is not implemented!")
