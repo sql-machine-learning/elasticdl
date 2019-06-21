c838e705229305cfae70aa62d057b5db23f41208
Yi Wang <yi.wang.2005@gmail.com>
2019-06-20 22:23:10 -0700

    Optionally convert Fashion MNIST dataset (#739)

--- elasticdl/python/data/recordio_gen/mnist/gen_data.py
+++ elasticdl/python/data/recordio_gen/mnist/gen_data.py
@@ -39,6 +39,11 @@ def main(argv):
         default="elasticdl/python/data/codec/tf_example_codec.py",
         help="Codec file name",
     )
+    parser.add_argument(
+        "--fashion_mnist",
+        action='store_true',
+        help="If convert the Fashion MNIST dataset",
+    )
     args = parser.parse_args(argv)
 
     records_per_file = args.num_record_per_chunk * args.num_chunk
@@ -76,23 +81,24 @@ def main(argv):
         codec=codec_module.codec,
     )
 
-    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-    convert_numpy_to_recordio(
-        args.dir + "/fashion/train",
-        x_train,
-        y_train,
-        feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
-    convert_numpy_to_recordio(
-        args.dir + "/fashion/test",
-        x_test,
-        y_test,
-        feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
+    if args.fashion_mnist:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+        convert_numpy_to_recordio(
+            args.dir + "/fashion/train",
+            x_train,
+            y_train,
+            feature_columns,
+            records_per_file=records_per_file,
+            codec=codec_module.codec,
+        )
+        convert_numpy_to_recordio(
+            args.dir + "/fashion/test",
+            x_test,
+            y_test,
+            feature_columns,
+            records_per_file=records_per_file,
+            codec=codec_module.codec,
+        )
 
 
 if __name__ == "__main__":
