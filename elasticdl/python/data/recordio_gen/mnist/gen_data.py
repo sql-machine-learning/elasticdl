2aeddf7027deecc6d3b66b6440a553ba55538536
Jialu Zhu <jialu.zhu@antfin.com>
2019-06-21 13:54:31 -0700

    rebase

--- elasticdl/python/data/recordio_gen/mnist/gen_data.py
+++ elasticdl/python/data/recordio_gen/mnist/gen_data.py
@@ -10,9 +10,31 @@ import tensorflow as tf
 from tensorflow.python.keras.datasets import mnist, fashion_mnist
 
 from elasticdl.python.data.recordio_gen.convert_numpy_to_recordio import (
-    convert_numpy_to_recordio,
+    convert_examples_to_recordio,
 )
 from elasticdl.python.elasticdl.common.model_helper import load_module
+
+
+def numpy_to_examples(x, y):
+    train_example_list = []
+    assert len(x) == len(y)
+    for i in range(len(x)):
+        numpy_image = x[i]
+        label = y[i]
+        feature_name_to_feature = {}
+        feature_name_to_feature['image'] = tf.train.Feature(
+            float_list=tf.train.FloatList(
+                value=numpy_image.astype(tf.float32.as_numpy_dtype).flatten(),
+            ),
+        )
+        feature_name_to_feature['label'] = tf.train.Feature(
+            int64_list=tf.train.Int64List(value=[label]),
+        )
+        example = tf.train.Example(
+            features=tf.train.Features(feature=feature_name_to_feature),
+        )
+        train_example_list.append(example)
+    return train_example_list
 
 
 def main(argv):
@@ -58,48 +80,49 @@ def main(argv):
     # Initilize codec
     codec_module = load_module(args.codec_file)
 
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    n = max(0.0, min(1.0, args.mnist_fraction))
+    if n > 0:
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-    train_example_list = numpy_to_examples(x_train, y_train)
-    convert_examples_to_recordio(
-        args.dir + "/mnist/train",
-        example,
-        # x_train,
-        # y_train,
-        # feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
+        n = round(x_train.shape[0] * n)
+        train_example_list = numpy_to_examples(x_train[:n], y_train[:n])
+        convert_examples_to_recordio(
+            args.dir + "/mnist/train",
+            train_example_list,
+            records_per_file=records_per_file,
+            encode_fn=codec_module.codec.encode,
+        )
 
-    convert_numpy_to_recordio(
-        args.dir + "/mnist/test",
-        example,
-        # x_test,
-        # y_test,
-        # feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
+        n = round(x_train.shape[0] * n)
+        test_example_list = numpy_to_examples(x_test[:n], y_test[:n])
+        convert_examples_to_recordio(
+            args.dir + "/mnist/test",
+            test_example_list,
+            records_per_file=records_per_file,
+            encode_fn=codec_module.codec.encode,
+        )
 
-    (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-    convert_numpy_to_recordio(
-        args.dir + "/fashion/train",
-        example,
-        # x_train,
-        # y_train,
-        # feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
-    convert_numpy_to_recordio(
-        args.dir + "/fashion/test",
-        example,
-        # x_test,
-        # y_test,
-        # feature_columns,
-        records_per_file=records_per_file,
-        codec=codec_module.codec,
-    )
+    n = max(0.0, min(1.0, args.fashion_mnist_fraction))
+    if n > 0:
+        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
+
+        n = round(x_train.shape[0] * n)
+        train_example_list = numpy_to_examples(x_train[:n], y_train[:n])
+        convert_examples_to_recordio(
+            args.dir + "/fashion/train",
+            train_example_list,
+            records_per_file=records_per_file,
+            encode_fn=codec_module.codec.encode,
+        )
+
+        n = round(x_train.shape[0] * n)
+        test_example_list = numpy_to_examples(x_test[:n], y_test[:n])
+        convert_examples_to_recordio(
+            args.dir + "/fashion/test",
+            test_example_list,
+            records_per_file=records_per_file,
+            encode_fn=codec_module.codec.encode,
+        )
 
 
 if __name__ == "__main__":
