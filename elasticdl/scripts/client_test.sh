57ada74110321b352d120fdd4b07b5373aa3e39d
Li Zou <zou000@gmail.com>
2019-06-28 11:14:38 -0700

    Fix

--- elasticdl/scripts/client_test.sh
+++ elasticdl/scripts/client_test.sh
@@ -1,18 +1,18 @@
 set -x
 python -m elasticdl.python.client.client train \
   --image_base=elasticdl:ci \
-  --model_file=$(pwd)/elasticdl/python/examples/mnist_subclass.py \
+  --model_file=$(pwd)/elasticdl/python/examples/mnist_functional_api.py \
   --training_data_dir=/data/mnist/train \
   --evaluation_data_dir=/data/mnist/test \
-  --num_epochs=1 \
+  --num_epochs=2 \
   --master_resource_request="cpu=400m,memory=1024Mi" \
   --master_resource_limit="cpu=1,memory=2048Mi" \
   --worker_resource_request="cpu=400m,memory=2048Mi" \
   --worker_resource_limit="cpu=1,memory=3072Mi" \
-  --minibatch_size=10 \
+  --minibatch_size=64 \
   --records_per_task=100 \
   --num_workers=2 \
-  --checkpoint_steps=20 \
+  --checkpoint_steps=10 \
   --grads_to_wait=2 \
   --job_name=test-mnist \
   --image_name=elasticdl:dev \
