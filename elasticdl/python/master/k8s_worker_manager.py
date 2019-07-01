2ba7bb3ec919238bbcbe2adfbb287e5e34def444
terrytangyuan <terrytangyuan@gmail.com>
2019-07-01 13:21:58 -0400

    Fix precommit
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/master/k8s_worker_manager.py
+++ elasticdl/python/master/k8s_worker_manager.py
@@ -4,7 +4,6 @@ import threading
 
 from collections import Counter
 from elasticdl.python.common import k8s_client as k8s
-from elasticdl.python.common.k8s_resource import parse
 from elasticdl.python.common.k8s_tensorboard_client import TensorBoardClient
 
 
