516b8a136ae6d5b12fbdc94cf8843ae38bdea1d4
terrytangyuan <terrytangyuan@gmail.com>
2019-07-02 08:55:49 -0400

    Use requirements.txt to populate install_requires field
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="ElasticDL",
     version="0.0.1",
     description="A Kubernetes-naitve Elastic Deep Learning Framework",
     author="Ant Financial",
     url="https://github.com/wangkuiyi/elasticdl",
+    install_requires=requirements,
     packages=find_packages(exclude=["*test*"]),
     package_data={"": ["proto/elasticdl.proto", "docker/*", "Makefile"]},
     entry_points={
