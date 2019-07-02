eadb50794c0b0b1d1bdc6acb5e9bd817e49df184
terrytangyuan <terrytangyuan@gmail.com>
2019-07-02 08:58:29 -0400

    Lower case package name
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="ElasticDL",
+    name="elasticdl",
     version="0.0.1",
     description="A Kubernetes-naitve Elastic Deep Learning Framework",
     author="Ant Financial",
