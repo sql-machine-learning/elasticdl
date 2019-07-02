50074bf13cf5e88df1d151361b6f21fa5233466e
Jialu Zhu <jialu.zhu@antfin.com>
2019-07-02 15:37:20 -0700

    feedback

--- elasticdl/python/examples/imagenet_resnet50/imagenet_resnet50.py
+++ elasticdl/python/examples/imagenet_resnet50/imagenet_resnet50.py
@@ -4,6 +4,10 @@ import tensorflow as tf
 def prepare_data_for_a_single_file(file_object, filename):
     """
     :param filename: training data file name
+    The raw images should be packed into a TAR file and named as
+    <label_id>_xxx.JPEG, in which label_id is an integer representing
+    the category the image belongs to, and xxx can be anything.
+
     :param file_object: a file object associated with filename
     """
     label = filename.split('/')[-1].split('_')[0]
@@ -11,12 +15,12 @@ def prepare_data_for_a_single_file(file_object, filename):
     image_bytes = file_object.read()
     feature_dict = {}
     feature_dict['image'] = tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=[image_bytes])
+        bytes_list=tf.train.BytesList(value=[image_bytes]),
     )
     feature_dict['label'] = tf.train.Feature(
-        int64_list=tf.train.Int64List(value=[int_label])
+        int64_list=tf.train.Int64List(value=[int_label]),
     )
     example = tf.train.Example(
-        features=tf.train.Features(feature=feature_dict)
+        features=tf.train.Features(feature=feature_dict),
     )
     return example.SerializeToString()
