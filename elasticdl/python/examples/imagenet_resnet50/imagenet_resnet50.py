e10b8de6202c2e6fdff58d627687823bf9b6a9f1
Jialu Zhu <jialu.zhu@antfin.com>
2019-07-02 17:56:44 -0700

    fix

--- elasticdl/python/examples/imagenet_resnet50/imagenet_resnet50.py
+++ elasticdl/python/examples/imagenet_resnet50/imagenet_resnet50.py
@@ -10,17 +10,17 @@ def prepare_data_for_a_single_file(file_object, filename):
 
     :param file_object: a file object associated with filename
     """
-    label = filename.split('/')[-1].split('_')[0]
+    label = filename.split("/")[-1].split("_")[0]
     int_label = int(label)
     image_bytes = file_object.read()
     feature_dict = {}
-    feature_dict['image'] = tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=[image_bytes]),
+    feature_dict["image"] = tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=[image_bytes])
     )
-    feature_dict['label'] = tf.train.Feature(
-        int64_list=tf.train.Int64List(value=[int_label]),
+    feature_dict["label"] = tf.train.Feature(
+        int64_list=tf.train.Int64List(value=[int_label])
     )
     example = tf.train.Example(
-        features=tf.train.Features(feature=feature_dict),
+        features=tf.train.Features(feature=feature_dict)
     )
     return example.SerializeToString()
