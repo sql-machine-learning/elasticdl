4a8a23e0bc530e00642b979331ab46e72e99f9db
Yuan Tang <terrytangyuan@gmail.com>
2019-07-10 08:38:59 -0400

    Add status labels for worker manager (#887)
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/common/constants.py
+++ elasticdl/python/common/constants.py
@@ -3,3 +3,9 @@ class GRPC(object):
     # It's too small to send model parameters.
     MAX_SEND_MESSAGE_LENGTH = 256 * 1024 * 1024
     MAX_RECEIVE_MESSAGE_LENGTH = 256 * 1024 * 1024
+
+
+class WorkerManagerStatus(object):
+    PENDING = "Pending"
+    RUNNING = "Running"
+    FINISHED = "Finished"
--- elasticdl/python/master/main.py
+++ elasticdl/python/master/main.py
@@ -8,7 +8,7 @@ import grpc
 import recordio
 
 from elasticdl.proto import elasticdl_pb2_grpc
-from elasticdl.python.common.constants import GRPC
+from elasticdl.python.common.constants import GRPC, WorkerManagerStatus
 from elasticdl.python.common.model_helper import get_model_file, load_module
 from elasticdl.python.master.args import parse_args
 from elasticdl.python.master.checkpoint_service import CheckpointService
@@ -159,6 +159,7 @@ def main():
     server.start()
     logger.info("Server started at port: %d", args.port)
 
+    worker_manager = None
     if args.num_workers:
         assert args.worker_image, "Worker image cannot be empty"
 
@@ -210,8 +211,10 @@ def main():
             restart_policy=args.restart_policy,
             cluster_spec=args.cluster_spec,
         )
+        worker_manager.update_status(WorkerManagerStatus.PENDING)
         logger.info("Launching %d workers", args.num_workers)
         worker_manager.start_workers()
+        worker_manager.update_status(WorkerManagerStatus.RUNNING)
 
         if tb_service:
             worker_manager.start_tensorboard_service()
@@ -219,6 +222,8 @@ def main():
     try:
         while True:
             if task_d.finished():
+                if worker_manager:
+                    worker_manager.update_status(WorkerManagerStatus.FINISHED)
                 break
             time.sleep(30)
     except KeyboardInterrupt:
