0028de93de3bd01f77c4e269926871d45935e864
terrytangyuan <terrytangyuan@gmail.com>
2019-07-12 16:08:35 -0400

    Refactor common args to common.args
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/master/args.py
+++ elasticdl/python/master/args.py
@@ -1,5 +1,10 @@
 import argparse
 
+from elasticdl.python.common.args import (
+    add_common_params,
+    add_train_params,
+)
+
 
 def _pos_int(arg):
     res = int(arg)
@@ -26,114 +31,9 @@ def parse_args():
         help="The listening port of master",
     )
     parser.add_argument(
-        "--model_def",
-        help="The directory that contains user-defined model files "
-        "or a specific model file",
-        required=True,
-    )
-    parser.add_argument(
-        "--training_data_dir",
-        help="Training data directory. Files should be in RecordIO format",
-        default="",
-    )
-    parser.add_argument(
-        "--evaluation_data_dir",
-        help="Evaluation data directory. Files should be in RecordIO format",
-        default="",
-    )
-    parser.add_argument(
         "---prediction_data_dir",
         help="Prediction data directory. Files should be in RecordIO format",
         default="",
-    )
-    parser.add_argument(
-        "--evaluation_steps",
-        type=_non_neg_int,
-        help="Evaluate the model every this many steps."
-        "If 0, step-based evaluation is disabled",
-        default=0,
-    )
-    parser.add_argument(
-        "--evaluation_start_delay_secs",
-        type=_non_neg_int,
-        help="Start time-based evaluation only after waiting for "
-        "this many seconds",
-        default=100,
-    )
-    parser.add_argument(
-        "--evaluation_throttle_secs",
-        type=_non_neg_int,
-        help="Do not re-evaluate unless the last evaluation was started "
-        "at least this many seconds ago."
-        "If 0, time-based evaluation is disabled",
-        default=0,
-    )
-    parser.add_argument("--records_per_task", type=_pos_int, required=True)
-    parser.add_argument("--num_epochs", type=_pos_int, default=1)
-    parser.add_argument(
-        "--grads_to_wait",
-        type=_pos_int,
-        default=2,
-        help="Number of gradients to wait before updating model",
-    )
-    parser.add_argument(
-        "--minibatch_size",
-        type=_pos_int,
-        help="Minibatch size used by workers",
-        required=True,
-    )
-    parser.add_argument(
-        "--num_workers", type=_pos_int, help="Number of workers", default=0
-    )
-    parser.add_argument(
-        "--checkpoint_filename_for_init",
-        help="The checkpoint file to initialize the training model",
-        default="",
-    )
-    parser.add_argument(
-        "--checkpoint_dir",
-        help="The directory to store the checkpoint files",
-        default="",
-    )
-    parser.add_argument(
-        "--checkpoint_steps",
-        type=_non_neg_int,
-        help="Save checkpoint every this many steps."
-        "If 0, no checkpoints to save.",
-        default=0,
-    )
-    parser.add_argument(
-        "--keep_checkpoint_max",
-        type=_non_neg_int,
-        help="The maximum number of recent checkpoint files to keep."
-        "If 0, keep all.",
-        default=0,
-    )
-    parser.add_argument(
-        "--worker_resource_request",
-        default="cpu=1,memory=4096Mi",
-        type=str,
-        help="The minimal resource required by worker, "
-        "e.g. cpu=1,memory=1024Mi,disk=1024Mi,gpu=1",
-    )
-    parser.add_argument(
-        "--worker_resource_limit",
-        type=str,
-        help="The maximal resource required by worker, "
-        "e.g. cpu=1,memory=1024Mi,disk=1024Mi,gpu=1, "
-        "default to worker_resource_request",
-    )
-    parser.add_argument(
-        "--worker_pod_priority", help="Priority requested by workers"
-    )
-    parser.add_argument(
-        "--worker_image", help="Docker image for workers", default=None
-    )
-    parser.add_argument("--job_name", help="Job name", required=True)
-    parser.add_argument(
-        "--volume",
-        help="The Kubernetes volume information, "
-        'e.g. "claim_name=c1,mount_path=/path1".',
     )
     parser.add_argument(
         "--log_level",
@@ -142,77 +42,9 @@ def parse_args():
         default="INFO",
         help="The logging level. Default to WARNING",
     )
-    parser.add_argument(
-        "--image_pull_policy",
-        default="Always",
-        help="Image pull policy of master and workers",
-    )
-    parser.add_argument(
-        "--restart_policy",
-        default="Never",
-        help="The pod restart policy when pod crashed",
-    )
-    parser.add_argument(
-        "--namespace",
-        default="default",
-        type=str,
-        help="The Kubernetes namespace where ElasticDL jobs run",
-    )
-    parser.add_argument(
-        "--tensorboard_log_dir",
-        default="",
-        type=str,
-        help="Directory where TensorBoard will look to find "
-        "TensorFlow event files that it can display. "
-        "TensorBoard will recursively walk the directory "
-        "structure rooted at log dir, looking for .*tfevents.* "
-        "files. You may also pass a comma separated list of log "
-        "directories, and TensorBoard will watch each "
-        "directory.",
-    )
-    parser.add_argument(
-        "--input_fn",
-        type=str,
-        default="input_fn",
-        help="The name of the input function defined in the model file",
-    )
-    parser.add_argument(
-        "--loss",
-        type=str,
-        default="loss",
-        help="The name of the loss function defined in the model file",
-    )
-    parser.add_argument(
-        "--optimizer",
-        type=str,
-        default="optimizer",
-        help="The name of the optimizer defined in the model file",
-    )
-    parser.add_argument(
-        "--eval_metrics_fn",
-        type=str,
-        default="eval_metrics_fn",
-        help="The name of the evaluation metrics function defined "
-        "in the model file",
-    )
-    parser.add_argument(
-        "--model_class",
-        type=str,
-        default="model",
-        help="The name of the model class defined in the model file",
-    )
-    parser.add_argument(
-        "--model_params",
-        type=str,
-        default="",
-        help="The dictionary of model parameters in a string that will be "
-        'used to instantiate the model, e.g. "param1=1,param2=2"',
-    )
-    parser.add_argument(
-        "--cluster_spec",
-        help="The file that contains user-defined cluster specification",
-        default="",
-    )
+    add_common_params(parser)
+    add_train_params(parser)
+
     args = parser.parse_args()
 
     if args.prediction_data_dir and (
