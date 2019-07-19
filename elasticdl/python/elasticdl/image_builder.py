b28b0107472fa5e72b2b63503c4412f850c09e8a
terrytangyuan <terrytangyuan@gmail.com>
2019-07-19 16:26:34 -0400

    Fix and address comments
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/elasticdl/image_builder.py
+++ elasticdl/python/elasticdl/image_builder.py
@@ -70,7 +70,12 @@ after _build_docker_image.
 
 
 def _copy_if_not_exists(src, dst, is_dir):
-    if not os.path.exists(dst):
+    if os.path.exists(dst):
+        print(
+            "Skip copying from %s to %s since the destination already exists"
+            % (src, dst)
+        )
+    else:
         if is_dir:
             shutil.copytree(src, dst)
         else:
