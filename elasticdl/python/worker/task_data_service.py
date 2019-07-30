1034d0326aa449e01b0101c828f3a9cdf9c49c95
haitao <zht108229@alipay.com>
2019-07-30 12:13:46 +0800

    reset data counts

--- elasticdl/python/worker/task_data_service.py
+++ elasticdl/python/worker/task_data_service.py
@@ -12,12 +12,18 @@ class TaskDataService(object):
     def __init__(self, worker):
         self._logger = logging.getLogger(__name__)
         self._worker = worker
+        self._lock = threading.Lock()
+        self._pending_dataset = True
+        self._reset()
+
+    def _reset(self):
+        """
+        Reset pending tasks and record counts
+        """
         self._record_count = 0
         self._reported_record_count = 0
-        self._pending_tasks_with_data_counts = []
-        self._lock = threading.Lock()
+        self._pending_tasks_with_counts = []
         self._current_task = None
-        self._pending_dataset = True
 
     def get_current_task(self):
         return self._current_task
@@ -27,43 +33,47 @@ class TaskDataService(object):
         Report the number of records in the latest processed batch,
         so TaskDataService knows if some pending tasks are finished
         and report_task_result to the master.
-        self._pending_tasks_with_data_counts[0][0] is the first pending task,
-        self._pending_tasks_with_data_counts[0][1] is the number of records
+        self._pending_tasks_with_counts[0][0] is the first pending task,
+        self._pending_tasks_with_counts[0][1] is the number of records
         in this task.
         """
         self._reported_record_count += count
         if (
-            len(self._pending_tasks_with_data_counts)
+            len(self._pending_tasks_with_counts)
             and self._reported_record_count
-            >= self._pending_tasks_with_data_counts[0][1]
+            >= self._pending_tasks_with_counts[0][1]
         ):
             with self._lock:
                 while (
-                    len(self._pending_tasks_with_data_counts)
+                    len(self._pending_tasks_with_counts)
                     and self._reported_record_count
-                    >= self._pending_tasks_with_data_counts[0][1]
+                    >= self._pending_tasks_with_counts[0][1]
                 ):
-                    task, _ = self._pending_tasks_with_data_counts.pop(0)
+                    task, _ = self._pending_tasks_with_counts.pop(0)
                     self._worker.report_task_result(task.task_id, err_msg)
-                if len(self._pending_tasks_with_data_counts):
-                    self._current_task = self._pending_tasks_with_data_counts[
-                        0
-                    ][0]
+                if len(self._pending_tasks_with_counts):
+                    self._current_task = self._pending_tasks_with_counts[0][0]
 
     def get_dataset(self):
         """
         Return a RecordIO dataset, or None if no more data.
         """
         if self._pending_dataset:
+            if self._pending_tasks_with_counts:
+                self._logger.error(
+                    "Cannot get new dataset when there are pending tasks"
+                )
+                return None
+            self._reset()
             ds = tf.data.Dataset.from_generator(
-                self.gen, (tf.string), (tf.TensorShape([]))
+                self._gen, (tf.string), (tf.TensorShape([]))
             )
             self._pending_dataset = False
             return ds
         else:
             return None
 
-    def gen(self):
+    def _gen(self):
         """
         A generator supports the iter() protocol (e.g. a generator function),
         which is used to create a dataset for RecordIO.
@@ -81,10 +91,10 @@ class TaskDataService(object):
                 break
             with self._lock:
                 self._record_count += task.end - task.start
-                self._pending_tasks_with_data_counts.append(
+                self._pending_tasks_with_counts.append(
                     (task, self._record_count)
                 )
-                if len(self._pending_tasks_with_data_counts) == 1:
+                if len(self._pending_tasks_with_counts) == 1:
                     self._current_task = task
             with closing(
                 recordio.Scanner(
