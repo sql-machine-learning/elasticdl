c99c11f4ec73d633856094291c48cf0371435b8e
Yuan Tang <terrytangyuan@gmail.com>
2019-08-05 11:06:37 -0400

    Add logging config for client code (#991)
    * Add logging config for client code
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Fix precommit
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/elasticdl/api.py
+++ elasticdl/python/elasticdl/api.py
@@ -6,6 +6,12 @@ from elasticdl.python.elasticdl.image_builder import (
     build_and_push_docker_image,
 )
 
+logging.basicConfig(
+    format="%(asctime)s %(name)s %(levelname)-8s "
+    "[%(filename)s:%(lineno)d] %(message)s"
+)
+logging.getLogger().setLevel("INFO")
+
 
 def train(args):
     image_name = build_and_push_docker_image(
