0359efb0de88e5b83cecc9f3809ddb2afb90e97d
yhjust1 <1056209137@qq.com>
2019-08-16 13:50:25 +0800

    add embedding_service.py

---
+++ elasticdl/python/common/embedding_service.py
@@ -0,0 +1,16 @@
+class EmbeddingService(object):
+    """Redis implementation of EmbeddingService"""
+
+    def __init__(self, redis_address_map=None, replicas=1):
+        """
+        # TODO
+        """
+        pass
+
+    @staticmethod
+    def embedding_lookup(**kwargs):
+        pass
+
+    @staticmethod
+    def update_embedding(**kwargs):
+        pass
