8a172a45b095c5a2ab3cbb5858c8da773ec7e377
yhjust1 <1056209137@qq.com>
2019-08-16 14:45:15 +0800

    add embedding_service.py (#1034)
    * add embedding_service.py
    
    * add start/stop function

---
+++ elasticdl/python/common/embedding_service.py
@@ -0,0 +1,22 @@
+class EmbeddingService(object):
+    """Redis implementation of EmbeddingService"""
+
+    def __init__(self, redis_address_map=None, replicas=1):
+        """
+        # TODO
+        """
+        pass
+
+    def start_embedding_service(self):
+        pass
+
+    def stop_embedding_service(self):
+        pass
+
+    @staticmethod
+    def lookup_embedding(**kwargs):
+        pass
+
+    @staticmethod
+    def update_embedding(**kwargs):
+        pass
