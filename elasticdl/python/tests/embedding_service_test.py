a8dd2eff4d7ba0a1127719ab30e15b4b60284c9d
Wei Yan <ywskycn@gmail.com>
2019-08-27 15:49:50 -0700

    Use temp directory when launching redis tests

--- elasticdl/python/tests/embedding_service_test.py
+++ elasticdl/python/tests/embedding_service_test.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import subprocess
+import tempfile
 import time
 import unittest
 
@@ -9,17 +10,17 @@ import numpy as np
 from elasticdl.python.common.embedding_service import EmbeddingService
 
 
-def start_redis_instances(first_port):
+def start_redis_instances(first_port, temp_dir):
     for i in range(6):
         port = first_port + i
         embedding_process = subprocess.Popen(
             [
-                "redis-server --port %d --cluster-enabled yes "
+                "redis-server --port %d --dir %s --cluster-enabled yes "
                 "--cluster-config-file nodes-%d.conf "
                 "--cluster-node-timeout 200 --appendonly yes --appendfilename "
                 "appendonly-%d.aof --dbfilename dump-%d.rdb "
                 "--logfile %d.log --daemonize yes --protected-mode no"
-                % (port, port, port, port, port)
+                % (port, temp_dir, port, port, port, port)
             ],
             shell=True,
             stdout=subprocess.DEVNULL,
@@ -30,77 +31,75 @@ def start_redis_instances(first_port):
     return embedding_endpoint
 
 
-def clean_test_file():
-    filetype_list = ["appendonly*.aof", "nodes*.conf", "*.log", "dump*.rdb"]
-    for filetype in filetype_list:
-        for file in glob.glob(os.path.join(".", filetype)):
-            os.remove(file)
-
-
 class EmbeddingServiceTest(unittest.TestCase):
     def test_embedding_service(self):
-        embedding_endpoint = start_redis_instances(30001)
-        # start
-        embedding_service = EmbeddingService(embedding_endpoint)
-        embedding_endpoint = embedding_service._create_redis_cluster()
-        # wait for cluster up-running
-        time.sleep(1)
-        self.assertFalse(embedding_endpoint is None)
-        # connection
-        redis_cluster = embedding_service._get_embedding_cluster()
-        self.assertFalse(redis_cluster is None)
-        # set value to a key
-        self.assertTrue(redis_cluster.set("test_key", "OK", nx=True))
-        # set value to a key existed
-        self.assertTrue(redis_cluster.set("test_key", "OK", nx=True) is None)
-        self.assertEqual(b"OK", redis_cluster.get("test_key"))
-        # close
-        self.assertTrue(embedding_service.stop_embedding_service())
+        with tempfile.TemporaryDirectory() as temp_dir:
+            embedding_endpoint = start_redis_instances(30001, temp_dir)
+            # start
+            embedding_service = EmbeddingService(embedding_endpoint)
+            embedding_endpoint = embedding_service._create_redis_cluster()
+            # wait for cluster up-running
+            time.sleep(1)
+            self.assertFalse(embedding_endpoint is None)
+            # connection
+            redis_cluster = embedding_service._get_embedding_cluster()
+            self.assertFalse(redis_cluster is None)
+            # set value to a key
+            self.assertTrue(redis_cluster.set("test_key", "OK", nx=True))
+            # set value to a key existed
+            self.assertTrue(
+                redis_cluster.set("test_key", "OK", nx=True) is None
+            )
+            self.assertEqual(b"OK", redis_cluster.get("test_key"))
+            # close
+            self.assertTrue(embedding_service.stop_embedding_service())
 
     def test_lookup_and_update_embedding(self):
-        embedding_endpoint = start_redis_instances(31001)
-        # start
-        embedding_service = EmbeddingService(embedding_endpoint)
-        embedding_endpoint = embedding_service._create_redis_cluster()
-        # wait for cluster up-running
-        time.sleep(1)
-        origin_data = np.random.rand(100, 10).astype(np.float32)
-        keys = ["test_%d" % i for i in range(origin_data.shape[0])]
+        with tempfile.TemporaryDirectory() as temp_dir:
+            embedding_endpoint = start_redis_instances(31001, temp_dir)
+            # start
+            embedding_service = EmbeddingService(embedding_endpoint)
+            embedding_endpoint = embedding_service._create_redis_cluster()
+            # wait for cluster up-running
+            time.sleep(1)
+            origin_data = np.random.rand(100, 10).astype(np.float32)
+            keys = ["test_%d" % i for i in range(origin_data.shape[0])]
 
-        EmbeddingService.update_embedding(
-            keys, origin_data, embedding_endpoint
-        )
-        lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
-            keys, embedding_endpoint, parse_type=np.float32
-        )
-        self.assertTrue(len(unknown_keys_index) == 0)
-        output_length = len(keys)
-        lookup_data = np.concatenate(lookup_data, axis=0)
-        lookup_data = lookup_data.reshape((output_length, -1))
-        self.assertTrue(np.equal(origin_data, lookup_data).all())
+            EmbeddingService.update_embedding(
+                keys, origin_data, embedding_endpoint
+            )
+            lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
+                keys, embedding_endpoint, parse_type=np.float32
+            )
+            self.assertTrue(len(unknown_keys_index) == 0)
+            output_length = len(keys)
+            lookup_data = np.concatenate(lookup_data, axis=0)
+            lookup_data = lookup_data.reshape((output_length, -1))
+            self.assertTrue(np.equal(origin_data, lookup_data).all())
 
-        # Test set_if_not_exist
-        origin_data_2 = np.random.rand(100, 10).astype(np.float32)
-        self.assertFalse(np.equal(origin_data, origin_data_2).all())
-        EmbeddingService.update_embedding(
-            keys, origin_data_2, embedding_endpoint, set_if_not_exist=True
-        )
-        lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
-            keys, embedding_endpoint, parse_type=np.float32
-        )
-        lookup_data = np.concatenate(lookup_data, axis=0)
-        lookup_data = lookup_data.reshape((output_length, -1))
-        self.assertTrue(np.equal(origin_data, lookup_data).all())
-        self.assertFalse(np.equal(origin_data_2, lookup_data).all())
+            # Test set_if_not_exist
+            origin_data_2 = np.random.rand(100, 10).astype(np.float32)
+            self.assertFalse(np.equal(origin_data, origin_data_2).all())
+            EmbeddingService.update_embedding(
+                keys, origin_data_2, embedding_endpoint, set_if_not_exist=True
+            )
+            lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
+                keys, embedding_endpoint, parse_type=np.float32
+            )
+            lookup_data = np.concatenate(lookup_data, axis=0)
+            lookup_data = lookup_data.reshape((output_length, -1))
+            self.assertTrue(np.equal(origin_data, lookup_data).all())
+            self.assertFalse(np.equal(origin_data_2, lookup_data).all())
 
-        # Test non-exist keys
-        keys_do_not_exist = ["test_no_exist_%d" % i for i in range(10)]
-        lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
-            keys_do_not_exist, embedding_endpoint, parse_type=np.float32
-        )
-        self.assertTrue(len(unknown_keys_index) == 10)
-        self.assertTrue(len(lookup_data) == 10)
-        clean_test_file()
+            # Test non-exist keys
+            keys_do_not_exist = ["test_no_exist_%d" % i for i in range(10)]
+            lookup_data, unknown_keys_index = EmbeddingService.lookup_embedding(
+                keys_do_not_exist, embedding_endpoint, parse_type=np.float32
+            )
+            self.assertTrue(len(unknown_keys_index) == 10)
+            self.assertTrue(len(lookup_data) == 10)
+            # Close
+            self.assertTrue(embedding_service.stop_embedding_service())
 
 
 if __name__ == "__main__":
