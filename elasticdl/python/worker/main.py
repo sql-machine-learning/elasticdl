d1b462894e7c0c5a24372853818ed193c618ccb7
chengfu <chengfu.wcy@antfin.com>
2019-09-06 15:38:52 +0800

    Fix #1129, add log for command line options

--- elasticdl/python/worker/main.py
+++ elasticdl/python/worker/main.py
@@ -3,6 +3,7 @@ import argparse
 import grpc
 
 from elasticdl.python.common import log_util
+from elasticdl.python.common.args import ALL_ARGS_GROUP, print_args
 from elasticdl.python.common.constants import GRPC
 from elasticdl.python.worker.worker import Worker
 
@@ -85,6 +86,7 @@ def _parse_args():
 
 def main():
     args = _parse_args()
+    print_args(args, groups=ALL_ARGS_GROUP)
     channel = grpc.insecure_channel(
         args.master_addr,
         options=[
