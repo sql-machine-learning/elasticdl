05f599d03c96efe757d7869f50a8a5557ee52804
Yuan Tang <terrytangyuan@gmail.com>
2019-09-09 16:23:08 -0400

    Add ElasticDL predict and evaluate jobs to Travis CI (#1143)
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scripts/setup_k8s_env.sh
+++ scripts/setup_k8s_env.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 export MINIKUBE_WANTUPDATENOTIFICATION=false
 export MINIKUBE_WANTREPORTERRORPROMPT=false
 export MINIKUBE_HOME=$HOME
@@ -10,10 +8,10 @@ export KUBECONFIG=$HOME/.kube/config
 export K8S_VERSION=1.14.0
 export MINIKUBE_VERSION=1.1.1
 
-curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v$MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${K8S_VERSION}/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p $HOME/.kube $HOME/.minikube
-touch $KUBECONFIG
-sudo minikube start --vm-driver=none --kubernetes-version=v$K8S_VERSION --cpus 2 --memory 6144
+touch ${KUBECONFIG}
+sudo minikube start --vm-driver=none --kubernetes-version=v${K8S_VERSION} --cpus 2 --memory 6144
 sudo chown -R travis: $HOME/.minikube/
 kubectl cluster-info
--- scripts/validate_job_status.sh
+++ scripts/validate_job_status.sh
@@ -1,8 +1,9 @@
-set -x
+#!/usr/bin/env bash
 
-MASTER_POD_NAME=elasticdl-test-mnist-master
-WORKER_0_POD_NAME=elasticdl-test-mnist-worker-0
-WORKER_1_POD_NAME=elasticdl-test-mnist-worker-1
+JOB_TYPE=$1
+MASTER_POD_NAME=elasticdl-test-mnist-${JOB_TYPE}-master
+WORKER_0_POD_NAME=elasticdl-test-mnist-${JOB_TYPE}-worker-0
+WORKER_1_POD_NAME=elasticdl-test-mnist-${JOB_TYPE}-worker-1
 CHECK_INTERVAL_SECS=10
 
 function get_pod_status {
@@ -15,22 +16,24 @@ for i in {1..200}; do
     WORKER_0_POD_STATUS=$(get_pod_status ${WORKER_0_POD_NAME})
     WORKER_1_POD_STATUS=$(get_pod_status ${WORKER_1_POD_NAME})
 
-    if [ "$MASTER_POD_STATUS" == "Succeeded" ] &&
-     [ "$WORKER_0_POD_STATUS" == "Succeeded" ] &&
-     [ "$WORKER_1_POD_STATUS" == "Succeeded" ]; then
+    if [[ "$MASTER_POD_STATUS" == "Succeeded" ]] &&
+     [[ "$WORKER_0_POD_STATUS" == "Succeeded" ]] &&
+     [[ "$WORKER_1_POD_STATUS" == "Succeeded" ]]; then
       echo "ElasticDL job succeeded."
+      kubectl delete pod ${MASTER_POD_NAME}
       exit 0
-    elif [ "$MASTER_POD_STATUS" == "Failed" ] ||
-       [ "$WORKER_0_POD_STATUS" == "Failed" ] ||
-       [ "$WORKER_1_POD_STATUS" == "Failed" ]; then
+    elif [[ "$MASTER_POD_STATUS" == "Failed" ]] ||
+       [[ "$WORKER_0_POD_STATUS" == "Failed" ]] ||
+       [[ "$WORKER_1_POD_STATUS" == "Failed" ]]; then
       echo "ElasticDL job failed."
-      kubectl describe pod $MASTER_POD_NAME
+      kubectl describe pod ${MASTER_POD_NAME}
       echo "\nMaster log:\n"
-      kubectl logs $MASTER_POD_NAME | tail
+      kubectl logs ${MASTER_POD_NAME} | tail
       echo "\nWorker0 log:\n"
-      kubectl logs $WORKER_0_POD_NAME | tail
+      kubectl logs ${WORKER_0_POD_NAME} | tail
       echo "\nWorker1 log:\n"
-      kubectl logs $WORKER_1_POD_NAME | tail
+      kubectl logs ${WORKER_1_POD_NAME} | tail
+      kubectl delete pod ${MASTER_POD_NAME}
       exit 1
     else
       echo "Master: ${MASTER_POD_STATUS}, Worker0: ${WORKER_0_POD_STATUS}, Worker1: ${WORKER_1_POD_STATUS}. Continue checking..."
@@ -39,4 +42,7 @@ for i in {1..200}; do
     top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}'
 done
 echo "ElasticDL job timed out."
+
+kubectl delete pod ${MASTER_POD_NAME}
+
 exit 1
