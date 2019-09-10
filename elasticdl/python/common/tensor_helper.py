1fef9f399952a64e820c1f6300f18db7b4f3af4e
chencheng (云谦) <sorrycc@gmail.com>
2019-09-10 09:18:09 +0800

    doc: initialize the doc (#1126)
    * doc: initialize the doc
    
    * feat: show menu in small screen
    
    * doc: update links of top menu

--- elasticdl/python/common/tensor_helper.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-
-def merge_indexed_slices(*args):
-    return tf.IndexedSlices(
-        tf.concat([i.values for i in args], axis=0),
-        tf.concat([i.indices for i in args], axis=0),
-    )
