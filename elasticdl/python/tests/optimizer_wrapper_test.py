f51ff08bac0ef5c1a11e0166accbcd7a6c2436ef
Minghao Li <Merlinhool1994@gmail.com>
2019-09-17 17:38:11 +0800

    Support tf.function when model has ElasticDL embedding layer (#1189)
    * support tf.function when model has embedding layer
    
    * fix && modify comments
    
    * refactor bet_ids_pair to namedtuple; refactor some repeat code to Embedding._record_gradients
    
    * rename bet_ids_pair to embedding_and_ids
    
    * fix pre-commit
    
    * remove repeat code in layer_test.py
    
    * fix pre-commit

--- elasticdl/python/tests/optimizer_wrapper_test.py
+++ elasticdl/python/tests/optimizer_wrapper_test.py
@@ -121,7 +121,10 @@ def _train_edl_embedding_with_optimizer_wrapper(
         embed_items = []
         for layer in embed_layers:
             embed_items.extend(
-                [(bet, layer.name, ids) for bet, ids in layer.bet_ids_pair]
+                [
+                    (bet, layer.name, ids)
+                    for bet, ids in layer.embedding_and_ids
+                ]
             )
 
         grads = tape.gradient(
