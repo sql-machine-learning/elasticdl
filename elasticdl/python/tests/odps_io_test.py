eac0d6b9043fcee691bab7b5ae6730823947cb04
Yuan Tang <terrytangyuan@gmail.com>
2019-09-17 12:42:00 -0400

    Enable tests related to ODPS on Travis CI (#1201)
    * Create iris ODPS table for testing
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Test ODPS on Travis
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Update .travis.yml
    
    * Regenerate secure tokens
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Fix tests
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Clean up
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Fix wrong message
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/tests/odps_io_test.py
+++ elasticdl/python/tests/odps_io_test.py
@@ -22,8 +22,11 @@ class ODPSIOTest(unittest.TestCase):
         self._project = os.environ[ODPSConfig.PROJECT_NAME]
         self._access_id = os.environ[ODPSConfig.ACCESS_ID]
         self._access_key = os.environ[ODPSConfig.ACCESS_KEY]
-        self._endpoint = os.environ[ODPSConfig.ENDPOINT]
-        self._test_read_table = "chicago_taxi_train_data"
+        self._endpoint = os.environ.get(ODPSConfig.ENDPOINT)
+        self._test_read_table = "test_odps_reader_%d_%d" % (
+            int(time.time()),
+            random.randint(1, 101),
+        )
         self._test_write_table = "test_odps_writer_%d_%d" % (
             int(time.time()),
             random.randint(1, 101),
@@ -31,6 +34,7 @@ class ODPSIOTest(unittest.TestCase):
         self._odps_client = ODPS(
             self._access_id, self._access_key, self._project, self._endpoint
         )
+        self.create_iris_odps_table()
 
     def test_read_to_iterator(self):
         reader = ODPSReader(
@@ -43,11 +47,17 @@ class ODPSIOTest(unittest.TestCase):
             4,
             None,
         )
-        records_iter = reader.to_iterator(1, 0, 200, 2, False, None)
-        for batch in records_iter:
-            self.assertEqual(
-                len(batch), 200, "incompatible size: %d" % len(batch)
-            )
+        records_iter = reader.to_iterator(1, 0, 50, 2, False, None)
+        records = list(records_iter)
+        self.assertEqual(
+            len(records), 6, "Unexpected number of batches: %d" % len(records)
+        )
+        flattened_records = [record for batch in records for record in batch]
+        self.assertEqual(
+            len(flattened_records),
+            220,
+            "Unexpected number of total records: %d" % len(flattened_records),
+        )
 
     def test_write_odps_to_recordio_shards_from_iterator(self):
         reader = ODPSReader(
@@ -60,15 +70,15 @@ class ODPSIOTest(unittest.TestCase):
             4,
             None,
         )
-        records_iter = reader.to_iterator(1, 0, 200, 2, False, None)
+        records_iter = reader.to_iterator(1, 0, 50, 2, False, None)
         with tempfile.TemporaryDirectory() as output_dir:
             write_recordio_shards_from_iterator(
                 records_iter,
-                ["f" + str(i) for i in range(18)],
+                ["f" + str(i) for i in range(5)],
                 output_dir,
-                records_per_shard=200,
+                records_per_shard=50,
             )
-            self.assertEqual(len(os.listdir(output_dir)), 100)
+            self.assertEqual(len(os.listdir(output_dir)), 5)
 
     def test_write_from_iterator(self):
         columns = ["num", "num2"]
@@ -108,9 +118,141 @@ class ODPSIOTest(unittest.TestCase):
         self.assertEqual(table.schema.types, column_types)
         self.assertEqual(table.to_df().count(), 2)
 
+    def create_iris_odps_table(self):
+        sql_tmpl = """
+        DROP TABLE IF EXISTS {PROJECT_NAME}.{TABLE_NAME};
+        CREATE TABLE {PROJECT_NAME}.{TABLE_NAME} (
+               sepal_length DOUBLE,
+               sepal_width  DOUBLE,
+               petal_length DOUBLE,
+               petal_width  DOUBLE,
+               class BIGINT);
+
+        INSERT INTO {PROJECT_NAME}.{TABLE_NAME} VALUES
+        (6.4,2.8,5.6,2.2,2),
+        (5.0,2.3,3.3,1.0,1),
+        (4.9,2.5,4.5,1.7,2),
+        (4.9,3.1,1.5,0.1,0),
+        (5.7,3.8,1.7,0.3,0),
+        (4.4,3.2,1.3,0.2,0),
+        (5.4,3.4,1.5,0.4,0),
+        (6.9,3.1,5.1,2.3,2),
+        (6.7,3.1,4.4,1.4,1),
+        (5.1,3.7,1.5,0.4,0),
+        (5.2,2.7,3.9,1.4,1),
+        (6.9,3.1,4.9,1.5,1),
+        (5.8,4.0,1.2,0.2,0),
+        (5.4,3.9,1.7,0.4,0),
+        (7.7,3.8,6.7,2.2,2),
+        (6.3,3.3,4.7,1.6,1),
+        (6.8,3.2,5.9,2.3,2),
+        (7.6,3.0,6.6,2.1,2),
+        (6.4,3.2,5.3,2.3,2),
+        (5.7,4.4,1.5,0.4,0),
+        (6.7,3.3,5.7,2.1,2),
+        (6.4,2.8,5.6,2.1,2),
+        (5.4,3.9,1.3,0.4,0),
+        (6.1,2.6,5.6,1.4,2),
+        (7.2,3.0,5.8,1.6,2),
+        (5.2,3.5,1.5,0.2,0),
+        (5.8,2.6,4.0,1.2,1),
+        (5.9,3.0,5.1,1.8,2),
+        (5.4,3.0,4.5,1.5,1),
+        (6.7,3.0,5.0,1.7,1),
+        (6.3,2.3,4.4,1.3,1),
+        (5.1,2.5,3.0,1.1,1),
+        (6.4,3.2,4.5,1.5,1),
+        (6.8,3.0,5.5,2.1,2),
+        (6.2,2.8,4.8,1.8,2),
+        (6.9,3.2,5.7,2.3,2),
+        (6.5,3.2,5.1,2.0,2),
+        (5.8,2.8,5.1,2.4,2),
+        (5.1,3.8,1.5,0.3,0),
+        (4.8,3.0,1.4,0.3,0),
+        (7.9,3.8,6.4,2.0,2),
+        (5.8,2.7,5.1,1.9,2),
+        (6.7,3.0,5.2,2.3,2),
+        (5.1,3.8,1.9,0.4,0),
+        (4.7,3.2,1.6,0.2,0),
+        (6.0,2.2,5.0,1.5,2),
+        (4.8,3.4,1.6,0.2,0),
+        (7.7,2.6,6.9,2.3,2),
+        (4.6,3.6,1.0,0.2,0),
+        (7.2,3.2,6.0,1.8,2),
+        (5.0,3.3,1.4,0.2,0),
+        (6.6,3.0,4.4,1.4,1),
+        (6.1,2.8,4.0,1.3,1),
+        (5.0,3.2,1.2,0.2,0),
+        (7.0,3.2,4.7,1.4,1),
+        (6.0,3.0,4.8,1.8,2),
+        (7.4,2.8,6.1,1.9,2),
+        (5.8,2.7,5.1,1.9,2),
+        (6.2,3.4,5.4,2.3,2),
+        (5.0,2.0,3.5,1.0,1),
+        (5.6,2.5,3.9,1.1,1),
+        (6.7,3.1,5.6,2.4,2),
+        (6.3,2.5,5.0,1.9,2),
+        (6.4,3.1,5.5,1.8,2),
+        (6.2,2.2,4.5,1.5,1),
+        (7.3,2.9,6.3,1.8,2),
+        (4.4,3.0,1.3,0.2,0),
+        (7.2,3.6,6.1,2.5,2),
+        (6.5,3.0,5.5,1.8,2),
+        (5.0,3.4,1.5,0.2,0),
+        (4.7,3.2,1.3,0.2,0),
+        (6.6,2.9,4.6,1.3,1),
+        (5.5,3.5,1.3,0.2,0),
+        (7.7,3.0,6.1,2.3,2),
+        (6.1,3.0,4.9,1.8,2),
+        (4.9,3.1,1.5,0.1,0),
+        (5.5,2.4,3.8,1.1,1),
+        (5.7,2.9,4.2,1.3,1),
+        (6.0,2.9,4.5,1.5,1),
+        (6.4,2.7,5.3,1.9,2),
+        (5.4,3.7,1.5,0.2,0),
+        (6.1,2.9,4.7,1.4,1),
+        (6.5,2.8,4.6,1.5,1),
+        (5.6,2.7,4.2,1.3,1),
+        (6.3,3.4,5.6,2.4,2),
+        (4.9,3.1,1.5,0.1,0),
+        (6.8,2.8,4.8,1.4,1),
+        (5.7,2.8,4.5,1.3,1),
+        (6.0,2.7,5.1,1.6,1),
+        (5.0,3.5,1.3,0.3,0),
+        (6.5,3.0,5.2,2.0,2),
+        (6.1,2.8,4.7,1.2,1),
+        (5.1,3.5,1.4,0.3,0),
+        (4.6,3.1,1.5,0.2,0),
+        (6.5,3.0,5.8,2.2,2),
+        (4.6,3.4,1.4,0.3,0),
+        (4.6,3.2,1.4,0.2,0),
+        (7.7,2.8,6.7,2.0,2),
+        (5.9,3.2,4.8,1.8,1),
+        (5.1,3.8,1.6,0.2,0),
+        (4.9,3.0,1.4,0.2,0),
+        (4.9,2.4,3.3,1.0,1),
+        (4.5,2.3,1.3,0.3,0),
+        (5.8,2.7,4.1,1.0,1),
+        (5.0,3.4,1.6,0.4,0),
+        (5.2,3.4,1.4,0.2,0),
+        (5.3,3.7,1.5,0.2,0),
+        (5.0,3.6,1.4,0.2,0),
+        (5.6,2.9,3.6,1.3,1),
+        (4.8,3.1,1.6,0.2,0);
+        """
+        self._odps_client.execute_sql(
+            sql_tmpl.format(
+                PROJECT_NAME=self._project, TABLE_NAME=self._test_read_table
+            ),
+            hints={"odps.sql.submit.mode": "script"},
+        )
+
     def tearDown(self):
         self._odps_client.delete_table(
             self._test_write_table, self._project, if_exists=True
+        )
+        self._odps_client.delete_table(
+            self._test_read_table, self._project, if_exists=True
         )
 
 
