65f4e01194f2b219f8703d0bf55dba7df737a01e
terrytangyuan <terrytangyuan@gmail.com>
2019-09-20 09:26:24 -0400

    Rename LearningRateModulation to LearningRateModulator and reformat docstring
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/master/learning_rate_modulator.py
+++ elasticdl/python/master/learning_rate_modulator.py
@@ -1,23 +1,31 @@
 import threading
 
 
-class LearningRateModulation:
-    """
-    Modify learning rate with a multiplier.
-    Support concurrent usage by using thread local storage.
-    Arguments
-      learning rate: can be a value or a callable.
+class LearningRateModulator:
+    """Modulates the learning rate with a multiplier.
+
+    Note:
+        This class supports concurrent usage by using
+        thread local storage.
     """
 
     def __init__(self, learning_rate):
+        """Constructs a `LearningRateModulator` instance.
+
+        Args:
+            learning_rate: The learning rate to be modulated.
+                This can be either a numeric value or a callable.
+        """
         self._learning_rate = learning_rate
         self._tls = threading.local()
         self._tls.multiplier = 1
 
     def set_multiplier(self, multiplier):
+        """Sets the multiplier."""
         self._tls.multiplier = multiplier
 
     def get_learning_rate(self):
+        """Gets the current learning rate."""
         lr = self._learning_rate
         if callable(lr):
             lr = lr()
@@ -26,18 +34,19 @@ class LearningRateModulation:
 
 
 def add_lr_modulation_to_optimizer(optimizer):
-    """
-    Add lr modulation feature in optimizer
-    Argument:
-      optimizer: the optimizer to add lr modulation feature
-    Return:
-      LearningRateModulation instance
+    """Adds learning rate modulation to the given optimizer.
+
+    Args:
+      optimizer: The optimizer to add learning rate modulation to.
+
+    Returns:
+      A `LearningRateModulator` instance.
     """
     # Get learning rate from optimizer
     learning_rate = optimizer._hyper["learning_rate"]
 
     # Replace the learning rate in optimizer with a calllable
-    lr_modulation = LearningRateModulation(learning_rate)
+    lr_modulation = LearningRateModulator(learning_rate)
     optimizer.learning_rate = lr_modulation.get_learning_rate
 
     return lr_modulation
