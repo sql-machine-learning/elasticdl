dfc7886b6edd7cf81365917973fe3fedda6bef60
Yuan Tang <terrytangyuan@gmail.com>
2019-10-09 12:58:52 -0400

    Add integration test for ElasticDL job with ODPS data source (#1279)
    * Add integration test for job with ODPS data source
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Fix precommit
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>
    
    * Remove redundant table deletion
    
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scripts/client_test.sh
+++ scripts/client_test.sh
@@ -20,7 +20,7 @@ if [[ "$JOB_TYPE" == "train" ]]; then
       --checkpoint_steps=10 \
       --evaluation_steps=15 \
       --grads_to_wait=2 \
-      --job_name=test-mnist-train \
+      --job_name=test-train \
       --log_level=INFO \
       --image_pull_policy=Never \
       --output=model_output
@@ -40,7 +40,7 @@ elif [[ "$JOB_TYPE" == "evaluate" ]]; then
       --num_minibatches_per_task=2 \
       --num_workers=2 \
       --evaluation_steps=15 \
-      --job_name=test-mnist-evaluate \
+      --job_name=test-evaluate \
       --log_level=INFO \
       --image_pull_policy=Never
 elif [[ "$JOB_TYPE" == "predict" ]]; then
@@ -57,9 +57,31 @@ elif [[ "$JOB_TYPE" == "predict" ]]; then
       --minibatch_size=64 \
       --num_minibatches_per_task=2 \
       --num_workers=2 \
-      --job_name=test-mnist-predict \
+      --job_name=test-predict \
       --log_level=INFO \
       --image_pull_policy=Never
+elif [[ "$JOB_TYPE" == "odps" ]]; then
+    elasticdl train \
+      --image_base=elasticdl:ci \
+      --model_zoo=model_zoo \
+      --model_def=odps_iris_dnn_model.odps_iris_dnn_model.custom_model \
+      --training_data_dir=$ODPS_TABLE_NAME \
+      --data_reader_params='columns=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]' \
+      --envs="ODPS_PROJECT_NAME=$ODPS_PROJECT_NAME,ODPS_ACCESS_ID=$ODPS_ACCESS_ID,ODPS_ACCESS_KEY=$ODPS_ACCESS_KEY" \
+      --num_epochs=2 \
+      --master_resource_request="cpu=400m,memory=1024Mi" \
+      --master_resource_limit="cpu=1,memory=2048Mi" \
+      --worker_resource_request="cpu=400m,memory=2048Mi" \
+      --worker_resource_limit="cpu=1,memory=3072Mi" \
+      --minibatch_size=64 \
+      --num_minibatches_per_task=2 \
+      --num_workers=2 \
+      --checkpoint_steps=10 \
+      --grads_to_wait=2 \
+      --job_name=test-odps \
+      --log_level=INFO \
+      --image_pull_policy=Never \
+      --output=model_output
 else
     echo "Unsupported job type specified: $JOB_TYPE"
     exit 1
