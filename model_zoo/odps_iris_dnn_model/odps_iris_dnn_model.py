79dcfbb60fc86fd3b55a43db5f3a6f969433507b
workingloong <WangQL1201@outlook.com>
2019-10-14 14:07:07 +0800

    modify data type to tf.string in ODPS reader

--- model_zoo/odps_iris_dnn_model/odps_iris_dnn_model.py
+++ model_zoo/odps_iris_dnn_model/odps_iris_dnn_model.py
@@ -24,7 +24,8 @@ def optimizer(lr=0.1):
 
 def dataset_fn(dataset, mode, metadata):
     def _parse_data(record):
-        label_col_name = "class"
+        label_col_name = "label"
+        record = tf.strings.to_number(record, tf.float32)
 
         def _get_features_without_labels(
             record, label_col_ind, features_shape
