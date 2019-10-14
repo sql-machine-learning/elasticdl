c2cdbe7b700d389294533426a8bea19e7214c53f
workingloong <WangQL1201@outlook.com>
2019-10-14 14:16:16 +0800

    redo modification label_col_name to

--- model_zoo/odps_iris_dnn_model/odps_iris_dnn_model.py
+++ model_zoo/odps_iris_dnn_model/odps_iris_dnn_model.py
@@ -24,7 +24,7 @@ def optimizer(lr=0.1):
 
 def dataset_fn(dataset, mode, metadata):
     def _parse_data(record):
-        label_col_name = "label"
+        label_col_name = "class"
         record = tf.strings.to_number(record, tf.float32)
 
         def _get_features_without_labels(
