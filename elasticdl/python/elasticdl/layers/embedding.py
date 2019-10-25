4275aaa6f4bb3c38af4d83d69020f82785c05b3d
Qinlong Wang <WangQL1201@outlook.com>
2019-10-25 20:57:38 +0800

    Cast input to int64 for embedding `call` (#1375)
    * cast input to int64 for embedding call
    
    * cast intput to tf.int64

--- elasticdl/python/elasticdl/layers/embedding.py
+++ elasticdl/python/elasticdl/layers/embedding.py
@@ -186,6 +186,7 @@ class Embedding(tf.keras.layers.Layer):
         return batch_embedding
 
     def call(self, input):
+        input = tf.cast(input, tf.int64)
         if isinstance(input, tf.SparseTensor):
             return self._sparse_input_call(input)
 
