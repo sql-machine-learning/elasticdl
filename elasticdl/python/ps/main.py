9e97f251a5ec2de2702d9f8f2c9d3d8be4a9a61f
haitao <tenn_2001c@yahoo.com>
2019-10-28 16:14:08 +0800

    reorg ps.main

--- elasticdl/python/ps/main.py
+++ elasticdl/python/ps/main.py
@@ -1,64 +1,13 @@
-import time
-from concurrent import futures
-
-import grpc
-
-from elasticdl.proto import elasticdl_pb2_grpc
 from elasticdl.python.common.args import parse_ps_args
-from elasticdl.python.common.constants import GRPC
-from elasticdl.python.common.log_utils import get_logger
-from elasticdl.python.common.model_utils import (
-    get_module_file_path,
-    load_module,
-)
-from elasticdl.python.ps.parameters import Parameters
-from elasticdl.python.ps.servicer import PserverServicer
+from elasticdl.python.ps.parameter_server import ParameterServer
 
 
 def main():
     args = parse_ps_args()
-    logger = get_logger("PS", level=args.log_level.upper())
+    pserver = ParameterServer(args)
+    pserver.prepare()
+    pserver.run()
 
-    model_module = load_module(
-        get_module_file_path(args.model_zoo, args.model_def)
-    ).__dict__
-    optimizer = model_module[args.optimizer]()
 
-    logger.info("Starting PS pod with optimizer as %s", optimizer._name)
-
-    # Create Parameters instance
-    parameters = Parameters()
-
-    # The pserver service
-    logger.info("Starting pserver service")
-    server = grpc.server(
-        futures.ThreadPoolExecutor(max_workers=64),
-        options=[
-            ("grpc.max_send_message_length", GRPC.MAX_SEND_MESSAGE_LENGTH),
-            (
-                "grpc.max_receive_message_length",
-                GRPC.MAX_RECEIVE_MESSAGE_LENGTH,
-            ),
-        ],
-    )
-    pserver_servicer = PserverServicer(
-        parameters,
-        args.grads_to_wait,
-        optimizer,
-        lr_staleness_modulation=args.lr_staleness_modulation,
-        use_async=args.use_async,
-    )
-    elasticdl_pb2_grpc.add_PserverServicer_to_server(pserver_servicer, server)
-    server.add_insecure_port("[::]:{}".format(args.port))
-    server.start()
-    logger.info("RPC Server started at port: %d", args.port)
-
-    try:
-        while True:
-            # TODO: add loop break condition
-            time.sleep(30)
-    except KeyboardInterrupt:
-        logger.warning("Server stopping")
-
-    server.stop(0)
-    logger.info("RPC server stopped")
+if __name__ == "__main__":
+    main()
--- elasticdl/python/tests/pserver_servicer_test.py
+++ elasticdl/python/tests/pserver_servicer_test.py
@@ -1,20 +1,42 @@
+import os
 import unittest
-from concurrent import futures
 
 import grpc
-import tensorflow as tf
 from google.protobuf import empty_pb2
 
 from elasticdl.proto import elasticdl_pb2_grpc
 from elasticdl.python.common.constants import GRPC
+from elasticdl.python.ps.parameter_server import ParameterServer
 from elasticdl.python.ps.parameters import Parameters
-from elasticdl.python.ps.servicer import PserverServicer
+
+_test_model_zoo_path = os.path.dirname(os.path.realpath(__file__))
+
+
+class PserverArgs(object):
+    def __init__(
+        self,
+        grads_to_wait=8,
+        lr_staleness_modulation=0,
+        use_async=False,
+        model_zoo=_test_model_zoo_path,
+        model_def="test_module.custom_model",
+        optimizer="optimizer",
+        port=9999,
+        log_level="INFO",
+    ):
+        self.grads_to_wait = grads_to_wait
+        self.lr_staleness_modulation = lr_staleness_modulation
+        self.use_async = use_async
+        self.model_zoo = model_zoo
+        self.model_def = model_def
+        self.optimizer = optimizer
+        self.port = port
+        self.log_level = log_level
 
 
 class PserverServicerTest(unittest.TestCase):
     def setUp(self):
         self._port = 9999
-
         addr = "localhost:%d" % self._port
         self._channel = grpc.insecure_channel(
             addr,
@@ -26,75 +48,47 @@ class PserverServicerTest(unittest.TestCase):
                 ),
             ],
         )
+        self._server = None
 
-    def create_server(
-        self,
-        parameters,
-        grads_to_wait,
-        optimizer,
-        lr_staleness_modulation,
-        use_async,
+    def tearDown(self):
+        if self._server:
+            self._server.stop(0)
+
+    def create_server_and_stub(
+        self, parameters, grads_to_wait, lr_staleness_modulation, use_async
     ):
-        server = grpc.server(
-            futures.ThreadPoolExecutor(max_workers=64),
-            options=[
-                ("grpc.max_send_message_length", GRPC.MAX_SEND_MESSAGE_LENGTH),
-                (
-                    "grpc.max_receive_message_length",
-                    GRPC.MAX_RECEIVE_MESSAGE_LENGTH,
-                ),
-            ],
-        )
-
-        pserver_servicer = PserverServicer(
-            parameters,
-            grads_to_wait,
-            optimizer,
+        args = PserverArgs(
+            grads_to_wait=grads_to_wait,
             lr_staleness_modulation=lr_staleness_modulation,
             use_async=use_async,
+            port=self._port,
         )
-
-        elasticdl_pb2_grpc.add_PserverServicer_to_server(
-            pserver_servicer, server
-        )
-        server.add_insecure_port("[::]:{}".format(self._port))
-        return server
-
-    def create_stub(self):
-        stub = elasticdl_pb2_grpc.PserverStub(self._channel)
-        return stub
+        pserver = ParameterServer(args)
+        pserver.prepare()
+        self._server = pserver.server
+        self._stub = elasticdl_pb2_grpc.PserverStub(self._channel)
 
     def testServicer(self):
         parameters = Parameters()
         grads_to_wait = 8
-        optimizer = tf.keras.optimizers.SGD()
         lr_staleness_modulation = False
         use_async = True
 
-        server = self.create_server(
-            parameters,
-            grads_to_wait,
-            optimizer,
-            lr_staleness_modulation,
-            use_async,
+        self.create_server_and_stub(
+            parameters, grads_to_wait, lr_staleness_modulation, use_async
         )
-        server.start()
-
-        stub = self.create_stub()
 
         # TODO: replace the section below with real RPC service tests
         # after service implementation
         try:
             req = empty_pb2.Empty()
-            res = stub.pull_variable(req)
+            res = self._stub.pull_variable(req)
             self.assertEqual(res, req)
-            res = stub.pull_embedding_vector(req)
+            res = self._stub.pull_embedding_vector(req)
             self.assertEqual(res, req)
-            res = stub.push_model(req)
+            res = self._stub.push_model(req)
             self.assertEqual(res, req)
-            res = stub.push_gradient(req)
+            res = self._stub.push_gradient(req)
             self.assertEqual(res, req)
         except Exception:
             self.assertTrue(False)
-
-        server.stop(0)
