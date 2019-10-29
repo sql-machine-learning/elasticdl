b2901a55b800df74fb8de1d912b7c1e593428122
haitao <tenn_2001c@yahoo.com>
2019-10-29 16:50:54 +0800

    push_model rpc implementation

--- elasticdl/python/ps/servicer.py
+++ elasticdl/python/ps/servicer.py
@@ -1,3 +1,5 @@
+import threading
+
 from google.protobuf import empty_pb2
 
 from elasticdl.proto import elasticdl_pb2, elasticdl_pb2_grpc
@@ -20,6 +22,7 @@ class PserverServicer(elasticdl_pb2_grpc.PserverServicer):
         self._lr_staleness_modulation = lr_staleness_modulation
         self._use_async = use_async
         self._version = 0
+        self._lock = threading.Lock()
 
     def pull_variable(self, request, _):
         # TODO: implement this RPC service
@@ -30,7 +33,8 @@ class PserverServicer(elasticdl_pb2_grpc.PserverServicer):
         return elasticdl_pb2.Tensor()
 
     def push_model(self, request, _):
-        # TODO: implement this RPC service
+        with self._lock:
+            self._parameters.init_from_model_pb(request)
         return empty_pb2.Empty()
 
     def push_gradient(self, request, _):
