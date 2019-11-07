373f634ffb95d7b1bd380999ded802b87c25eb3c
terrytangyuan <terrytangyuan@gmail.com>
2019-11-07 09:53:36 -0500

    Properly set pipeline exit option when validating job status
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scripts/validate_job_status.sh
+++ scripts/validate_job_status.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+# We intentionally `set +e` here and then `set -e` right before we `exit` in this script since we
+# allow certain kinds of known failures that can be ignored, e.g. pods may not have been created yet
+# so neither `kubectl get pod` nor `kubectl delete pod` would be successful at earlier stages of
+# this script.
+set +e
+
 JOB_TYPE=$1
 MASTER_POD_NAME=elasticdl-test-${JOB_TYPE}-master
 WORKER_0_POD_NAME=elasticdl-test-${JOB_TYPE}-worker-0
@@ -21,6 +27,7 @@ for i in {1..200}; do
      [[ "$WORKER_1_POD_STATUS" == "Succeeded" ]]; then
       echo "ElasticDL job succeeded."
       kubectl delete pod ${MASTER_POD_NAME}
+      set -e
       exit 0
     elif [[ "$MASTER_POD_STATUS" == "Failed" ]] ||
        [[ "$WORKER_0_POD_STATUS" == "Failed" ]] ||
@@ -34,6 +41,7 @@ for i in {1..200}; do
       echo "\nWorker1 log:\n"
       kubectl logs ${WORKER_1_POD_NAME} | tail
       kubectl delete pod ${MASTER_POD_NAME}
+      set -e
       exit 1
     else
       echo "Master: ${MASTER_POD_STATUS}, Worker0: ${WORKER_0_POD_STATUS}, Worker1: ${WORKER_1_POD_STATUS}. Continue checking..."
@@ -45,4 +53,5 @@ echo "ElasticDL job timed out."
 
 kubectl delete pod ${MASTER_POD_NAME}
 
+set -e
 exit 1
