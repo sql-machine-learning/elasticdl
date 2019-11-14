4a10596e755e6230f3a70b76cf3db03c276aea1b
terrytangyuan <terrytangyuan@gmail.com>
2019-11-14 10:44:44 -0500

    Add check for empty string for ODPS endpoint
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- scripts/client_test.sh
+++ scripts/client_test.sh
@@ -70,7 +70,7 @@ elif [[ "$JOB_TYPE" == "odps" ]]; then
       --model_def=odps_iris_dnn_model.odps_iris_dnn_model.custom_model \
       --training_data=$ODPS_TABLE_NAME \
       --data_reader_params='columns=["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]' \
-      --envs="ODPS_PROJECT_NAME=$ODPS_PROJECT_NAME,ODPS_ACCESS_ID=$ODPS_ACCESS_ID,ODPS_ACCESS_KEY=$ODPS_ACCESS_KEY" \
+      --envs="ODPS_PROJECT_NAME=$ODPS_PROJECT_NAME,ODPS_ACCESS_ID=$ODPS_ACCESS_ID,ODPS_ACCESS_KEY=$ODPS_ACCESS_KEY,ODPS_ENDPOINT=" \
       --num_epochs=2 \
       --master_resource_request="cpu=400m,memory=1024Mi" \
       --master_resource_limit="cpu=1,memory=2048Mi" \
