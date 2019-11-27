367042e17ba3823e89441a70c5c1d37ae4adc51f
workingloong <WangQL1201@outlook.com>
2019-11-27 14:17:35 +0800

    add partition args for ODPSDataReader

--- elasticdl/python/data/data_reader.py
+++ elasticdl/python/data/data_reader.py
@@ -157,6 +157,7 @@ class ODPSDataReader(AbstractDataReader):
             access_key=self._kwargs["access_key"],
             table=table_name,
             endpoint=self._kwargs.get("endpoint"),
+            partition=self._kwargs.get("partition", None),
             num_processes=self._kwargs.get("num_processes", 1),
         )
 
