a6120c25fdeafbc70037c6a54ac93e4df979c981
brightcoder01 <55301748+brightcoder01@users.noreply.github.com>
2019-12-02 19:55:25 +0800

    BUGFIX: generate_mock_embedding_vectors will fail if id >= (dimension of embedding) (#1541)
    * BUGFIX: generate_mock_embedding_vectors will fail if id >= (dimension of embedding)
    
    * Resolve pre-commit error

--- elasticdl/python/tests/feature_column_test.py
+++ elasticdl/python/tests/feature_column_test.py
@@ -12,9 +12,15 @@ def call_feature_columns(feature_columns, input):
 
 
 def generate_mock_embedding_vectors(ids, dimension):
-    identity_matrix = np.identity(dimension)
+    if isinstance(ids, tf.Tensor):
+        ids = ids.numpy()
 
-    return np.array([identity_matrix[id] for id in ids])
+    return np.array(
+        [
+            (np.arange(dimension) == (id % dimension)).astype(np.int)
+            for id in ids
+        ]
+    )
 
 
 class EmbeddingColumnTest(unittest.TestCase):
