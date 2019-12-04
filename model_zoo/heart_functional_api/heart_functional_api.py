66df66c1f010b94a8a27600759e29dd582138dc4
brightcoder01 <bright.gong@outlook.com>
2019-12-05 07:17:06 +0800

    Make sure the FeatureInputLayer and FeatureColumns are sorted

--- model_zoo/heart_functional_api/heart_functional_api.py
+++ model_zoo/heart_functional_api/heart_functional_api.py
@@ -5,25 +5,22 @@ def get_feature_columns_and_inputs():
     feature_columns = []
     feature_input_layers = {}
 
-    '''
-    for header in ['trestbps', 'chol', 'thalach', 'slope', 'ca']:
+    for header in ['trestbps', 'chol', 'thalach', 'oldpeak', 'slope', 'ca']:
         feature_columns.append(tf.feature_column.numeric_column(header))
         feature_input_layers[header] = tf.keras.Input(shape=(1,), name=header)
-    '''
 
     age = tf.feature_column.numeric_column('age')
     age_buckets = tf.feature_column.bucketized_column(age, boundaries=[18, 25, 30, 35, 40, 45, 50, 55, 60, 65])
     feature_columns.append(age_buckets)
     feature_input_layers['age'] = tf.keras.Input(shape=(1,), name='age')
 
-    for header in ['chol', 'ca']:
-        feature_columns.append(tf.feature_column.numeric_column(header))
-        feature_input_layers[header] = tf.keras.Input(shape=(1,), name=header)
-
-    thal_hashed = tf.feature_column.categorical_column_with_hash_bucket('thal', hash_bucket_size=100, dtype=tf.string)
+    thal_hashed = tf.feature_column.categorical_column_with_hash_bucket('thal', hash_bucket_size=100)
     thal_embedding = tf.feature_column.embedding_column(thal_hashed, dimension=8)
     feature_columns.append(thal_embedding)
     feature_input_layers['thal'] = tf.keras.Input(shape=(1,), name='thal', dtype=tf.string)
+
+    feature_columns = sorted(feature_columns, key=lambda x: x.name)
+    feature_input_layers = dict(sorted(feature_input_layers.items(), key=lambda x: x[0]))
 
     return feature_columns, feature_input_layers
 
@@ -55,38 +52,15 @@ def optimizer(lr=0.1):
 
 def dataset_fn(dataset, mode, _):
     def _parse_data(record):
-        '''
+
         feature_description = {
             "age": tf.io.FixedLenFeature([], tf.int64),
-            "sex": tf.io.FixedLenFeature([], tf.int64),
-            "cp": tf.io.FixedLenFeature([], tf.int64),
             "trestbps": tf.io.FixedLenFeature([], tf.int64),
             "chol": tf.io.FixedLenFeature([], tf.int64),
-            "fbs": tf.io.FixedLenFeature([], tf.int64),
-            "restecg": tf.io.FixedLenFeature([], tf.int64),
             "thalach": tf.io.FixedLenFeature([], tf.int64),
-            "exang": tf.io.FixedLenFeature([], tf.int64),
             "oldpeak": tf.io.FixedLenFeature([], tf.float32),
             "slope": tf.io.FixedLenFeature([], tf.int64),
             "ca": tf.io.FixedLenFeature([], tf.int64),
-            "thal": tf.io.FixedLenFeature([], tf.string),
-            "target": tf.io.FixedLenFeature([], tf.int64),
-        }
-        feature_description = {
-            "trestbps": tf.io.FixedLenFeature([], tf.int64),
-            "chol": tf.io.FixedLenFeature([], tf.int64),
-            "thalach": tf.io.FixedLenFeature([], tf.int64),
-            "slope": tf.io.FixedLenFeature([], tf.int64),
-            "ca": tf.io.FixedLenFeature([], tf.int64),
-            "age": tf.io.FixedLenFeature([], tf.int64),
-            "thal": tf.io.FixedLenFeature([], tf.string),
-            "target": tf.io.FixedLenFeature([], tf.int64),
-        }
-        '''
-        feature_description = {
-            "age": tf.io.FixedLenFeature([], tf.int64),
-            "ca": tf.io.FixedLenFeature([], tf.int64),
-            "chol": tf.io.FixedLenFeature([], tf.int64),
             "thal": tf.io.FixedLenFeature([], tf.string),
             "target": tf.io.FixedLenFeature([], tf.int64),
         }
