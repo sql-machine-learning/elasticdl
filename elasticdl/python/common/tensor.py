89b7d98637ca5e9bffcabbf467db1712acccd988
shijungg <497602391@qq.com>
2019-12-15 17:41:56 +0800

    Override __add__ and __iadd__ in Tensor

--- elasticdl/python/common/tensor.py
+++ elasticdl/python/common/tensor.py
@@ -89,7 +89,7 @@ class Tensor(object):
             )
         return self.values
 
-    def __add__(self, other):
+    def __iadd__(self, other):
         if self.is_indexed_slices() and other.is_indexed_slices():
             self.values = np.concatenate((self.values, other.values), axis=0)
             self.indices = np.concatenate(
@@ -102,6 +102,21 @@ class Tensor(object):
                 "Only Tensor with the same type could be added"
             )
         return self
+
+    def __add__(self, other):
+        indices = None
+        if self.is_indexed_slices() and other.is_indexed_slices():
+            values = np.concatenate((self.values, other.values), axis=0)
+            indices = np.concatenate(
+                (self.indices, other.indices), axis=0
+            )
+        elif not self.is_indexed_slices() and not other.is_indexed_slices():
+            values = self.values + other.values
+        else:
+            raise NotImplementedError(
+                "Only Tensor with the same type could be added"
+            )
+        return Tensor(values, indices)
 
     def __radd__(self, other):
         return self + other
