4e329c90fb3ec6310861ccd53a86496677e2f9ff
terrytangyuan <terrytangyuan@gmail.com>
2019-12-19 13:47:54 -0500

    Switch to use allreduce implementation from FTLib
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

--- elasticdl/python/collective_ops/communicator.py
+++ elasticdl/python/collective_ops/communicator.py
@@ -1,17 +1,52 @@
-# TODO: This is dummy for now until the real implementation
-# has been open sourced
-
 from elasticdl.python.common.constants import CollectiveCommunicatorStatus
+from elasticdl.python.common.log_utils import default_logger as logger
+
+try:
+    from ftlib import BasicFTLib
+    from ftlib.ftlib_status import FTAllReduceStatus
+
+    _FTLIB_INSTALLED = True
+except ImportError:
+    BasicFTLib = object
+    FTAllReduceStatus = object
+    _FTLIB_INSTALLED = False
+
+
+_SUPPORTED_ALLREDUCE_OPS = ["MEAN"]
 
 
 class CollectiveCommunicator(object):
     def __init__(self):
-        pass
+        if _FTLIB_INSTALLED:
+            self._ftlib = BasicFTLib()
+            self._ftlib.init(
+                consensus="shared_storage", framework="dummy_NCCL"
+            )
+        else:
+            self._ftlib = None
 
     def allreduce(self, data, op="MEAN"):
         if data is None:
+            logger.error("Data is required for allreduce operation")
             return CollectiveCommunicatorStatus.FAILED, data
-        return CollectiveCommunicatorStatus.SUCCEEDED, data
+        if op not in _SUPPORTED_ALLREDUCE_OPS:
+            logger.error(
+                "%s is not in list of supported allreduce operations: %s"
+                % (op, _SUPPORTED_ALLREDUCE_OPS)
+            )
+            return CollectiveCommunicatorStatus.FAILED, data
+        if self._ftlib is not None:
+            res = self._ftlib.allreduce_average(data)
+            if res == FTAllReduceStatus.SUCCESS:
+                return CollectiveCommunicatorStatus.SUCCEEDED, data
+            else:
+                return CollectiveCommunicatorStatus.FAILED, data
+        else:
+            logger.warning(
+                "FTLib is not installed. "
+                "Default to succeeded for testing purposes"
+            )
+            return CollectiveCommunicatorStatus.SUCCEEDED, data
 
     def broadcast(self, data, root_ip):
         return CollectiveCommunicatorStatus.SUCCEEDED, data
