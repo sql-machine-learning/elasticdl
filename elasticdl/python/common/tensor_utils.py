9f60ebbfebb3211ae6f932469d41658aafa8efb8
brightcoder01 <bright.gong@outlook.com>
2019-12-25 15:17:54 +0800

    Do some rephrase

--- elasticdl/python/common/tensor_utils.py
+++ elasticdl/python/common/tensor_utils.py
@@ -9,18 +9,21 @@ def merge_indexed_slices(*args):
 
 
 def deduplicate_indexed_slices(values, indices):
-    """Sums `values` associated with any non-unique `indices`.
+    """
+    Sum up the values associated with duplicated indices in the IndexedSlices.
     Args:
-        values: A `Tensor` with rank >= 1.
-        indices: A one-dimensional integer `Tensor`, indexing into the first
-        dimension of `values` (as in an IndexedSlices object).
+        values: A Tensor with rank >= 1. Particularly IndexedSlices.values.
+        indices: A one-dimension integer of Tensor. Particularly 
+        IndexedSlices.indices.
     Returns:
-        A tuple of (`summed_values`, `unique_indices`) where `unique_indices`
-        is a de-duplicated version of `indices` and `summed_values` contains
-        the sum of `values` slices associated with each unique index.
+        A tuple of (`sum_combined_values`, `unique_indices`).
+        `sum_combined_values` contains the sum of `values` associated
+        with each unique indice.
+        `unique_indices` is a de-duplicated version of `indices`.
     """
     unique_indices, new_index_positions = tf.unique(indices)
-    summed_values = tf.math.unsorted_segment_sum(
+    sum_combined_values = tf.math.unsorted_segment_sum(
         values, new_index_positions, tf.shape(unique_indices)[0]
     )
-    return (summed_values, unique_indices)
+
+    return (sum_combined_values, unique_indices)
