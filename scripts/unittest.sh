6c7b277790e3a1639fdc382b0573b8acf6384523
qijun <qijun1994@hotmail.com>
2020-01-15 19:30:38 +0800

    fix ci

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -1,10 +1,11 @@
 make -f elasticdl/Makefile
 
+# Go unittests
+cd elasticdl
+go install ./...
+go test ./...
+cd ..
+
 # Python unittests
 K8S_TESTS=True pytest elasticdl/python/tests --cov=elasticdl/python --cov-report=xml
 mv coverage.xml shared
-
-
-# Go unittests
-go install ./...
-go test ./...
