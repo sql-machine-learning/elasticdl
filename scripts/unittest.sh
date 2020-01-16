e7b513308225edba273b67eda25eed1716f084d7
qijun <qijun1994@hotmail.com>
2020-01-16 14:25:13 +0800

    update

--- scripts/unittest.sh
+++ scripts/unittest.sh
@@ -4,12 +4,7 @@ set -e
 make -f elasticdl/Makefile
 
 # Go unittests
-rm -rf $GOPATH/src/elasticdl.org/elasticdl
-mkdir -p $GOPATH/src/elasticdl.org/elasticdl
-cp -r elasticdl/pkg $GOPATH/src/elasticdl.org/elasticdl
 pushd $GOPATH/src/elasticdl.org/elasticdl
-export GO111MODULE=on
-go mod init elasticdl.org/elasticdl
 go install ./...
 go test ./...
 popd
