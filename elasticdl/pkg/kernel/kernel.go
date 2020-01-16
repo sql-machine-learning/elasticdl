94059f94f3d203cf607be6ccda252b24d998fae4
qijun <qijun1994@hotmail.com>
2020-01-16 19:24:41 +0800

    fix code style

--- elasticdl/pkg/kernel/kernel.go
+++ elasticdl/pkg/kernel/kernel.go
@@ -5,8 +5,9 @@ package kernel
 import "C"
 import "unsafe"
 
+// SGD kernel
 func SGD(grad []float32, param []float32, lr float64, size int64) {
-    gradPtr := (*C.float)(unsafe.Pointer(&grad[0]))
-    paramPtr := (*C.float)(unsafe.Pointer(&param[0]))
-    C.SGD(gradPtr, paramPtr, C.double(lr), C.longlong(size))
+	gradPtr := (*C.float)(unsafe.Pointer(&grad[0]))
+	paramPtr := (*C.float)(unsafe.Pointer(&param[0]))
+	C.SGD(gradPtr, paramPtr, C.double(lr), C.longlong(size))
 }
