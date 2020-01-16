4e9d71908ccfb9bd9aa2b1078d50e23ace0ba1e9
qijun <qijun1994@hotmail.com>
2020-01-16 11:13:50 +0800

    test import

--- elasticdl/pkg/common/tensor.go
+++ elasticdl/pkg/common/tensor.go
@@ -1,8 +1,14 @@
 package common
 
+import "elasticdl/proto"
+
 type Tensor struct {
 	name    string
 	value   []float32
 	dim     []int64
 	indices []int64
+}
+
+
+func deserializeTensorPB(proto.Tensor* pb, Tensor* tensor) {
 }
