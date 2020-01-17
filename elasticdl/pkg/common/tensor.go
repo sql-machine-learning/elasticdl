1b82cd3a8c19ea39cffb85f8a5ce08591f4251de
qijun <qijun1994@hotmail.com>
2020-01-17 14:55:55 +0800

    refine code style

--- elasticdl/pkg/common/tensor.go
+++ elasticdl/pkg/common/tensor.go
@@ -16,17 +16,24 @@ type Tensor struct {
 	Indices []int64
 }
 
-// GetTensorSize returns the size of a tensor
-func GetTensorSize(t *Tensor) int64 {
+// GetDimProduct returns the product of a slice of dim
+func GetDimProduct(dim []int64) int64 {
 	var size int64 = 1
-	for _, d := range t.Dim {
+	for _, d := range dim {
 		size *= d
 	}
 	return size
 }
 
 // DeserializeTensorPB transforms pb to tensor
-func DeserializeTensorPB(pb *proto.Tensor, t *Tensor) {
+func DeserializeTensorPB(pb *proto.Tensor) *Tensor {
+	var t Tensor
+	if pb.Dtype != proto.TensorDtype_DT_FLOAT32 {
+		return nil
+	}
+	if GetDimProduct(pb.Dim)*4 != int64(len(pb.Content)) {
+		return nil
+	}
 	t.Name = pb.Name
 	t.Dim = make([]int64, len(pb.Dim))
 	copy(t.Dim, pb.Dim)
@@ -35,20 +42,23 @@ func DeserializeTensorPB(pb *proto.Tensor, t *Tensor) {
 	t.Value = make([]float32, len(pb.Content)/4)
 	br := bytes.NewReader(pb.GetContent())
 	binary.Read(br, binary.LittleEndian, &t.Value)
+	return &t
 }
 
 // SerializeTensor transforms tensor to pb
-func SerializeTensor(t *Tensor, pb *proto.Tensor) {
+func SerializeTensor(t *Tensor) *proto.Tensor {
+	var pb proto.Tensor
 	pb.Name = t.Name
 	pb.Dim = make([]int64, len(t.Dim))
 	copy(pb.Dim, t.Dim)
 	pb.Indices = make([]int64, len(t.Indices))
 	copy(pb.Indices, t.Indices)
-	pb.Content = make([]byte, GetTensorSize(t)*4)
+	pb.Content = make([]byte, GetDimProduct(t.Dim)*4)
 	for i, num := range t.Value {
 		bits := math.Float32bits(num)
 		binary.LittleEndian.PutUint32(pb.Content[(i*4):], bits)
 	}
 	// set dtype to float32
-	pb.Dtype = 6
+	pb.Dtype = proto.TensorDtype_DT_FLOAT32
+	return &pb
 }
--- elasticdl/pkg/common/tensor_test.go
+++ elasticdl/pkg/common/tensor_test.go
@@ -24,11 +24,10 @@ func TestTensorSerialization(t *testing.T) {
 	v1 := []float32{1.0, 2.0, 3.0, 4.0, 5.0, 6.0}
 	t1 := Tensor{"t1", v1, d1, i1}
 
-	var p proto.Tensor
-	SerializeTensor(&t1, &p)
+	var p *proto.Tensor = SerializeTensor(&t1)
+	assert.NotNil(t, p)
 
-	var t2 Tensor
-	DeserializeTensorPB(&p, &t2)
+	var t2 *Tensor = DeserializeTensorPB(p)
 
-	assert.Equal(t, t1, t2)
+	assert.Equal(t, t1, *t2)
 }
