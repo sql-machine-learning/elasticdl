02fa6b0e05d76f3ef29cc6f4a8dc6b8829cd9726
qijun <qijun1994@hotmail.com>
2020-01-22 11:07:40 +0800

    add sparse sgd kernel

--- elasticdl/pkg/kernel/capi/kernel_api.cc
+++ elasticdl/pkg/kernel/capi/kernel_api.cc
@@ -2,7 +2,7 @@
 
 #include <eigen3/Eigen/Dense>
 
-void SGD(float* grad, float* param, double lr, long long size) {
+void SGD(float* grad, float* param, float lr, long long size) {
   Eigen::Map<Eigen::Array<float, 1, Eigen::Dynamic>> eg{
       grad, static_cast<Eigen::Index>(size)};
 
--- elasticdl/pkg/kernel/kernel.go
+++ elasticdl/pkg/kernel/kernel.go
@@ -4,10 +4,33 @@ package kernel
 // #include "capi/kernel_api.h"
 import "C"
 import "unsafe"
+import "elasticdl.org/elasticdl/pkg/common"
+import "fmt"
 
 // SGD kernel
-func SGD(grad []float32, param []float32, lr float64, size int64) {
-	gradPtr := (*C.float)(unsafe.Pointer(&grad[0]))
-	paramPtr := (*C.float)(unsafe.Pointer(&param[0]))
-	C.SGD(gradPtr, paramPtr, C.double(lr), C.longlong(size))
+func SGD(grad *common.Tensor, param *common.Tensor, lr float32) error {
+	if len(grad.Value) != len(param.Value) {
+		return fmt.Errorf("grad %s Value size not equal to param", grad.Name)
+	}
+	gradPtr := (*C.float)(unsafe.Pointer(&grad.Value[0]))
+	paramPtr := (*C.float)(unsafe.Pointer(&param.Value[0]))
+	C.SGD(gradPtr, paramPtr, C.float(lr), C.longlong(len(grad.Value)))
+	return nil
+}
+
+// SparseSGD kernel
+func SparseSGD(grad *common.Tensor, param *common.EmbeddingTable, lr float32) error {
+	if grad.Indices == nil || len(grad.Dim) != 2 {
+		return fmt.Errorf("grad %s is not row sparse tensor", grad.Name)
+	}
+	if grad.Dim[1] != param.Dim {
+		return fmt.Errorf("grad %s width is not equal to embedding dim", grad.Name)
+	}
+	for i, index := range grad.Indices {
+		vector := param.GetEmbeddingVector(index)
+		for j := range vector {
+			vector[j] -= lr * grad.Value[j+i*int(param.Dim)]
+		}
+	}
+	return nil
 }
