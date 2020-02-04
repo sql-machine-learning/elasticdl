8bafdc4d65a810e43b45aa8137c7be59174bd727
qijun <qijun1994@hotmail.com>
2020-02-04 14:59:35 +0800

    fix ci

--- elasticdl/pkg/ps/server_test.go
+++ elasticdl/pkg/ps/server_test.go
@@ -205,7 +205,7 @@ func TestPushGradient(t *testing.T) {
 	epb.Dim = 2
 	epb.Initializer = "zero"
 
-	ev := []float32{1.0, 2.0, 3.0, 4.0, 6.0, 7.0, 8.0}
+	ev := []float32{1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0}
 	ed := []int64{4, 2}
 	ei := []int64{0, 1, 2, 3}
 	e1 := common.Tensor{"e1", ev, ed, ei}
