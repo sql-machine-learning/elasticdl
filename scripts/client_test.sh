92e1e00b581b99b93bbb1dd8df313c426121b804
workingloong <WangQL1201@outlook.com>
2020-02-14 20:58:39 +0800

    Genearte checkpoint for test automatically

--- scripts/client_test.sh
+++ scripts/client_test.sh
@@ -4,6 +4,10 @@ JOB_TYPE=$1
 PS_NUM=$2
 WORKER_NUM=$3
 
+# Generate checkpoint for mnist to test evaluation and prediction
+MNIST_CKPT_DIR=${PWD}/mnist/
+python gen_mnist_checkpoint.py --checkpoint_dir=${MNIST_CKPT_DIR}
+
 if [[ "$JOB_TYPE" == "train" ]]; then
     elasticdl train \
       --image_base=elasticdl:ci \
@@ -37,7 +41,7 @@ elif [[ "$JOB_TYPE" == "evaluate" ]]; then
       --image_base=elasticdl:ci \
       --model_zoo=model_zoo \
       --model_def=mnist_functional_api.mnist_functional_api.custom_model \
-      --checkpoint_dir_for_init=elasticdl/python/tests/testdata/mnist_functional_api_model/version-110  \
+      --checkpoint_dir_for_init=/saved_model/mnist/version-100  \
       --validation_data=/data/mnist/test \
       --num_epochs=1 \
       --master_resource_request="cpu=0.3,memory=1024Mi" \
@@ -54,13 +58,14 @@ elif [[ "$JOB_TYPE" == "evaluate" ]]; then
       --tensorboard_log_dir=/tmp/tensorboard-log \
       --job_name=test-evaluate \
       --log_level=INFO \
-      --image_pull_policy=Never
+      --image_pull_policy=Never \
+      --volume="host_path=${PWD},mount_path=/saved_model"
 elif [[ "$JOB_TYPE" == "predict" ]]; then
     elasticdl predict \
       --image_base=elasticdl:ci \
       --model_zoo=model_zoo \
       --model_def=mnist_functional_api.mnist_functional_api.custom_model \
-      --checkpoint_dir_for_init=elasticdl/python/tests/testdata/mnist_functional_api_model/version-110 \
+      --checkpoint_dir_for_init=/saved_model/mnist/version-100 \
       --prediction_data=/data/mnist/test \
       --master_resource_request="cpu=0.2,memory=1024Mi" \
       --master_resource_limit="cpu=1,memory=2048Mi" \
@@ -74,7 +79,8 @@ elif [[ "$JOB_TYPE" == "predict" ]]; then
       --num_ps_pods=$PS_NUM \
       --job_name=test-predict \
       --log_level=INFO \
-      --image_pull_policy=Never
+      --image_pull_policy=Never \
+      --volume="host_path=${PWD},mount_path=/saved_model"
 elif [[ "$JOB_TYPE" == "local" ]]; then
     elasticdl train \
       --model_zoo=model_zoo \
