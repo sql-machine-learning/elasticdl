6e0ead5f1a4eb50410eec7306925f0bff93b40dc
liwen <lw210205@antfin.com>
2020-02-21 15:54:41 +0800

    fix ci

--- elasticdl/docker/scripts/install-go.bash
+++ elasticdl/docker/scripts/install-go.bash
@@ -8,3 +8,13 @@ curl --silent ${GO_MIRROR_URL}/go1.13.4.linux-amd64.tar.gz | tar -C /usr/local -
 
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.io,direct
+
+go get github.com/golang/protobuf/protoc-gen-go	
+go get golang.org/x/lint/golint	
+go get golang.org/x/tools/cmd/goyacc	
+go get golang.org/x/tools/cmd/cover	
+go get github.com/mattn/goveralls	
+go get github.com/rakyll/gotest	
+go get github.com/wangkuiyi/goyaccfmt
+
+cp $GOPATH/bin/* /usr/local/bin/
