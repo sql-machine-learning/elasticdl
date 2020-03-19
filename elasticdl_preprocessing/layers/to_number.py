f30cfa630bf180311519afa2a418c7f93bf99c94
brightcoder01 <55301748+brightcoder01@users.noreply.github.com>
2020-03-19 14:43:41 +0800

    Add the ToNumber preprocess layer. (#1845)
    * Add the Preprocessing folder in ElasticDL and README.md
    
    * Do some rephrase
    
    * Add the initial version of to_number and to_sparse
    
    * Build the directory structure
    
    * Add the ToNumber preprocessing layer
    
    * Add test_utils in elasticdl_preprocessing tests

---
+++ elasticdl_preprocessing/layers/to_number.py
@@ -0,0 +1,55 @@
+import tensorflow as tf
+
+_NUMBER_DTYPES = [
+    tf.int8,
+    tf.uint8,
+    tf.int16,
+    tf.uint16,
+    tf.int32,
+    tf.uint32,
+    tf.int64,
+    tf.uint64,
+    tf.float16,
+    tf.float32,
+    tf.float64,
+    tf.bfloat16,
+    tf.double,
+]
+
+
+class ToNumber(tf.keras.layers.Layer):
+    """Convert the inputs to a number dtype (int, float, double)
+
+    Input Shape: Tensor or SparseTensor of any shape
+    Output Shape: Tensor or SparseTensor of the same shape with input
+    """
+
+    def __init__(self, out_type, default_value):
+        super(ToNumber, self).__init__()
+        if out_type not in _NUMBER_DTYPES:
+            raise ValueError("{} is not a number type.".format(out_type))
+        self.out_type = out_type
+        self.default_value = default_value
+
+    def call(self, inputs):
+        if isinstance(inputs, tf.SparseTensor):
+            number_value = self._cast_dense_to_number(inputs.values)
+            return tf.SparseTensor(
+                indices=inputs.indices,
+                values=number_value,
+                dense_shape=inputs.dense_shape,
+            )
+        else:
+            return self._cast_dense_to_number(inputs)
+
+    def _cast_dense_to_number(self, dense_inputs):
+        if dense_inputs.dtype is tf.string:
+            default_value = str(self.default_value)
+            outputs = tf.where(
+                tf.equal(dense_inputs, ""), x=default_value, y=dense_inputs
+            )
+            outputs = tf.strings.to_number(outputs, out_type=self.out_type)
+        else:
+            outputs = tf.cast(dense_inputs, self.out_type)
+
+        return outputs
