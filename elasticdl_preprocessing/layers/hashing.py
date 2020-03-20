6aa22708472eacaf000fc6aa1180a78083f4e4f0
workingloong <WangQL1201@outlook.com>
2020-03-20 11:20:29 +0800

    Use TensorFlow public API

--- elasticdl_preprocessing/layers/hashing.py
+++ elasticdl_preprocessing/layers/hashing.py
@@ -1,12 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-from tensorflow.python.framework import dtypes, sparse_tensor, tensor_spec
-from tensorflow.python.keras.engine.base_layer import Layer
-from tensorflow.python.ops import string_ops
-from tensorflow.python.ops.ragged import ragged_functional_ops, ragged_tensor
+import tensorflow as tf
 
 
-class Hashing(Layer):
+class Hashing(tf.keras.layers.Layer):
     """Implements categorical feature hashing, also known as "hashing trick".
 
     This layer transforms categorical inputs to hashed output. It converts a
@@ -48,50 +45,38 @@ class Hashing(Layer):
     def call(self, inputs):
         # Converts integer inputs to string.
         if inputs.dtype.is_integer:
-            if isinstance(inputs, sparse_tensor.SparseTensor):
-                inputs = sparse_tensor.SparseTensor(
+            if isinstance(inputs, tf.SparseTensor):
+                inputs = tf.SparseTensor(
                     indices=inputs.indices,
-                    values=string_ops.as_string(inputs.values),
+                    values=tf.as_string(inputs.values),
                     dense_shape=inputs.dense_shape,
                 )
             else:
-                inputs = string_ops.as_string(inputs)
-        if ragged_tensor.is_ragged(inputs):
-            return ragged_functional_ops.map_flat_values(
-                string_ops.string_to_hash_bucket_fast,
+                inputs = tf.as_string(inputs)
+        if isinstance(inputs, tf.RaggedTensor):
+            return tf.ragged.map_flat_values(
+                tf.strings.to_hash_bucket_fast,
                 inputs,
                 num_buckets=self.num_bins,
                 name="hash",
             )
-        elif isinstance(inputs, sparse_tensor.SparseTensor):
+        elif isinstance(inputs, tf.SparseTensor):
             sparse_values = inputs.values
-            sparse_hashed_values = string_ops.string_to_hash_bucket_fast(
+            sparse_hashed_values = tf.strings.to_hash_bucket_fast(
                 sparse_values, self.num_bins, name="hash"
             )
-            return sparse_tensor.SparseTensor(
+            return tf.SparseTensor(
                 indices=inputs.indices,
                 values=sparse_hashed_values,
                 dense_shape=inputs.dense_shape,
             )
         else:
-            return string_ops.string_to_hash_bucket_fast(
+            return tf.strings.to_hash_bucket_fast(
                 inputs, self.num_bins, name="hash"
             )
 
     def compute_output_shape(self, input_shape):
         return input_shape
-
-    def compute_output_signature(self, input_spec):
-        output_shape = self.compute_output_shape(input_spec.shape.as_list())
-        output_dtype = dtypes.int64
-        if isinstance(input_spec, sparse_tensor.SparseTensorSpec):
-            return sparse_tensor.SparseTensorSpec(
-                shape=output_shape, dtype=output_dtype
-            )
-        else:
-            return tensor_spec.TensorSpec(
-                shape=output_shape, dtype=output_dtype
-            )
 
     def get_config(self):
         config = {"num_bins": self.num_bins}
