7d273e4bb593fdac0bb8b829dc316b35541d0209
workingloong <WangQL1201@outlook.com>
2020-03-21 09:39:14 +0800

    Fix the docstring by comments

--- elasticdl_preprocessing/layers/discretization.py
+++ elasticdl_preprocessing/layers/discretization.py
@@ -8,8 +8,8 @@ class Discretization(tf.keras.layers.Layer):
     """Buckets data into discrete ranges.
 
     TensorFlow 2.2 has developed `tf.keras.layers.preprocessing.Discretization`
-    but not released it yet. So the layer is a simple temporary version.
-    https://github.com/tensorflow/tensorflow/blob/r2.2/tensorflow/python/keras/layers/preprocessing/discretization.py
+    but not released it yet. So the layer is a simple temporary version 
+    `tensorflow.python.keras.layers.preprocessing.discretization.Discretization`
 
     Input shape:
         Any `tf.Tensor` or `tf.RaggedTensor` of dimension 2 or higher.
