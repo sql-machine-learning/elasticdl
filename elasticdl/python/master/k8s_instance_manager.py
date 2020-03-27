acb29e06798d1c52a6eaa84a312672845b603cdd
qijun <qijun1994@hotmail.com>
2020-03-27 11:17:46 +0800

    fix ci

--- elasticdl/python/master/k8s_instance_manager.py
+++ elasticdl/python/master/k8s_instance_manager.py
@@ -11,8 +11,11 @@ _SERVICE_ADDR_SEP = ","
 
 def _parse_worker_pod_priority(num_workers, worker_pod_priority):
     res = {}
-    if "high=" in worker_pod_priority:
-        fraction = float(worker_pod_priority.spllit("=")[1])
+    if worker_pod_priority is None:
+        for i in range(num_workers):
+            res[i] = None
+    elif "high=" in worker_pod_priority:
+        fraction = float(worker_pod_priority.split("=")[1])
         high_count = int(num_workers * fraction)
         for i in range(num_workers):
             if i < high_count:
@@ -22,9 +25,12 @@ def _parse_worker_pod_priority(num_workers, worker_pod_priority):
     elif worker_pod_priority == "high":
         for i in range(num_workers):
             res[i] = "high"
-    else:
+    elif worker_pod_priority == "low":
         for i in range(num_workers):
             res[i] = "low"
+    else:
+        for i in range(num_workers):
+            res[i] = worker_pod_priority
     return res
 
 
