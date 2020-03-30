6ee7c6eadc797b3fae6fd9d5dbce82e0266f2082
Qinlong Wang <WangQL1201@outlook.com>
2020-03-30 19:18:15 +0800

    Layer to convert tensor to ragged tensor dropping the ignored value (#1862)
    * Layer to convert tensor to ragged tensor
    
    * Remove annotation
    
    * Remove white space
    
    * Move the expected output form example codes
    
    * Check input type
    
    * Add the note with TensorFlow version
    
    * Reformat codes by pre-commit
    
    * Add unit test to create model with a ragged layer
    
    * Fix by comments
    
    * Don't split string tensor

---
+++ elasticdl_preprocessing/layers/to_ragged.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+
+_COMMA_SEP = ","
+
+
+class ToRagged(tf.keras.layers.Layer):
+    """Converts a `Tensor` or `RaggedTensor` to a `RaggedTensor`,
+    dropping ignore_value cells. If the input's dtype is string, split
+    the string elements to convert the input to `RaggedTensor` firstly.
+
+    Note that the TensorFlow version with the layer must be greater than 2.0.0.
+
+    Example (Integer):
+    ```python
+        layer = ToRagged()
+        input_tensor = tf.constant([[1], [-1], [4]], tf.int64)
+        out = layer(input_tensor)
+        [[1], [], [4]]
+    ```
+
+    Example (String):
+    ```python
+        layer = ToRagged()
+        input_tensor = tf.constant([["1", "2", "3"], ["4", "5"], [""]])
+        out = layer(input_tensor)
+    ```
+    The expected output is `[["1", "2", "3"], ["4", "5"], []]`
+
+    Arguments:
+        sep: Valid if the input's dtype is string.
+        ignore_value: Entries in inputs equal to this value will be
+            absent from the output `RaggedTensor`. If `None`, default value of
+            input's dtype will be used ('' for `str`, -1 for `int`).
+
+    Input shape: A numeric or string `Tensor` or `RaggedTensor` of shape
+        `[batch_size, d1, ..., dm]`
+
+    Output shape: An `RaggedTensor` with the same shape as inputs
+    """
+
+    def __init__(self, sep=_COMMA_SEP, ignore_value=None):
+        super(ToRagged, self).__init__()
+        self.sep = sep
+        self.ignore_value = ignore_value
+
+    def call(self, inputs):
+        if not isinstance(inputs, (tf.Tensor, tf.RaggedTensor)):
+            raise TypeError(
+                "The inputs must be a Tensor or RaggedTensor and "
+                "the type of inputs is {}".format(type(inputs))
+            )
+
+        if isinstance(inputs, tf.Tensor):
+            inputs = tf.RaggedTensor.from_tensor(inputs)
+
+        ignore_value = self._get_ignore_value(inputs.dtype)
+        if ignore_value is None:
+            return inputs
+        else:
+            return tf.ragged.boolean_mask(
+                inputs, tf.not_equal(inputs, ignore_value)
+            )
+
+    def _get_ignore_value(self, input_dtype):
+        ignore_value = self.ignore_value
+
+        if ignore_value is None:
+            if input_dtype == tf.string:
+                ignore_value = ""
+            elif input_dtype.is_integer:
+                ignore_value = -1
+            else:
+                return None
+
+        return tf.cast(ignore_value, input_dtype)
---
+++ elasticdl_preprocessing/tests/to_ragged_test.py
@@ -0,0 +1,34 @@
+import unittest
+
+import tensorflow as tf
+
+from elasticdl_preprocessing.layers.to_ragged import ToRagged
+from elasticdl_preprocessing.tests.test_utils import ragged_tensor_equal
+
+
+class ToRaggedTest(unittest.TestCase):
+    def test_dense_to_ragged(self):
+        layer = ToRagged()
+        input_data = tf.constant([[1], [-1], [4]], tf.int64)
+        out = layer(input_data)
+        expected_out = tf.ragged.constant([[1], [], [4]], tf.int64)
+        self.assertTrue(ragged_tensor_equal(out, expected_out))
+
+    def test_string_split_to_ragged(self):
+        layer = ToRagged()
+        input_data = tf.ragged.constant([["1", "2", "3"], ["4", "5"], [""]])
+        out = layer(input_data)
+        expected_out = tf.ragged.constant([["1", "2", "3"], ["4", "5"], []])
+        self.assertTrue(ragged_tensor_equal(out, expected_out))
+
+    def test_model_with_ragged(self):
+        inputs = tf.keras.Input(shape=(1,), dtype=tf.int32)
+        ragged = ToRagged(ignore_value=-1)(inputs)
+        sum_out = tf.reduce_sum(ragged)
+        model = tf.keras.Model(inputs=inputs, outputs=sum_out)
+        sum_value = model.call(tf.constant([[1], [-1], [4]])).numpy()
+        self.assertEqual(sum_value, 5.0)
+
+
+if __name__ == "__main__":
+    unittest.main()
