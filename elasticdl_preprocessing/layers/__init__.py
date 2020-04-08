cba3150c7db54bfc88d3226703017da03f803ccb
Qinlong Wang <WangQL1201@outlook.com>
2020-04-08 19:48:26 +0800

    import preprocessing layers into layer package (#1902)

---
+++ elasticdl_preprocessing/layers/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+from elasticdl_preprocessing.layers.concatenate_with_offset import (
+    ConcatenateWithOffset,
+)
+from elasticdl_preprocessing.layers.discretization import Discretization
+from elasticdl_preprocessing.layers.hashing import Hashing
+from elasticdl_preprocessing.layers.index_lookup import IndexLookup
+from elasticdl_preprocessing.layers.round_identity import RoundIdentity
+from elasticdl_preprocessing.layers.to_number import ToNumber
+from elasticdl_preprocessing.layers.to_ragged import ToRagged
+from elasticdl_preprocessing.layers.to_sparse import ToSparse
