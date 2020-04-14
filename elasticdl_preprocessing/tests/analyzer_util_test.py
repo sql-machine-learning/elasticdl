e705790dc2d47ebd2c8f892efbc70043e22a9eda
workingloong <WangQL1201@outlook.com>
2020-04-14 15:54:02 +0800

    Add unit test for analyzer_util

---
+++ elasticdl_preprocessing/tests/analyzer_util_test.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+import tempfile
+
+from elasticdl_preprocessing.utils import analyzer_util
+
+
+class AnalyzerUtilTest(unittest.TestCase):
+
+    def test_methods_from_environment(self):
+        # using default value
+        self.assertEqual(analyzer_util.get_mean("age", 10), 10)
+        self.assertEqual(analyzer_util.get_stddev("age", 10), 10)
+        self.assertListEqual(
+            analyzer_util.get_bucket_boundaries("age", [10, 78]),
+            [10, 78]
+        )
+        self.assertEqual(analyzer_util.get_value_count("city", 19), 19)
+        self.assertListEqual(
+            analyzer_util.get_vocabulary("city", ["a", 'b']),
+            ["a", "b"]
+        )
+
+        # Get value from environment
+        os.environ["age_mean"] = "50.9"
+        os.environ["age_stddev"] = "90.87"
+        os.environ['age_bkt_boundaries'] = "15,67,89"
+        os.environ['city_count'] = "50"
+
+        self.assertEqual(analyzer_util.get_mean("age", 10), 50.9)
+        self.assertEqual(analyzer_util.get_stddev("age", 10), 90.87)
+        self.assertListEqual(
+            analyzer_util.get_bucket_boundaries("age", [10, 78]),
+            [15, 67, 89]
+        )
+        self.assertEqual(analyzer_util.get_value_count("city", 19), 50)
+
+        with tempfile.TemporaryDirectory() as temp_dir:
+            vocab_path = os.path.join(temp_dir, "city.txt")
+
+            with open(vocab_path, "w") as f:
+                f.write("Beijing\n")
+                f.write("NewYork\n")
+            os.environ['city_vocab_path'] = vocab_path
+            self.assertListEqual(
+                analyzer_util.get_vocabulary("city", ["a", 'b']),
+                ["Beijing", "NewYork"]
+            )
