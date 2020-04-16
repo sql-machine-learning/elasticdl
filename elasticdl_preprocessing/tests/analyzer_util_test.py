e4202f54b3cd4e222bd8449b99a355574abde991
workingloong <WangQL1201@outlook.com>
2020-04-16 15:09:26 +0800

    Rename analyzer_util to analyzer_utils

--- elasticdl_preprocessing/tests/analyzer_util_test.py
+++ elasticdl_preprocessing/tests/analyzer_util_test.py
@@ -2,22 +2,22 @@ import os
 import tempfile
 import unittest
 
-from elasticdl_preprocessing.utils import analyzer_util
+from elasticdl_preprocessing.utils import analyzer_utils
 
 
 class AnalyzerUtilTest(unittest.TestCase):
     def test_methods_from_environment(self):
         # using default value
-        self.assertEqual(analyzer_util.get_min("age", 10), 10)
-        self.assertEqual(analyzer_util.get_max("age", 100), 100)
-        self.assertEqual(analyzer_util.get_avg("age", 10), 10)
-        self.assertEqual(analyzer_util.get_stddev("age", 10), 10)
+        self.assertEqual(analyzer_utils.get_min("age", 10), 10)
+        self.assertEqual(analyzer_utils.get_max("age", 100), 100)
+        self.assertEqual(analyzer_utils.get_avg("age", 10), 10)
+        self.assertEqual(analyzer_utils.get_stddev("age", 10), 10)
         self.assertListEqual(
-            analyzer_util.get_bucket_boundaries("age", [10, 78]), [10, 78]
+            analyzer_utils.get_bucket_boundaries("age", [10, 78]), [10, 78]
         )
-        self.assertEqual(analyzer_util.get_count("city", 19), 19)
+        self.assertEqual(analyzer_utils.get_count("city", 19), 19)
         self.assertListEqual(
-            analyzer_util.get_vocabulary("city", ["a", "b"]), ["a", "b"]
+            analyzer_utils.get_vocabulary("city", ["a", "b"]), ["a", "b"]
         )
 
         # Get value from environment
@@ -28,14 +28,14 @@ class AnalyzerUtilTest(unittest.TestCase):
         os.environ["age_bkt_boundaries"] = "15,67,89"
         os.environ["city_count"] = "50"
 
-        self.assertEqual(analyzer_util.get_min("age", 10), 11)
-        self.assertEqual(analyzer_util.get_max("age", 10), 100)
-        self.assertEqual(analyzer_util.get_avg("age", 10), 50.9)
-        self.assertEqual(analyzer_util.get_stddev("age", 10), 90.87)
+        self.assertEqual(analyzer_utils.get_min("age", 10), 11)
+        self.assertEqual(analyzer_utils.get_max("age", 10), 100)
+        self.assertEqual(analyzer_utils.get_avg("age", 10), 50.9)
+        self.assertEqual(analyzer_utils.get_stddev("age", 10), 90.87)
         self.assertListEqual(
-            analyzer_util.get_bucket_boundaries("age", [10, 78]), [15, 67, 89]
+            analyzer_utils.get_bucket_boundaries("age", [10, 78]), [15, 67, 89]
         )
-        self.assertEqual(analyzer_util.get_count("city", 19), 50)
+        self.assertEqual(analyzer_utils.get_count("city", 19), 50)
 
         with tempfile.TemporaryDirectory() as temp_dir:
             vocab_path = os.path.join(temp_dir, "city.txt")
@@ -45,6 +45,6 @@ class AnalyzerUtilTest(unittest.TestCase):
                 f.write("NewYork\n")
             os.environ["city_vocab_path"] = vocab_path
             self.assertListEqual(
-                analyzer_util.get_vocabulary("city", ["a", "b"]),
+                analyzer_utils.get_vocabulary("city", ["a", "b"]),
                 ["Beijing", "NewYork"],
             )
