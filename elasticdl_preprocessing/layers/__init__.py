ae06ba012e4bf089413bcbc7279838c731d366f5
Qinlong Wang <WangQL1201@outlook.com>
2020-04-20 19:24:32 +0800

    Add LogRound layer to convert numeric values to integer IDs (#1916)
    * Add LogRound layer to convert numeric values to integer ids
    
    * Rename unit test class for LogRound
    
    * Add test case with a zero input
    
    * Modify docstring

--- elasticdl_preprocessing/layers/__init__.py
+++ elasticdl_preprocessing/layers/__init__.py
@@ -7,6 +7,7 @@ from elasticdl_preprocessing.layers.concatenate_with_offset import (
 from elasticdl_preprocessing.layers.discretization import Discretization
 from elasticdl_preprocessing.layers.hashing import Hashing
 from elasticdl_preprocessing.layers.index_lookup import IndexLookup
+from elasticdl_preprocessing.layers.log_round import LogRound
 from elasticdl_preprocessing.layers.round_identity import RoundIdentity
 from elasticdl_preprocessing.layers.sparse_embedding import SparseEmbedding
 from elasticdl_preprocessing.layers.to_number import ToNumber
---
+++ elasticdl_preprocessing/tests/log_round_test.py
@@ -0,0 +1,48 @@
+import unittest
+
+import numpy as np
+import tensorflow as tf
+
+from elasticdl_preprocessing.layers import LogRound
+from elasticdl_preprocessing.tests.test_utils import (
+    ragged_tensor_equal,
+    sparse_tensor_equal,
+)
+
+
+class LogRoundTest(unittest.TestCase):
+    def test_round_indentity(self):
+        log_round = LogRound(num_bins=20, base=2)
+
+        dense_input = tf.constant([[0.0], [2.6], [0.2], [3.1], [1024]])
+        output = log_round(dense_input)
+        expected_out = np.array([[0], [1], [0], [2], [10]])
+
+        self.assertTrue(np.array_equal(output.numpy(), expected_out))
+
+        ragged_input = tf.ragged.constant([[1.1, 3.4], [1025]])
+        ragged_output = log_round(ragged_input)
+        expected_ragged_out = tf.ragged.constant(
+            [[0, 2], [10]], dtype=tf.int64
+        )
+        self.assertTrue(
+            ragged_tensor_equal(ragged_output, expected_ragged_out)
+        )
+
+        sparse_input = ragged_input.to_sparse()
+        sparse_output = log_round(sparse_input)
+        expected_sparse_out = expected_ragged_out.to_sparse()
+        self.assertTrue(
+            sparse_tensor_equal(sparse_output, expected_sparse_out)
+        )
+
+    def test_model_with_loground(self):
+        inputs = tf.keras.Input(shape=(1,), dtype=tf.string)
+        log_round = LogRound(num_bins=20, base=2)(inputs)
+        model = tf.keras.Model(inputs=inputs, outputs=log_round)
+        out = model.call(tf.constant([[1.2], [2.6], [0.2], [3.1], [1024]]))
+        self.assertTrue(
+            np.array_equal(
+                np.array([[0], [1], [0], [2], [10]], dtype=int), out.numpy()
+            )
+        )
