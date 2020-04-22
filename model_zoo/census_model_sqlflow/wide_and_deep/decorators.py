6b9112caa6113492f1dd50fc147545f6aee66291
brightcoder01 <bright.gong@outlook.com>
2020-04-22 10:13:11 +0800

    Check whether all the input names are string type.

--- model_zoo/census_model_sqlflow/wide_and_deep/decorators.py
+++ model_zoo/census_model_sqlflow/wide_and_deep/decorators.py
@@ -2,7 +2,7 @@ def model_input_name(input_names):
     def decorator(clz):
         if isinstance(input_names, str):
             setattr(clz, "_model_input_names", [input_names])
-        elif isinstance(input_names, list):
+        elif isinstance(input_names, list) and all(isinstance(name, str) for name in names):
             setattr(clz, "_model_input_names", input_names)
 
         return clz
