65d947e5a042085b6cf15a76227002cb196a69fc
Qinlong Wang <WangQL1201@outlook.com>
2020-05-07 10:18:30 +0800

    Add normalizer preprocessing layer (#1925)

--- elasticdl_preprocessing/layers/__init__.py
+++ elasticdl_preprocessing/layers/__init__.py
@@ -8,6 +8,7 @@ from elasticdl_preprocessing.layers.discretization import Discretization
 from elasticdl_preprocessing.layers.hashing import Hashing
 from elasticdl_preprocessing.layers.index_lookup import IndexLookup
 from elasticdl_preprocessing.layers.log_round import LogRound
+from elasticdl_preprocessing.layers.normalizer import Normalizer
 from elasticdl_preprocessing.layers.round_identity import RoundIdentity
 from elasticdl_preprocessing.layers.sparse_embedding import SparseEmbedding
 from elasticdl_preprocessing.layers.to_number import ToNumber
