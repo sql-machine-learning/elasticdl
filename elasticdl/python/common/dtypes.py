7f0f78db0ee84193bbd32ebb1fda53422f0f0305
workingloong <WangQL1201@outlook.com>
2020-05-27 14:03:37 +0800

    Add a TODO

--- elasticdl/python/common/dtypes.py
+++ elasticdl/python/common/dtypes.py
@@ -40,6 +40,7 @@ _DT_NP_TO_TENSOR = {
     np.bool: types_pb2.DT_BOOL,
 }
 
+# TODO: There are many dtypes in MaxCompute and we can add them if needed.
 MAXCOMPUTE_DTYPE_TO_TF = {
     types.bigint: types_pb2.DT_INT64,
     types.double: types_pb2.DT_DOUBLE,
