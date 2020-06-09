061c7de09b179177467e32c113214a57ec5116a1
Yi Wang <yi.wang.2005@gmail.com>
2020-06-09 13:47:01 -0700

    More silence

--- elasticdl/docker/scripts/install-protobuf.bash
+++ elasticdl/docker/scripts/install-protobuf.bash
@@ -2,6 +2,6 @@
 
 set -e
 
-wget https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
+wget -q https://github.com/protocolbuffers/protobuf/releases/download/v3.7.1/protoc-3.7.1-linux-x86_64.zip
 unzip -qq protoc-3.7.1-linux-x86_64.zip -d /usr/local
 rm protoc-3.7.1-linux-x86_64.zip
