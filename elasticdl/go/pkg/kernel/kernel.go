4a3da2f70718a9ade9db35082ecb799a5f95252d
QI JUN <qijun1994@hotmail.com>
2020-06-11 16:25:15 +0800

    refine go ps directory (#1989)
    * refine go ps directory
    
    * fix ci
    
    * fix ci

--- elasticdl/go/pkg/kernel/kernel.go
+++ elasticdl/go/pkg/kernel/kernel.go
@@ -1,12 +1,26 @@
+// Copyright 2020 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kernel
 
 // #cgo LDFLAGS: -L./capi -lkernel_api -lm
 // #include "capi/kernel_api.h"
 import "C"
 import (
-	"elasticdl.org/elasticdl/common"
 	"fmt"
 	"unsafe"
+
+	"elasticdl.org/elasticdl/pkg/common"
 )
 
 // SGD kernel
--- elasticdl/go/pkg/ps/checkpoint.go
+++ elasticdl/go/pkg/ps/checkpoint.go
@@ -1,16 +1,30 @@
+// Copyright 2020 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ps
 
 import (
 	"crypto/sha256"
-	"elasticdl.org/elasticdl/common"
-	"elasticdl.org/elasticdl/proto"
 	"encoding/hex"
 	"fmt"
-	go_pb "github.com/golang/protobuf/proto"
 	"io/ioutil"
 	"math/big"
 	"os"
 	"path"
+
+	"elasticdl.org/elasticdl/pkg/common"
+	"elasticdl.org/elasticdl/pkg/proto"
+	go_pb "github.com/golang/protobuf/proto"
 )
 
 // StringToID maps a string to an id
