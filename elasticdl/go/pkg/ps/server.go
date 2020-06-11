4ec01966ea0827aa193cea660b0105eab4cfcea8
qijun <qijun1994@hotmail.com>
2020-06-11 09:41:57 +0800

    refine go ps directory

--- elasticdl/go/pkg/ps/server.go
+++ elasticdl/go/pkg/ps/server.go
@@ -1,17 +1,31 @@
+// Copyright 2020 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package ps
 
 import (
 	"context"
-	"elasticdl.org/elasticdl/proto"
 	"fmt"
-	"github.com/golang/protobuf/ptypes/empty"
-	"github.com/tensorflow/tensorflow/tensorflow/go/core/framework/tensor_go_proto"
-	"google.golang.org/grpc"
 	"log"
 	"net"
 	"os"
 	"path"
 	"sync"
+
+	"elasticdl.org/elasticdl/pkg/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	"github.com/tensorflow/tensorflow/tensorflow/go/core/framework/tensor_go_proto"
+	"google.golang.org/grpc"
 )
 
 const (
--- elasticdl/python/master/master.py
+++ elasticdl/python/master/master.py
@@ -1,3 +1,16 @@
+# Copyright 2020 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 import threading
 import time
@@ -394,8 +407,7 @@ class Master(object):
 
             if args.use_go_ps:
                 opt_type, opt_args = get_optimizer_info(self.optimizer)
-                # TODO: rename the Go PS executable using a meaningful filename
-                ps_client_command = "main"
+                ps_client_command = "start_ps"
                 ps_args = [
                     "-job_name=" + args.job_name,
                     "-namespace=" + args.namespace,
