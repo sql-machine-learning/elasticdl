12778fbac4e60545cad8cd76665e897292cf100c
Yi Wang <yi.wang.2005@gmail.com>
2020-06-12 14:33:07 -0700

    Simplify build_all.sh

--- elasticdl/docker/build_all.sh
+++ elasticdl/docker/build_all.sh
@@ -12,29 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-TF_VERSION=2.1.0
-
-if [[ ! -d .git ]]; then
-    echo "We must run this script at the root of the source tree."
-    exit 1
-fi
-
-if [[ $# -eq 1 && $1 == "-gpu" ]]; then
-    base_image="tensorflow/tensorflow:${TF_VERSION}-gpu-py3"
-    echo "To support CUDA; all images are from " $base_image
-    dev_image="elasticdl:dev-gpu"
-    ci_image="elasticdl:ci-gpu"
-
-else
-    base_image="tensorflow/tensorflow:${TF_VERSION}-py3"
-    dev_image="elasticdl:dev"
-    ci_image="elasticdl:ci"
-fi
-
-docker build --target dev -t $dev_image -f elasticdl/docker/Dockerfile --build-arg BASE_IMAGE=$base_image .
-
-docker build --target ci -t $ci_image -f elasticdl/docker/Dockerfile --build-arg BASE_IMAGE=$base_image .
-
+docker build --target dev -t elasticdl:dev -f elasticdl/docker/Dockerfile .
+docker build --target ci -t elasticdl:ci -f elasticdl/docker/Dockerfile .
 docker build -t elasticdl:dev_allreduce -f elasticdl/docker/Dockerfile.dev_allreduce .
