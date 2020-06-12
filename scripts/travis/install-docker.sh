df3467fa0e54739fcc3aa238d2e2ec0657144263
Yi Wang <yi.wang.2005@gmail.com>
2020-06-12 12:51:22 -0700

    Update

---
+++ scripts/travis/install-docker.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Copyright 2020 The ElasticDL Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+
+# This file comes from https://docs.docker.com/engine/install/ubuntu/.
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Update the apt package index and install packages to allow apt to use a
+# repository over HTTPS.
+sudo apt-get -qq update
+sudo apt-get -qq install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common > /dev/null
+
+# Add Docker’s official GPG key.
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Set up the stable repository.
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# Install the Docker engine
+sudo apt-get -qq update
+sudo apt-get -qq install -y docker-ce docker-ce-cli containerd.io > /dev/null
+
+# Enable docker without sudo.
+sudo groupadd docker
+sudo usermod -aG docker "$USER"
+
+# Activate the changes to groups.
+newgrp docker
