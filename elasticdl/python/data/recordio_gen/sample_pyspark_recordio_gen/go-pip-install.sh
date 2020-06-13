0ea790b026bb6bccfc35c818c0a237b93e80570f
Yi Wang <yi.wang.2005@gmail.com>
2020-06-12 22:13:54 -0700

    Update

--- elasticdl/python/data/recordio_gen/sample_pyspark_recordio_gen/go-pip-install.sh
+++ elasticdl/python/data/recordio_gen/sample_pyspark_recordio_gen/go-pip-install.sh
@@ -15,7 +15,8 @@
 
 set -e
 
-# Some of the code here is copied from https://github.com/canha/golang-tools-install-script/blob/master/goinstall.sh
+# Some of the code here is copied from
+# https://github.com/canha/golang-tools-install-script/blob/master/goinstall.sh
 
 # Install Golang, which is needed to install pyrecordio
 VERSION="1.12.5"
@@ -49,12 +50,6 @@ case $OS in
   ;;
 esac
 
-if [ -n "`$SHELL -c 'echo $ZSH_VERSION'`" ]; then
-  shell_profile="zshrc"
-  elif [ -n "`$SHELL -c 'echo $BASH_VERSION'`" ]; then
-  shell_profile="bashrc"
-fi
-
 PACKAGE_NAME="go$VERSION.$PLATFORM.tar.gz"
 
 if [ -d "$HOME/.go" ]; then
@@ -69,35 +64,24 @@ else
   curl -o /tmp/go.tar.gz https://storage.googleapis.com/golang/$PACKAGE_NAME
 fi
 
-if [ $? -ne 0 ]; then
-  echo "Download failed! Exiting."
-  exit 1
-fi
-
 echo "Extracting File..."
 mkdir -p "$HOME/.go/"
 tar -C "$HOME/.go" --strip-components=1 -xzf /tmp/go.tar.gz
-touch "$HOME/.${shell_profile}"
-{
-  echo '# GoLang'
-  echo "export GOROOT=${GOROOT}"
-  echo 'export PATH=$PATH:$GOROOT/bin'
-  echo "export GOPATH=$GOPATH"
-  echo 'export PATH=$PATH:$GOPATH/bin'
-} >> "$HOME/.${shell_profile}"
+cat >> "$HOME/.bashrc" <<EOF
+export GOROOT=$GOROOT
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=$GOPATH
+export PATH=$PATH:$GOPATH/bin
+EOF
 
-mkdir -p $GOPATH/{src,pkg,bin}
-echo -e "\nGo $VERSION was installed.\nMake sure to relogin into your shell or run:"
-echo -e "\n\tsource $HOME/.${shell_profile}\n\nto update your environment variables."
-echo "Tip: Opening a new terminal window usually just works. :)"
+mkdir -p "$GOPATH"/{src,pkg,bin}
 rm -f /tmp/go.tar.gz
-source /.bashrc
 
 # Install pip
 easy_install pip
 
 # Install the dependencies we need
-pip install pyrecordio>=0.0.6 Pillow
+pip install --user pyrecordio>=0.0.6 Pillow
 
 # A hacky fix for tensorflow installation
 rm -rf /opt/conda/default/lib/python3.6/site-packages/wrapt*
