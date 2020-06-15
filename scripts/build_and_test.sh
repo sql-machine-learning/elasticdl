dce2bbdd974d5ee36062b384ad50d8a1193548f7
Yi Wang <yi.wang.2005@gmail.com>
2020-06-15 11:21:15 -0700

    Update

--- scripts/build_and_test.sh
+++ scripts/build_and_test.sh
@@ -15,32 +15,36 @@
 set -e
 
 # Build elasticdl.org/elasticdl Go package
-go env -w GOPROXY=https://goproxy.cn,https://developer.aliyun.com/mirror/goproxy,direct
-cd /elasticdl && \
-make -f elasticdl/Makefile && \
-cp -r /elasticdl/elasticdl/go/ /root/elasticdl && \
-cd "$GOPATH"/pkg/mod/github.com/tensorflow && \
-go mod init github.com/tensorflow &&\
-cd /root/elasticdl && \
-go mod init elasticdl.org/elasticdl && \
-go mod edit -replace github.com/tensorflow="${GOPATH}"/pkg/mod/github.com/tensorflow && \
-go get k8s.io/client-go@v0.17.0 && \
-go mod tidy && \
-go install ./...
+GO_PROXY_CN=https://goproxy.cn
+GO_PROXY_ALIYUN=https://developer.aliyun.com/mirror/goproxy
+go env -w GOPROXY="$GO_PROXY_CN","$GO_PROXY_ALIYUN",direct
 
-# Run Go unittests
-go test -v -cover ./...
+# NOTE: The following commands requires that the current working directory is
+# the source tree root.
+make -f elasticdl/Makefile
+
+# TODO(QiJune): How about we put the go.mod file in elasticdl/elasticdl/go/ in
+# the Git repo, so we don't need to create it at building time?
+(
+    cp -r elasticdl/go/ /tmp/elasticdl
+    cd "$GOPATH"/pkg/mod/github.com/tensorflow
+    go mod init github.com/tensorflow
+    cd /tmp/elasticdl
+    go mod init elasticdl.org/elasticdl
+    go mod edit -replace github.com/tensorflow="${GOPATH}"/pkg/mod/github.com/tensorflow
+    go get k8s.io/client-go@v0.17.0
+    go mod tidy
+    go install ./...
+    go test -v -cover ./...
+)
 
 # Run Python unittests
-cd /elasticdl
 pytest elasticdl/python/tests elasticdl_preprocessing/tests --cov=elasticdl/python --cov-report=xml
-mkdir -p /elasticdl/build && mv coverage.xml /elasticdl/build
+mkdir -p ./build
+mv coverage.xml /elasticdl/build
 
 # Create elasticdl package
-mkdir /elasticdl/elasticdl/go/bin && cp /root/go/bin/elasticdl_ps /elasticdl/elasticdl/go/bin
-cd /elasticdl && \
-python setup.py --quiet bdist_wheel && \
-cp dist/elasticdl-develop-py3-none-any.whl /elasticdl/build
-
-
-
+mkdir ./elasticdl/go/bin
+cp "$GOPATH"/go/bin/elasticdl_ps ./elasticdl/go/bin
+python setup.py --quiet bdist_wheel --dist-dir ./build
+# cp dist/elasticdl-develop-py3-none-any.whl /elasticdl/build
