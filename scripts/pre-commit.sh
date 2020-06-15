2929f256b01d8b6785a2ab3569037037feec3aea
brightcoder01 <bright.gong@outlook.com>
2020-06-15 11:21:22 +0800

    List markdown files in pre-commit.sh

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -26,4 +26,6 @@ GO_SOURCE_FILES=$(find elasticdl/pkg -name '*.go' -print0 | tr '\0' ' ')
 
 YAML_SOURCE_FILES=$(find .travis.yml -name '*.yml' -print0 | tr '\0' ' ')
 
-pre-commit run --files "$PYTHON_SOURCE_FILES $GO_SOURCE_FILES"
+MARKDOWN_SOURCE_FILES=$(find elasticdl -name '*.md' -print0 | tr '\0' ' ')
+
+pre-commit run --files "$PYTHON_SOURCE_FILES $GO_SOURCE_FILES $YAML_SOURCE_FILES $MARKDOWN_SOURCE_FILES"
