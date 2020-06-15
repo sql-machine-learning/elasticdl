218cc58ea052d665c51c224d582663e490b8d181
brightcoder01 <bright.gong@outlook.com>
2020-06-15 14:13:00 +0800

    Try pre-commit with from-ref and to-ref

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -12,20 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-
-PYTHON_SOURCE_FILES=$(find elasticdl/python \
-                           elasticdl_preprocessing \
-                           model_zoo \
-                           tools \
-                           setup.py \
-                           scripts \
-                           -name '*.py' -print0 | tr '\0' ' ')
-
-GO_SOURCE_FILES=$(find elasticdl/pkg -name '*.go' -print0 | tr '\0' ' ')
-
-YAML_SOURCE_FILES=$(find .travis.yml -name '*.yml' -print0 | tr '\0' ' ')
-
-MARKDOWN_SOURCE_FILES=$(find . -name '*.md' -print0 | tr '\0' ' ')
-
-pre-commit run --files "$PYTHON_SOURCE_FILES $GO_SOURCE_FILES $YAML_SOURCE_FILES $MARKDOWN_SOURCE_FILES"
+pre-commit run --from-ref origin.HEAD --to-ref HEAD
