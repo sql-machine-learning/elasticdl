e2873d8a2d43b77cfca639369c84a9a1b0f22d9f
brightcoder01 <bright.gong@outlook.com>
2020-06-15 13:37:08 +0800

    List all the markdown files to run the markdownlint check

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -26,6 +26,6 @@ GO_SOURCE_FILES=$(find elasticdl/pkg -name '*.go' -print0 | tr '\0' ' ')
 
 YAML_SOURCE_FILES=$(find .travis.yml -name '*.yml' -print0 | tr '\0' ' ')
 
-MARKDOWN_SOURCE_FILES=$(find elasticdl -name '*.md' -print0 | tr '\0' ' ')
+MARKDOWN_SOURCE_FILES=$(find . -name '*.md' -print0 | tr '\0' ' ')
 
 pre-commit run --files "$PYTHON_SOURCE_FILES $GO_SOURCE_FILES $YAML_SOURCE_FILES $MARKDOWN_SOURCE_FILES"
