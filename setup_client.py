d70311f957bb14cfd1ef2a81df206640b29080a7
brightcoder01 <55301748+brightcoder01@users.noreply.github.com>
2020-06-20 00:23:01 +0800

    Setup the directory structure for elasticdl_client. (#2048)
    * Add the setup and requirements for elasticdl_client
    
    * Fix issues in setup file
    
    * Add __init__.py
    
    * Exclude elasticdl_client from main package.
    
    * Add the elasticdl_client pip package build and installation in travis ci and Dockerfile.

---
+++ setup_client.py
@@ -0,0 +1,29 @@
+# Copyright 2020 The ElasticDL Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+with open("elasticdl_client/requirements.txt") as f:
+    required_deps = f.read().splitlines()
+
+setup(
+    name="elasticdl_client",
+    version="develop",
+    description="The client tool for ElasticDL.",
+    author="Ant Financial",
+    url="https://elasticdl.org",
+    install_requires=required_deps,
+    python_requires=">=3.5",
+    packages=find_packages(include=["elasticdl_client*"]),
+    package_data={"": ["requirements.txt"]},
+)
