d3a65e757cdf43233118c18162b928f5ddc21b66
brightcoder01 <bright.gong@outlook.com>
2020-06-27 11:09:00 +0800

    Add unit test for argument parser.

--- elasticdl_client/main.py
+++ elasticdl_client/main.py
@@ -11,25 +11,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import argparse
 
-def main():
-    """
+
+def zoo_init(args):
+    print("Initialize model zoo")
+
+
+def zoo_build(args):
+    print("Build model zoo")
+
+
+def zoo_push(args):
+    print("Push model zoo")
+
+
+def build_argument_parser():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
     subparsers.required = True
 
+    # Initialize the parser for the commands
+    # elasticdl zoo init
+    # elasticdl zoo build
+    # elasticdl zoo push
     zoo_parser = subparsers.add_parser("zoo")
     zoo_subparsers = zoo_parser.add_subparsers()
+
     zoo_init_parser = zoo_subparsers.add_parser("init")
-    zoo_init_parser.add_argument()
+    zoo_init_parser.set_defaults(func=zoo_init)
+    zoo_init_parser.add_argument(
+        "--base_image", type=str, default="python:latest"
+    )
+
     zoo_build_parser = zoo_subparsers.add_parser("build")
+    zoo_build_parser.set_defaults(func=zoo_build)
+    zoo_build_parser.add_argument("path", type=str)
+    zoo_build_parser.add_argument("--image", type=str, required=True)
+
     zoo_push_parser = zoo_subparsers.add_parser("push")
+    zoo_push_parser.set_defaults(func=zoo_push)
+    zoo_push_parser.add_argument("image", type=str)
 
-    train_parser = subparsers.add_parser("train")
+    return parser
 
+
+def main():
+    parser = build_argument_parser()
     args, _ = parser.parse_known_args()
     print(args)
-    """
 
 
 if __name__ == "__main__":
---
+++ elasticdl_client/tests/arg_parser_test.py
@@ -0,0 +1,80 @@
+# Copyright 2020 The ElasticDL Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from elasticdl_client.main import build_argument_parser
+
+
+class ArgParserTest(unittest.TestCase):
+    def setUp(self):
+        self._parser = build_argument_parser()
+
+    def test_parse_zoo_init(self):
+        args = ["zoo", "init"]
+        args = self._parser.parse_args(args)
+        self.assertEqual(args.base_image, "python:latest")
+        args.func(args)
+
+        args = ["zoo", "init", "--base_image=elasticdl:base"]
+        args = self._parser.parse_args(args)
+        self.assertEqual(args.base_image, "elasticdl:base")
+
+        with self.assertRaises(SystemExit):
+            args = ["zoo", "init", "--mock_param=mock_value"]
+            args = self._parser.parse_args(args)
+
+    def test_parse_zoo_build(self):
+        args = [
+            "zoo",
+            "build",
+            "--image=a_docker_registry/bright/elasticdl-wnd:1.0",
+            ".",
+        ]
+        args = self._parser.parse_args(args)
+        self.assertEqual(
+            args.image, "a_docker_registry/bright/elasticdl-wnd:1.0"
+        )
+        self.assertEqual(args.path, ".")
+
+        with self.assertRaises(SystemExit):
+            args = ["zoo", "build", "."]
+            args = self._parser.parse_args(args)
+
+        with self.assertRaises(SystemExit):
+            args = [
+                "zoo",
+                "build",
+                "--image=a_docker_registry/bright/elasticdl-wnd:1.0",
+            ]
+            args = self._parser.parse_args(args)
+
+    def test_parse_zoo_push(self):
+        args = ["zoo", "push", "a_docker_registry/bright/elasticdl-wnd:1.0"]
+        args = self._parser.parse_args(args)
+        self.assertEqual(
+            args.image, "a_docker_registry/bright/elasticdl-wnd:1.0"
+        )
+
+        with self.assertRaises(SystemExit):
+            args = ["zoo", "push"]
+            args = self._parser.parse_args(args)
+
+    def test_parse_train(self):
+        pass
+
+    def test_parse_predict(self):
+        pass
+
+    def test_parse_evaluate(self):
+        pass
