37e18738c3836a1fb5000642b1ae6e35d261154c
brightcoder01 <bright.gong@outlook.com>
2020-06-29 11:46:24 +0800

    Add the arguments for train|predict|evaluate

--- elasticdl_client/common/args.py
+++ elasticdl_client/common/args.py
@@ -79,16 +79,137 @@ def add_docker_arguments(parser):
 
 
 def add_train_params(parser):
-    pass
+    parser.add_argument(
+        "--tensorboard_log_dir",
+        default="",
+        type=str,
+        help="Directory where TensorBoard will look to find "
+        "TensorFlow event files that it can display. "
+        "TensorBoard will recursively walk the directory "
+        "structure rooted at log dir, looking for .*tfevents.* "
+        "files. You may also pass a comma separated list of log "
+        "directories, and TensorBoard will watch each "
+        "directory.",
+    )
+    parser.add_argument("--num_epochs", type=int, default=1)
+    parser.add_argument(
+        "--grads_to_wait",
+        type=int,
+        help="Number of gradients to wait before updating model",
+        default=1,
+    )
+    parser.add_argument(
+        "--training_data",
+        help="Either the data directory that contains RecordIO files "
+        "or an ODPS table name used for training.",
+        default="",
+    )
+    parser.add_argument(
+        "--validation_data",
+        help="Either the data directory that contains RecordIO files "
+        "or an ODPS table name used for evaluation.",
+        default="",
+    )
+    parser.add_argument(
+        "--evaluation_steps",
+        type=int,
+        help="Evaluate the model every this many steps."
+        "If 0, step-based evaluation is disabled",
+        default=0,
+    )
+    parser.add_argument(
+        "--evaluation_start_delay_secs",
+        type=int,
+        help="Start time-based evaluation only after waiting for "
+        "this many seconds",
+        default=100,
+    )
+    parser.add_argument(
+        "--evaluation_throttle_secs",
+        type=int,
+        help="Do not re-evaluate unless the last evaluation was started "
+        "at least this many seconds ago."
+        "If 0, time-based evaluation is disabled",
+        default=0,
+    )
+    parser.add_argument(
+        "--checkpoint_dir_for_init",
+        help="The checkpoint directory to initialize the training model",
+        default="",
+    )
+    parser.add_argument(
+        "--sync_version_tolerance",
+        type=int,
+        help="The maximum model version difference between reported gradients "
+        "and PS that synchronous SGD can accepts.",
+        default=0,
+    )
+    parser.add_argument(
+        "--log_loss_steps",
+        type=int,
+        help="The frequency, in number of global steps, that the global step "
+        "and the loss will be logged during training.",
+        default=100,
+    )
+    add_bool_param(
+        parser=parser,
+        name="--use_async",
+        default=False,
+        help="True for asynchronous SGD, False for synchronous SGD",
+    )
+    add_bool_param(
+        parser=parser,
+        name="--lr_staleness_modulation",
+        default=False,
+        help="If True, PS will modulate the learning rate with staleness "
+        "in asynchronous SGD",
+    )
 
 
 def add_evaluate_params(parser):
-    pass
+    parser.add_argument(
+        "--validation_data",
+        help="Either the data directory that contains RecordIO files "
+        "or an ODPS table name used for evaluation.",
+        required=True,
+    )
+    parser.add_argument(
+        "--checkpoint_dir_for_init",
+        help="The checkpoint directory to initialize the training model",
+        required=True,
+    )
 
 
 def add_predict_params(parser):
-    pass
+    parser.add_argument(
+        "--prediction_data",
+        help="Either the data directory that contains RecordIO files "
+        "or an ODPS table name used for prediction.",
+        required=True,
+    )
+    parser.add_argument(
+        "--prediction_outputs_processor",
+        help="The name of the prediction output processor class "
+        "defined in the model definition file.",
+        default="PredictionOutputsProcessor",
+    )
+    parser.add_argument(
+        "--checkpoint_dir_for_init",
+        help="The checkpoint directory to initialize the training model",
+        required=True,
+    )
 
 
 def add_common_params(parser):
     pass
+
+
+def add_bool_param(parser, name, default, help):
+    parser.add_argument(
+        name,  # should be in "--foo" format
+        nargs="?",
+        const=not default,
+        default=default,
+        type=lambda x: x.lower() in ["true", "yes", "t", "y"],
+        help=help,
+    )
