5144d2996887035e71ce7f934775e1d854e0a240
workingloong <WangQL1201@outlook.com>
2020-07-01 09:19:12 +0800

    Format shell command

--- scripts/docker_build_wheel.sh
+++ scripts/docker_build_wheel.sh
@@ -14,6 +14,8 @@
 
 export BASE_IMAGE=tensorflow/tensorflow:2.1.0-py3
 
-docker build --target dev -t elasticdl:dev -f elasticdl/docker/Dockerfile --build-arg BASE_IMAGE="$BASE_IMAGE" .
+docker build --target dev -t elasticdl:dev -f elasticdl/docker/Dockerfile \
+    --build-arg BASE_IMAGE="$BASE_IMAGE" .
 
-docker run --rm -it --net=host -v $PWD:/work -w /work elasticdl:dev bash -c "scripts/build.sh"
+docker run --rm -it --net=host -v "$PWD":/work -w /work elasticdl:dev \
+    bash -c "scripts/build.sh"
