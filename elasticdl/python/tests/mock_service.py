c4cc50e9f3cfa978a0275a22c99235437980769b
Yancey1989 <yancey1989@gmail.com>
2020-07-17 20:46:02 +0800

    add mock servicer

---
+++ elasticdl/python/tests/mock_service.py
@@ -0,0 +1,41 @@
+# Copyright 2020 The ElasticDL Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from concurrent import futures
+
+import grpc
+from google.protobuf import empty_pb2
+
+from elasticdl.proto import elasticdl_pb2, elasticdl_pb2_grpc
+from elasticdl.python.common.grpc_utils import find_free_port
+
+
+class MockMasterService(elasticdl_pb2_grpc.MasterServicer):
+    def report_evaluation_metrics(self, request, _):
+        return empty_pb2.Empty()
+
+    def report_task_result(self, request, _):
+        return empty_pb2.Empty()
+
+    def get_task(self, request, _):
+        return elasticdl_pb2.Task()
+
+
+def mock_master_servicer(server_instance=MockMasterService):
+    port = find_free_port()
+    master_servicer = server_instance()
+    svr = grpc.server(futures.ThreadPoolExecutor(max_workers=2))
+    elasticdl_pb2_grpc.add_MasterServicer_to_server(master_servicer, svr)
+    svr.add_insecure_port("[::]:{}".format(port))
+    svr.start()
+    return svr, port
