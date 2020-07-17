5e2366ad9635e3979e7b080645e9ea571f7ba97b
Yancey1989 <yancey1989@gmail.com>
2020-07-17 18:14:42 +0800

    remove unused callback args

--- elasticdl/python/tests/in_process_master.py
+++ elasticdl/python/tests/in_process_master.py
@@ -12,13 +12,11 @@
 # limitations under the License.
 
 """In process master for unittests"""
-from elasticdl.python.tests import test_call_back
 
 
 class InProcessMaster(object):
-    def __init__(self, master, callbacks=[]):
+    def __init__(self, master):
         self._m = master
-        self._callbacks = callbacks
 
     def get_task(self, req):
         return self._m.get_task(req, None)
@@ -32,11 +30,6 @@ class InProcessMaster(object):
     """
 
     def report_evaluation_metrics(self, req):
-        for callback in self._callbacks:
-            if test_call_back.ON_REPORT_EVALUATION_METRICS_BEGIN in (
-                callback.call_times
-            ):
-                callback()
         return self._m.report_evaluation_metrics(req, None)
 
     def report_task_result(self, req):
