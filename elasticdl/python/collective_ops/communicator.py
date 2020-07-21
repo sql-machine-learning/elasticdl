910d4ed8c02c9f6ea42880c2c2ba530d13ba6ade
haitao <haitao@B-N3DAG8WN-2041.local>
2020-07-21 20:09:36 +0800

    Add allreduce timing and retry when connection fails

--- elasticdl/python/collective_ops/communicator.py
+++ elasticdl/python/collective_ops/communicator.py
@@ -77,11 +77,30 @@ class CollectiveCommunicator(object):
                     ),
                 },
             )
+            connection_try_num = 0
             while peer_list and not self._ftlib.consensus_joined():
                 logger.warning("Retry building consensus...")
-                self._ftlib.manual_join(
-                    known_addr_list=list(self._get_peer_set(service_name))
-                )
+                try:
+                    self._ftlib.manual_join(
+                        known_addr_list=list(self._get_peer_set(service_name))
+                    )
+                except Exception:
+                    if (
+                        connection_try_num * 5
+                        > _FTLIB_CONSENSUS_CONNECTION_TIMEOUT_SECS
+                    ):
+                        logger.error(
+                            "Cannot join FTLib consensus service in %s "
+                            "seconds",
+                            str(_FTLIB_CONSENSUS_CONNECTION_TIMEOUT_SECS),
+                        )
+                        self._ftlib = None
+                        return
+                    logger.info(
+                        "Cannot join FTLib consensus service, " "trying again."
+                    )
+                    connection_try_num += 1
+                    time.sleep(5)
         else:
             logger.warning(
                 "FTLib is not installed. The CollectiveCommunicator "
