7951646d98833992a96183cfd4e6fca0c8202c3c
haitao <haitao@B-N3DAG8WN-2041.local>
2020-07-28 11:55:40 +0800

    Polish timing

--- elasticdl/python/common/timing_utils.py
+++ elasticdl/python/common/timing_utils.py
@@ -13,13 +13,6 @@
 
 import time
 
-TIMING_GROUP = [
-    "task_process",
-    "batch_process",
-    "get_model",
-    "report_gradient",
-]
-
 
 class Timing(object):
     def __init__(self, enable, logger=None):
@@ -27,31 +20,29 @@ class Timing(object):
         self.logger = logger
         self.timings = {}
         self.start_time = {}
-        self.reset()
 
     def reset(self):
         if not self.enable:
             return
-        for timing_type in TIMING_GROUP:
-            self.timings[timing_type] = 0
+        self.timings = {}
+        self.start_time = {}
 
     def start_record_time(self, timing_type):
         if not self.enable:
             return
+        if timing_type not in self.timings:
+            self.timings[timing_type] = 0
         self.start_time[timing_type] = time.time()
 
     def end_record_time(self, timing_type):
-        if not self.enable:
+        if not self.enable or timing_type not in self.start_time:
             return
         self.timings[timing_type] += time.time() - self.start_time[timing_type]
 
     def report_timing(self, reset=False):
         if not self.enable:
             return
-        for timing_type in TIMING_GROUP:
-            self.logger.debug(
-                "%s time is %.6g seconds"
-                % (timing_type, self.timings[timing_type])
-            )
+        for timing_type, value in self.timings.items():
+            self.logger.debug("%s time is %.6g seconds" % (timing_type, value))
         if reset:
             self.reset()
