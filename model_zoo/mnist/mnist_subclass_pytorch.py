0db72af18d40a40fcc9d9dd6a94b422a7c67ab47
kelang <1141180644@qq.com>
2020-08-13 11:26:56 +0800

    update mnist pytorch

--- model_zoo/mnist/mnist_subclass_pytorch.py
+++ model_zoo/mnist/mnist_subclass_pytorch.py
@@ -17,69 +17,42 @@ import tensorflow as tf
 import torch
 from torch.utils.data import Dataset, DataLoader
 import torch.nn as nn
-import torchvision
 import torch.nn.functional as F
 
 from elasticdl.python.common.constants import Mode
 
+
 class CustomModel(nn.Module):
     def __init__(self):
-        super(CustomModel,self).__init__()
-        self.conv1 = nn.Conv2d(1,32,3,padding = 1)
-        self.pool1 = nn.MaxPool2d(2,2)
-        self.conv2 = nn.Conv2d(32,64,3,padding = 1)
-        self.pool2 = nn.MaxPool2d(2,2)
-        self.conv3 = nn.Conv2d(64,128,3,padding = 1)
-        self.pool3 = nn.MaxPool2d(2,2)
-        
-        self.fc1 = nn.Linear(128*3*3,625)
-        self.fc2 = nn.Linear(625,10)
-          
-    def forward(self,x):
+        super(CustomModel, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, padding=1)
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
+        self.pool3 = nn.MaxPool2d(2, 2)
+
+        self.fc1 = nn.Linear(128 * 3 * 3, 625)
+        self.fc2 = nn.Linear(625, 10)
+
+    def forward(self, x):
         x = self.pool1(F.relu(self.conv1(x)))
         x = self.pool2(F.relu(self.conv2(x)))
         x = self.pool3(F.relu(self.conv3(x)))
-        x = x.view(-1,128*3*3)
+        x = x.view(-1, 128 * 3 * 3)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
-
         return x
 
 
-def prepare_data_for_a_single_file(file_object, filename):
-    """
-    :param filename: training data file name
-    :param file_object: a file object associated with filename
-    """
-    label = int(filename.split("/")[-2])
-    image = PIL.Image.open(file_object)
-    numpy_image = np.array(image)
-    example_dict = {
-        "image": tf.train.Feature(
-            float_list=tf.train.FloatList(value=numpy_image.flatten())
-        ),
-        "label": tf.train.Feature(
-            int64_list=tf.train.Int64List(value=[label])
-        ),
-    }
-    example = tf.train.Example(
-        features=tf.train.Features(feature=example_dict)
-    )
-    return example.SerializeToString()
-
-
 def loss(labels, predictions):
-    # loss_func = nn.CrossEntropyLoss()
-    loss_func = nn.NLLLoss()
-    print("loss_func:")
-    print("labels:",labels.dtype)
-    print("predictions:",predictions.dtype)
-    loss = loss_func(predictions, labels)
-    return loss
+    labels = labels.long()  # labels.dtype must be Long/int64 for CrossEntropyLoss()
+    loss_func = nn.CrossEntropyLoss()
+    return loss_func(predictions, labels)
+
 
 # def optimizer(model,lr=0.001):
-#     return torch.optim.Adam(model.parameters(), lr) 
-
+#     return torch.optim.Adam(model.parameters(), lr)
 def optimizer(lr=0.01):
     return tf.optimizers.SGD(lr)
 
@@ -110,6 +83,7 @@ def dataset_fn(dataset, mode, _):
         dataset = dataset.shuffle(buffer_size=1024)
     return dataset
 
+
 def eval_metrics_fn():
     return {
         "accuracy": lambda labels, predictions: tf.equal(
@@ -118,6 +92,7 @@ def eval_metrics_fn():
         )
     }
 
+
 class CustomDataset(torch.utils.data.IterableDataset):
     def __init__(self, data):
         self.data_source = data
@@ -125,8 +100,19 @@ class CustomDataset(torch.utils.data.IterableDataset):
     def __iter__(self):
         return iter(self.data_source)
 
-def dataset_pytorch(dataset, minibatch_size):
-    dataset = list(dataset.as_numpy_iterator())
-    iterable_dataset = CustomDataset(dataset)
-    dataloader = DataLoader(dataset=iterable_dataset, batch_size=minibatch_size)
+
+def _dataset_pytorch(dataset, batch_size):
+    """
+    _dataset_fn() builds dataset for TensorFlow
+    this func transforms dataset and set DataLoader for PyTorch
+    TODO: rewrite _dataset_fn() by IterableDataSet for PyTorch
+    """
+    dataset_list = []
+    for data_enum in list(dataset.as_numpy_iterator()):
+        shape_0 = data_enum[0].shape[0]
+        for i in range(shape_0):
+            dataset_list.append((data_enum[0][i:i + 1, ...], data_enum[1][i:i + 1, ...]))
+
+    iterable_dataset = CustomDataset(dataset_list)
+    dataloader = DataLoader(dataset=iterable_dataset, batch_size=batch_size)
     return dataloader
