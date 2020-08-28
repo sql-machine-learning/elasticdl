ab4455f414e85da84e79d4ca992aa3fda97fe557
workingloong <WangQL1201@outlook.com>
2020-08-28 08:10:25 +0800

    Polish codes

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,8 +38,6 @@ make -f elasticdl/Makefile
     GOBIN=/tmp go install ./...
 )
 
-rm ./build/*.whl
-
 # Create elasticdl_preprocessing package
 rm -rf ./build/lib
 python setup_preprocessing.py --quiet bdist_wheel --dist-dir ./build
