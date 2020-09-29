0daf37be5c0316403b23d897039db0fd37d87d48
Qinlong Wang <WangQL1201@outlook.com>
2020-09-29 16:52:55 +0800

    Do not scale learning rate of optimizer. (#2312)
    * Don't scale learning rate
    
    * Remove unused variables
    
    * Add an ut for training_process in AllReduceTrainer
    
    * Pre-commit
    
    * Fix unit test

--- elasticdl/python/worker/allreduce_trainer.py
+++ elasticdl/python/worker/allreduce_trainer.py
@@ -47,21 +47,8 @@ class AllReduceTrainer(Trainer):
         self._rendezvous_id = None
         self._need_broadcast = True
         self._var_created = False
-        self._world_size = None
-        self._set_optimizer(model.optimizer)
+        self._optimizer = model.optimizer
         self._set_horovod_env()
-
-    def _set_optimizer(self, optimizer):
-        self._optimizer = optimizer
-        self._lr = optimizer._hyper["learning_rate"]
-        self._optimizer.lr = self._get_learning_rate
-
-    def _get_learning_rate(self):
-        scaler = 1 if self._world_size is None else self._world_size
-        lr = self._lr
-        if callable(lr):
-            lr = lr()
-        return lr * scaler
 
     @tf.function
     def _training_process(self, features, labels):
@@ -143,7 +130,6 @@ class AllReduceTrainer(Trainer):
             hvd.shutdown()
             hvd.init()
             os.environ[HorovodEnv.ELASTIC] = str(1)
-            self._world_size = hvd.size()
             self._rendezvous_id = rank_response.rendezvous_id
             self._need_broadcast = True
 
