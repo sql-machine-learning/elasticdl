0ff9bf101dba0af4a29dc7b2e493eebc5897d382
workingloong <WangQL1201@outlook.com>
2020-10-16 10:24:06 +0800

    Fix the unittest

--- elasticdl/python/tests/allreduce_trainer_test.py
+++ elasticdl/python/tests/allreduce_trainer_test.py
@@ -38,14 +38,14 @@ class AllReduceTrainerTest(unittest.TestCase):
         self._trainer = AllReduceTrainer(master_client, "", model)
 
     def test_training_process(self):
-        self._trainer.init_horovod_if_needed()
+        self._trainer._rendezvous_manager.init_horovod_if_needed()
         features = tf.constant([[0.5], [0.6], [0.7]])
         labels = tf.constant([[1.0], [0.0], [1.0]])
         loss = self._trainer._training_process(features, labels)
         self.assertIsNotNone(loss)
 
     def test_train_minibatch(self):
-        self._trainer.init_horovod_if_needed()
+        self._trainer._rendezvous_manager.init_horovod_if_needed()
         features = tf.constant([[0.5], [0.6], [0.7]])
         labels = tf.constant([[1.0], [0.0], [1.0]])
         _, version, loss = self._trainer.train_minibatch(features, labels)
@@ -56,7 +56,9 @@ class AllReduceTrainerTest(unittest.TestCase):
     def test_init_variables_if_needed(self):
         features = tf.constant([[0.5], [0.6], [0.7]])
         labels = tf.constant([[1.0], [0.0], [1.0]])
-        self._trainer.init_variables_if_need(features, labels)
+        self._trainer._rendezvous_manager.init_variables_if_need(
+            features, labels
+        )
         self.assertTrue(self._trainer._var_created)
         self.assertEqual(self._trainer._optimizer.iterations.numpy(), 0)
 
