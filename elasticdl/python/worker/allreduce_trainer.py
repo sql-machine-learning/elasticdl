db99ea1b420e89bb7f0a0694d32e2751b0e6a713
Qinlong Wang <WangQL1201@outlook.com>
2020-10-17 13:23:42 +0800

    Separate the RendevousManager from AllReduceTrainer (#2323)
    * Separate the RendevousManager from AllReduceTrainer
    
    * Fix unittest
    
    * Fix the unittest
    
    * Fix the unit test
    
    * Pre-commit

--- elasticdl/python/worker/allreduce_trainer.py
+++ elasticdl/python/worker/allreduce_trainer.py
@@ -36,74 +36,15 @@ DEFAULT_MAX_ALLREDUCE_RETRY_NUM = 5
 DEFAULT_STEPS_TO_CHECK_RENDEZVOUS = 20
 
 
-class AllReduceTrainer(Trainer):
-    def __init__(self, master_client, master_addr, model):
-        if not hvd:
-            raise RuntimeError("Horovod is not installed for AllReduce")
+class RendevousManager(object):
+    def __init__(self, master_client, master_addr):
+        self.need_broadcast = True
         self._master_client = master_client
         self._rendezvous_addr = master_addr
-        self._model = model
-        self._loss = model.loss
         self._rendezvous_id = None
-        self._need_broadcast = True
-        self._var_created = False
-        self._optimizer = model.optimizer
-        self._set_horovod_env()
-
-    @tf.function
-    def _training_process(self, features, labels):
-        with tf.GradientTape() as tape:
-            outputs = self._model.call(features, training=True)
-            loss = self._loss(labels, outputs)
-            if self._model.losses:
-                loss += tf.math.add_n(self._model.losses)
-
-        tape = hvd.DistributedGradientTape(tape)
-        grads = tape.gradient(loss, self._model.trainable_variables)
-        # Take care of the order of grads and vars if worker modifies
-        # `_non_embed_vars` during training.
-        self._optimizer.apply_gradients(
-            zip(grads, self._model.trainable_variables)
-        )
-        return loss
-
-    def train_minibatch(self, features, labels, train_with_local_model=False):
-        self._check_new_communication_world()
-
-        for _ in range(DEFAULT_MAX_ALLREDUCE_RETRY_NUM + 1):
-            try:
-                if self._need_broadcast:
-                    self._broadcast_model()
-                    self._need_broadcast = False
-                loss = self._training_process(features, labels)
-                version = self._optimizer.iterations.numpy()
-                return True, version, loss
-            except UnknownError as e:
-                logger.warning(
-                    "Failed to perform allreduce operation on "
-                    "the gradients. Retrying..."
-                )
-                # Those error message show that the communication
-                # to merge gradient fails and we can rebuild the
-                # communication.
-                if (
-                    "HorovodAllreduce" in e.message
-                    or "HorovodAllgather" in e.message
-                    or "HorovodBroadcast" in e.message
-                ):
-                    time.sleep(3)
-                    self.init_horovod_if_needed()
-
-    def _check_new_communication_world(self):
-        """"Check periodically whether new workers join the job
-        and re-initialize Horovod if True.
-        """
-        iter_steps = self._optimizer.iterations.numpy()
-
-        if iter_steps % DEFAULT_STEPS_TO_CHECK_RENDEZVOUS == 0:
-            self.init_horovod_if_needed()
 
     def init_horovod_if_needed(self):
+        self._set_horovod_env()
         for _ in range(DEFAULT_MAX_ALLREDUCE_RETRY_NUM):
             rank_response = self._master_client.get_comm_rank()
             if rank_response.rank_id < 0:
@@ -131,7 +72,7 @@ class AllReduceTrainer(Trainer):
             hvd.init()
             os.environ[HorovodEnv.ELASTIC] = str(1)
             self._rendezvous_id = rank_response.rendezvous_id
-            self._need_broadcast = True
+            self.need_broadcast = True
 
     def _set_horovod_env(self):
         if self._rendezvous_addr:
@@ -140,6 +81,71 @@ class AllReduceTrainer(Trainer):
         os.environ[HorovodEnv.CPU_OPERATIONS] = "gloo"
         domain_ip = socket.gethostbyname(socket.gethostname())
         os.environ[HorovodEnv.HOSTNAME] = domain_ip
+
+
+class AllReduceTrainer(Trainer):
+    def __init__(self, master_client, master_addr, model):
+        if not hvd:
+            raise RuntimeError("Horovod is not installed for AllReduce")
+        self._rendezvous_manager = RendevousManager(master_client, master_addr)
+        self._model = model
+        self._loss = model.loss
+        self._need_broadcast = True
+        self._var_created = False
+        self._optimizer = model.optimizer
+
+    @tf.function
+    def _training_process(self, features, labels):
+        with tf.GradientTape() as tape:
+            outputs = self._model.call(features, training=True)
+            loss = self._loss(labels, outputs)
+            if self._model.losses:
+                loss += tf.math.add_n(self._model.losses)
+
+        tape = hvd.DistributedGradientTape(tape)
+        grads = tape.gradient(loss, self._model.trainable_variables)
+        # Take care of the order of grads and vars if worker modifies
+        # `_non_embed_vars` during training.
+        self._optimizer.apply_gradients(
+            zip(grads, self._model.trainable_variables)
+        )
+        return loss
+
+    def train_minibatch(self, features, labels, train_with_local_model=False):
+        self._check_new_communication_world()
+
+        for _ in range(DEFAULT_MAX_ALLREDUCE_RETRY_NUM + 1):
+            try:
+                if self._rendezvous_manager.need_broadcast:
+                    self._broadcast_model()
+                    self._rendezvous_manager.need_broadcast = False
+                loss = self._training_process(features, labels)
+                version = self._optimizer.iterations.numpy()
+                return True, version, loss
+            except UnknownError as e:
+                logger.warning(
+                    "Failed to perform allreduce operation on "
+                    "the gradients. Retrying..."
+                )
+                # Those error message show that the communication
+                # to merge gradient fails and we can rebuild the
+                # communication.
+                if (
+                    "HorovodAllreduce" in e.message
+                    or "HorovodAllgather" in e.message
+                    or "HorovodBroadcast" in e.message
+                ):
+                    time.sleep(3)
+                    self._rendezvous_manager.init_horovod_if_needed()
+
+    def _check_new_communication_world(self):
+        """"Check periodically whether new workers join the job
+        and re-initialize Horovod if True.
+        """
+        iter_steps = self._optimizer.iterations.numpy()
+
+        if iter_steps % DEFAULT_STEPS_TO_CHECK_RENDEZVOUS == 0:
+            self._rendezvous_manager.init_horovod_if_needed()
 
     def _broadcast_model(self):
         broadcast_variables(self._model.variables, root_rank=0)
