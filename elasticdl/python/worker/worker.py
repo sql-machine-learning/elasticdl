77fbbf189e8e354838268318913a722a245962bf
workingloong <WangQL1201@outlook.com>
2020-11-13 18:44:43 +0800

    Clean codes for allreduce training loop

--- elasticdl/python/worker/worker.py
+++ elasticdl/python/worker/worker.py
@@ -501,16 +501,13 @@ class Worker(object):
         # Initialize Horovod locally to generate varibles of the model
         # and optimizer.
         elastic_controller.init_horovod_locally()
-        while True:
-            dataset = self._task_data_service.get_dataset()
-            if not dataset:
-                self._process_train_end_callback_task_if_needed()
-                break
-            dataset = self._feed(
-                dataset,
-                Mode.TRAINING,
-                self._task_data_service.data_reader.metadata,
-            )
-            dataset = dataset.batch(self._minibatch_size).prefetch(1)
-            self._training_func(dataset, elastic_controller)
-            del dataset
+        dataset = self._task_data_service.get_dataset()
+        dataset = self._feed(
+            dataset,
+            Mode.TRAINING,
+            self._task_data_service.data_reader.metadata,
+        )
+        dataset = dataset.batch(self._minibatch_size).prefetch(1)
+        self._training_func(dataset, elastic_controller)
+        del dataset
+        self._process_train_end_callback_task_if_needed()
