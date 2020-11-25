450560092f7110db4df8267b478aadf29e54db50
Qinlong Wang <WangQL1201@outlook.com>
2020-11-25 15:54:08 +0800

    Move AllReduce controllers into an individual folder (#2374)
    * Move allreduce controllers into an individual folder
    
    * Pre-commit
    
    * Add init
    
    * Pre-commit
    
    * Move imports to the begining of the python file

--- elasticdl/python/allreduce/base_controller.py
+++ elasticdl/python/allreduce/base_controller.py
@@ -13,11 +13,8 @@
 import os
 import socket
 import time
-import traceback
 from abc import abstractmethod
 from functools import wraps
-
-from tensorflow.python.framework.errors_impl import UnknownError
 
 from elasticdl.python.common.constants import HorovodEnv, WorkerEnv
 from elasticdl.python.common.log_utils import default_logger as logger
@@ -27,8 +24,6 @@ try:
         import horovod.torch as hvd
     else:
         import horovod.tensorflow as hvd
-        from horovod.tensorflow.functions import broadcast_variables
-    from horovod.common.exceptions import HorovodInternalError
 
 except ImportError:
     hvd = None
@@ -147,101 +142,3 @@ class AllReduceController(object):
     @abstractmethod
     def broadcast(self):
         pass
-
-
-class TensorFlowV2AllReduceController(AllReduceController):
-    """The controller is responsible for elastic training of
-    TensorFlow eager execution using AllReduce.
-    """
-
-    def __init__(self, master_client, data_shard_service):
-        super(TensorFlowV2AllReduceController, self).__init__(
-            master_client, data_shard_service
-        )
-        self._model = None
-        self._optimizer = None
-
-    def set_broadcast_model(self, model):
-        self._model = model
-
-    def set_broadcast_optimizer(self, optimizer):
-        self._optimizer = optimizer
-
-    def broadcast(self):
-        broadcast_variables(self._model.variables, root_rank=0)
-        broadcast_variables(self._optimizer.variables(), root_rank=0)
-
-    def train_one_batch_with_retries(self, func, *args, **kwargs):
-        for _ in range(DEFAULT_MAX_ALLREDUCE_RETRY_NUM + 1):
-            try:
-                self._broadcast_if_needed()
-                result = func(*args, **kwargs)
-                break
-            except UnknownError as e:
-                logger.warning(
-                    "Failed to perform allreduce operation on "
-                    "the gradients. Retrying..."
-                )
-                # Those error message show that the communication
-                # to merge gradient fails and we can rebuild the
-                # communication.
-                if (
-                    "HorovodAllreduce" in e.message
-                    or "HorovodAllgather" in e.message
-                    or "HorovodBroadcast" in e.message
-                ):
-                    time.sleep(3)
-                    self._rendezvous_manager.init_horovod_if_needed()
-        self.data_shard_service.report_batch_done()
-        return result
-
-
-class PyTorchAllReduceController(AllReduceController):
-    def __init__(self, master_client, data_shard_service):
-        super(PyTorchAllReduceController, self).__init__(
-            master_client, data_shard_service
-        )
-        self._model = None
-        self._optimizer = None
-
-    def set_broadcast_model(self, model):
-        self._model = model
-
-    def set_broadcast_optimizer(self, optimizer):
-        self._optimizer = optimizer
-
-    def broadcast(self):
-        from horovod.torch.functions import (
-            broadcast_optimizer_state,
-            broadcast_parameters,
-        )
-
-        broadcast_parameters(self._model.state_dict(), root_rank=0)
-        broadcast_optimizer_state(self._optimizer, root_rank=0)
-
-    def train_one_batch_with_retries(self, func, *args, **kwargs):
-        for _ in range(DEFAULT_MAX_ALLREDUCE_RETRY_NUM + 1):
-            try:
-                self._broadcast_if_needed()
-                result = func(*args, **kwargs)
-                break
-            except HorovodInternalError:
-                logger.warning(
-                    "Failed to perform allreduce operation on"
-                    "the gradients. Retrying..."
-                )
-                # Those error message show that the communication
-                # to merge gradient fails and we can rebuild the
-                # communication.
-                self.restore()
-            except RuntimeError:
-                traceback.print_exc()
-                self.restore()
-        self.data_shard_service.report_batch_done()
-        return result
-
-    def restore(self):
-        time.sleep(3)
-        # Call `load_state_dict` to reset the state of Horovod optimizer
-        self._optimizer.load_state_dict(self._optimizer.state_dict())
-        self._rendezvous_manager.init_horovod_if_needed()
