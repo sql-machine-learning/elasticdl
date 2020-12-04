0bbc61e47ca4e4b8b57bef16eaf75bd1bf539e4c
brightcoder01 <bright.gong@outlook.com>
2020-12-04 16:58:17 +0800

    Record the start running time stamp in PodInfo and sort the live workers according to this property.

--- elasticdl/python/master/pod_manager.py
+++ elasticdl/python/master/pod_manager.py
@@ -103,6 +103,16 @@ def _should_relaunch_killed_pod(evt_obj):
     )
 
 
+def _get_start_running_time_stamp(pod_status_obj):
+    if (
+        pod_status_obj.container_statuses
+        and pod_status_obj.container_statuses[0].running
+    ):
+        return pod_status_obj.container_statuses[0].running.started_at
+
+    return None
+
+
 def get_image_cluster_spec(cluster_spec):
     if cluster_spec:
         filename = os.path.basename(cluster_spec)
@@ -480,6 +490,7 @@ class PodManager(object):
         pod_name = evt_obj.metadata.name
         pod_ip = evt_obj.status.pod_ip
         phase = evt_obj.status.phase
+        pod_start_time = _get_start_running_time_stamp(evt_obj.status)
         pod_type = evt_obj.metadata.labels[ELASTICDL_REPLICA_TYPE_KEY]
 
         if pod_type == PodType.MASTER:
@@ -509,6 +520,7 @@ class PodManager(object):
                 name=pod_name,
                 ip=pod_ip,
                 status=new_state,
+                start_time=pod_start_time,
             )
             self._pod_info_cache[pod_type][pod_name] = pod_info
 
@@ -597,9 +609,7 @@ class PodManager(object):
 
     def get_alive_worker_name_addr(self):
         alive_workers = self.get_alive_workers()
-        # TODO: Update from sorting by id to sort by
-        # the timestamp when the pod changes to the Running state.
-        alive_workers.sort(key=lambda tup: tup.id)
+        alive_workers.sort(key=lambda pod_info: pod_info.start_time)
 
         return [(info.name, info.ip) for info in alive_workers]
 
