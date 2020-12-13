56a8b29dc0a6eb1cd9bd7698ddbc809d1246a701
Qinlong Wang <WangQL1201@outlook.com>
2020-12-14 06:45:30 +0800

    Elastic AllReduce controller for TensorFlow 1.x (#2414)
    * Support custom training loop for TensorFlow 2.x
    
    * Fix unittest
    
    * Fix unittest
    
    * Set the default value with the empty string for master address
    
    * Fix the feed api
    
    * Pre-commit
    
    * Add unittest for worker
    
    * Pre-commit
    
    * Add unittest for ElasticController
    
    * Fix too long functions
    
    * Fix ABC
    
    * Fix unittest
    
    * Fix ABC
    
    * Fix by comments
    
    * Elastic AllReduce for Pytorch
    
    * Add allreduce controller for PyTorch
    
    * Fix unittest
    
    * Add an unittest for PyTorchAllreduce
    
    * Add allreduce controller for TensorFlow 1.x
    
    * Fix unit tests
    
    * Fix by comments
    
    * Pre-commit

--- elasticdl/python/worker/worker.py
+++ elasticdl/python/worker/worker.py
@@ -13,6 +13,7 @@
 
 import os
 import traceback
+from distutils.version import LooseVersion
 
 import tensorflow as tf
 
@@ -39,6 +40,8 @@ from elasticdl_client.common.constants import DistributionStrategy
 DEFAULT_MAX_MINIBATCH_RETRY_NUM = 64
 
 DEFAULT_STEPS_TO_CHECK_RENDEZVOUS = 20
+
+_IS_TF2 = LooseVersion(tf.__version__) >= LooseVersion("2.0.0")
 
 
 class Worker(object):
@@ -475,13 +478,21 @@ class Worker(object):
             elastic_controller = PyTorchAllReduceController(
                 self._mc, self._data_shard_service
             )
-        else:
+        elif _IS_TF2:
             from elasticdl.python.allreduce.tensorflow_controller import (
                 TensorFlowV2AllReduceController,
             )
 
             elastic_controller = TensorFlowV2AllReduceController(
                 self._mc, self._data_shard_service
+            )
+        else:
+            from elasticdl.python.allreduce.tensorflow_controller import (
+                TensorFlowV1AllReduceController,
+            )
+
+            elastic_controller = TensorFlowV1AllReduceController(
+                self._mc, self._master_addr
             )
         # Initialize Horovod locally to generate varibles of the model
         # and optimizer.
