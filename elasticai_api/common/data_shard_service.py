93349cf2037712d972db17d8b13add996ba1f15e
brightcoder01 <55301748+brightcoder01@users.noreply.github.com>
2020-12-23 17:18:49 +0800

    Make data_shard_service to be compatible with various task types. (#2455)

--- elasticai_api/common/data_shard_service.py
+++ elasticai_api/common/data_shard_service.py
@@ -19,17 +19,23 @@ from elasticai_api.common.master_client import build_master_client
 from elasticai_api.proto import elasticai_api_pb2
 
 
-def build_data_shard_service(batch_size):
+def build_data_shard_service(batch_size, task_type=elasticai_api_pb2.TRAINING):
     master_client = build_master_client()
-    return DataShardService(batch_size=batch_size, master_client=master_client)
+    return DataShardService(
+        batch_size=batch_size, master_client=master_client, task_type=task_type
+    )
 
 
 class DataShardService(object):
     def __init__(
-        self, batch_size, master_client=None,
+        self,
+        batch_size,
+        master_client=None,
+        task_type=elasticai_api_pb2.TRAINING,
     ):
         self._mc = master_client
         self._batch_size = batch_size
+        self._task_type = task_type
         self._lock = threading.Lock()
         self._failed_record_count = 0
         self._reported_record_count = 0
@@ -41,11 +47,12 @@ class DataShardService(object):
 
     def get_task(self, task_type=None):
         task = self._mc.get_task(task_type)
-        if task.type == elasticai_api_pb2.TRAINING:
+        if task.type == self._task_type:
             with self._lock:
                 self._pending_tasks.append(task)
                 if len(self._pending_tasks) == 1:
                     self._current_task = task
+
         return task
 
     def _report_task(self, task, err_msg=""):
@@ -102,7 +109,8 @@ class DataShardService(object):
         """Fetch data shard and each shard contains the name,
         start and end index.
         """
-        task = self.get_task()
-        if task.type != elasticai_api_pb2.TRAINING:
+        task = self.get_task(self._task_type)
+        if task.type != self._task_type:
             return None
+
         return task.shard
