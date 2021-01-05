e26dfdd4344fd95b808330668e388f036b435431
workingloong <WangQL1201@outlook.com>
2021-01-05 19:52:27 +0800

    PyTorch example

--- model_zoo/mnist/mnist_pytorch_custom_dataset.py
+++ model_zoo/mnist/mnist_pytorch_custom_dataset.py
@@ -34,16 +34,15 @@ elasticdl train \
 """
 
 import argparse
-import linecache
-import os
-
 import cv2
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader, IterableDataset
+import torchvision
+from torch.utils.data import DataLoader, Dataset
+from torch.optim.lr_scheduler import StepLR
 
 from elasticai_api.pytorch.optimizer import DistributedOptimizer
 from elasticai_api.pytorch.controller import (
@@ -51,48 +50,21 @@ from elasticai_api.pytorch.controller import (
 )
 
 
-def read_images(shard):
-    """
-    shard.name is a CSV file path, like "/data/mnist/train/images.csv"
-    shard.start and shard.end is the line number of the CSV file.
-    The content of the CSV like:
-        5/img_26887.jpg,5
-        5/img_9954.jpg,5
-        5/img_29578.jpg,5
-        5/img_7979.jpg,5
-    Each line of the CSV contains the relative path and label.
-    """
-    filename = shard.name.split(":")[0]
-    records = linecache.getlines(filename)[shard.start : shard.end]
-    file_path = os.path.dirname(filename)
+class ElasticDataset(Dataset):
+    def __init__(self, data_shard_service, images):
+        self.data_shard_service = data_shard_service
+        self.images = images
 
-    images = []
-    for record in records:
-        image_path = os.path.join(file_path, record.split(",")[0])
-        label = int(record.split(",")[1])
+    def __len__(self):
+        return 100000
+
+    def __getitem__(self, index):
+        index = self.data_shard_service.fetch_record_index()
+        image_path, label = self.images[index]
         image = cv2.imread(image_path)
         image = np.array(image / 255.0, np.float32)
         image = image.reshape(3, 28, 28)
-        images.append((image, label))
-    return images
-
-
-class ImageDataset(IterableDataset):
-    def __init__(self, data_shard_service, shuffle=False):
-        self.data_shard_service = data_shard_service
-        self._shuffle = shuffle
-
-    def __iter__(self):
-        while True:
-            shard = self.data_shard_service.fetch_shard()
-            if shard:
-                images = read_images(shard)
-                if self._shuffle:
-                    np.random.shuffle(images)
-                for image in images:
-                    yield image
-            else:
-                break
+        return image, label
 
 
 class Net(nn.Module):
@@ -130,30 +102,50 @@ def train(args):
         pass a torch.utils.data.DataLoader.
         elastic_controller: The controller for elastic training.
     """
+    training_data = torchvision.datasets.ImageFolder(args.training_data)
     allreduce_controller = create_elastic_controller(
-        batch_size=args.batch_size
+        batch_size=args.batch_size,
+        dataset_size=len(training_data.imgs),
+        num_epochs=args.num_epochs,
+        shuffle=True,
     )
-    dataset = ImageDataset(
-        allreduce_controller.data_shard_service, shuffle=True
+    dataset = ElasticDataset(
+        allreduce_controller.data_shard_service, training_data.imgs
     )
-    data_loader = DataLoader(dataset=dataset, batch_size=args.batch_size)
+    data_loader = DataLoader(
+        dataset=dataset,
+        batch_size=args.batch_size,
+        num_workers=2
+    )
     model = Net()
     optimizer = optim.SGD(model.parameters(), lr=args.learning_rate)
     optimizer = DistributedOptimizer(optimizer, fixed_global_batch_size=True)
+    scheduler = StepLR(optimizer, step_size=1, gamma=0.5)
 
     # Set the model and optimizer to broadcast.
     allreduce_controller.set_broadcast_model(model)
     allreduce_controller.set_broadcast_optimizer(optimizer)
     model.train()
-    for batch_idx, (data, target) in enumerate(data_loader):
-        target = target.type(torch.LongTensor)
+    epoch = 0
+    with allreduce_controller.scope():
+        for batch_idx, (data, target) in enumerate(data_loader):
 
-        # Use the elastic function to wrap the training function with a batch.
-        elastic_train_one_batch = allreduce_controller.elastic_run(
-            train_one_batch
-        )
-        loss = elastic_train_one_batch(model, optimizer, data, target)
-        print("loss = {}, step = {}".format(loss, batch_idx))
+            new_epoch = int(
+                allreduce_controller.global_completed_batch_num / 100
+            )
+            if new_epoch > epoch:
+                epoch = new_epoch
+                scheduler.step()
+
+            target = target.type(torch.LongTensor)
+
+            # Use the elastic function to wrap the training function with
+            # a batch.
+            elastic_train_one_batch = allreduce_controller.elastic_run(
+                train_one_batch
+            )
+            loss = elastic_train_one_batch(model, optimizer, data, target)
+            print("loss = {}, step = {}".format(loss, batch_idx))
 
 
 def train_one_batch(model, optimizer, data, target):
@@ -168,8 +160,13 @@ def train_one_batch(model, optimizer, data, target):
 def arg_parser():
     parser = argparse.ArgumentParser(description="Process training parameters")
     parser.add_argument("--batch_size", type=int, default=64, required=False)
+    parser.add_argument("--num_epochs", type=int, default=1, required=False)
     parser.add_argument(
         "--learning_rate", type=float, default=0.1, required=False
+    )
+    parser.add_argument("--training_data", type=str, required=True)
+    parser.add_argument(
+        "--validation_data", type=str, default="", required=False
     )
     return parser
 
