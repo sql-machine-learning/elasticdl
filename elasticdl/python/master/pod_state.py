1bbbf7fd4b5e8c86393a2f835401b2afa7800c62
workingloong <WangQL1201@outlook.com>
2021-01-06 16:09:13 +0800

    The event type of PodStateFlow contains ADDED and MODIFED

--- elasticdl/python/master/pod_state.py
+++ elasticdl/python/master/pod_state.py
@@ -29,77 +29,77 @@ POD_STATE_FLOWS = [
     PodStateFlow(
         from_status=PodStatus.INITIAL,
         to_status=PodStatus.PENDING,
-        event_type="ADDED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Pending",
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.INITIAL,
         to_status=PodStatus.RUNNING,
-        event_type="ADDED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Running",
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.PENDING,
         to_status=PodStatus.RUNNING,
-        event_type="MODIFIED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Running",
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.PENDING,
         to_status=PodStatus.SUCCEEDED,
-        event_type="MODIFIED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Succeeded",
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.PENDING,
         to_status=PodStatus.FAILED,
-        event_type="MODIFIED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Failed",
         should_relaunch=True,
     ),
     PodStateFlow(
         from_status=PodStatus.RUNNING,
         to_status=PodStatus.SUCCEEDED,
-        event_type="MODIFIED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Succeeded",
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.RUNNING,
         to_status=PodStatus.FAILED,
-        event_type="MODIFIED",
+        event_type=["ADDED", "MODIFIED"],
         phase="Failed",
         should_relaunch=True,
     ),
     PodStateFlow(
         from_status=PodStatus.PENDING,
         to_status=PodStatus.DELETED,
-        event_type="DELETED",
+        event_type=["DELETED"],
         phase=None,
         should_relaunch=True,
     ),
     PodStateFlow(
         from_status=PodStatus.RUNNING,
         to_status=PodStatus.DELETED,
-        event_type="DELETED",
+        event_type=["DELETED"],
         phase=None,
         should_relaunch=True,
     ),
     PodStateFlow(
         from_status=PodStatus.SUCCEEDED,
         to_status=PodStatus.DELETED,
-        event_type="DELETED",
+        event_type=["DELETED"],
         phase=None,
         should_relaunch=False,
     ),
     PodStateFlow(
         from_status=PodStatus.FAILED,
         to_status=PodStatus.DELETED,
-        event_type="DELETED",
+        event_type=["DELETED"],
         phase=None,
         should_relaunch=False,
     ),
@@ -110,7 +110,7 @@ def get_pod_state_flow(from_status, event_type, phase):
     for pod_state_flow in POD_STATE_FLOWS:
         if (
             from_status == pod_state_flow.from_status
-            and event_type == pod_state_flow.event_type
+            and event_type in pod_state_flow.event_type
             and (pod_state_flow.phase is None or phase == pod_state_flow.phase)
         ):
             return pod_state_flow
